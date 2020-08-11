c442bacecc8e74584d8df3efb8178eecf2b12fae
edragoev1 <48771892+edragoev1@users.noreply.github.com>
2020-08-11 00:34:44 -0400

    Add files via upload

---
+++ build-dotnet-core.cmd
@@ -0,0 +1,200 @@
+rd/s/q bin
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_01
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_02
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_03
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_04
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_05
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_06
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_07
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_08
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_09
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_10
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_11
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_12
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_13
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_14
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_15
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_16
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_17
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_18
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_19
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_20
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_21
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_22
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_23
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_24
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_25
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_26
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_27
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_28
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_29
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_30
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_31
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_32
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_33
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_34
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_35
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_36
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_37
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_38
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_39
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_40
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_41
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_42
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_43
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_44
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_45
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_46
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_47
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_48
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_49
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_50
+dotnet run
---
+++ build-go.sh
@@ -0,0 +1,101 @@
+go build -o Example_01.exe src/example01/main.go
+go build -o Example_02.exe src/example02/main.go
+go build -o Example_03.exe src/example03/main.go
+go build -o Example_04.exe src/example04/main.go
+go build -o Example_05.exe src/example05/main.go
+go build -o Example_06.exe src/example06/main.go
+go build -o Example_07.exe src/example07/main.go
+go build -o Example_08.exe src/example08/main.go
+go build -o Example_09.exe src/example09/main.go
+go build -o Example_10.exe src/example10/main.go
+go build -o Example_11.exe src/example11/main.go
+go build -o Example_12.exe src/example12/main.go
+go build -o Example_13.exe src/example13/main.go
+go build -o Example_14.exe src/example14/main.go
+go build -o Example_15.exe src/example15/main.go
+go build -o Example_16.exe src/example16/main.go
+go build -o Example_17.exe src/example17/main.go
+go build -o Example_18.exe src/example18/main.go
+go build -o Example_19.exe src/example19/main.go
+go build -o Example_20.exe src/example20/main.go
+go build -o Example_21.exe src/example21/main.go
+go build -o Example_22.exe src/example22/main.go
+go build -o Example_23.exe src/example23/main.go
+go build -o Example_24.exe src/example24/main.go
+go build -o Example_25.exe src/example25/main.go
+go build -o Example_26.exe src/example26/main.go
+go build -o Example_27.exe src/example27/main.go
+go build -o Example_28.exe src/example28/main.go
+go build -o Example_29.exe src/example29/main.go
+go build -o Example_30.exe src/example30/main.go
+go build -o Example_31.exe src/example31/main.go
+go build -o Example_32.exe src/example32/main.go
+go build -o Example_33.exe src/example33/main.go
+go build -o Example_34.exe src/example34/main.go
+go build -o Example_35.exe src/example35/main.go
+go build -o Example_36.exe src/example36/main.go
+go build -o Example_37.exe src/example37/main.go
+go build -o Example_38.exe src/example38/main.go
+go build -o Example_39.exe src/example39/main.go
+go build -o Example_40.exe src/example40/main.go
+go build -o Example_41.exe src/example41/main.go
+go build -o Example_42.exe src/example42/main.go
+go build -o Example_43.exe src/example43/main.go
+go build -o Example_44.exe src/example44/main.go
+go build -o Example_45.exe src/example45/main.go
+go build -o Example_46.exe src/example46/main.go
+go build -o Example_47.exe src/example47/main.go
+go build -o Example_48.exe src/example48/main.go
+go build -o Example_49.exe src/example49/main.go
+go build -o Example_50.exe src/example50/main.go
+
+./Example_01.exe
+./Example_02.exe
+./Example_03.exe
+./Example_04.exe
+./Example_05.exe
+./Example_06.exe
+./Example_07.exe
+./Example_08.exe
+./Example_09.exe
+./Example_10.exe
+./Example_11.exe
+./Example_12.exe
+./Example_13.exe
+./Example_14.exe
+./Example_15.exe
+./Example_16.exe
+./Example_17.exe
+./Example_18.exe
+./Example_19.exe
+./Example_20.exe
+./Example_21.exe
+./Example_22.exe
+./Example_23.exe
+./Example_24.exe
+./Example_25.exe
+./Example_26.exe
+./Example_27.exe
+./Example_28.exe
+./Example_29.exe
+./Example_30.exe
+./Example_31.exe
+./Example_32.exe
+./Example_33.exe
+./Example_34.exe
+./Example_35.exe
+./Example_36.exe
+./Example_37.exe
+./Example_38.exe
+./Example_39.exe
+./Example_40.exe
+./Example_41.exe
+./Example_42.exe
+./Example_43.exe
+./Example_44.exe
+./Example_45.exe
+./Example_46.exe
+./Example_47.exe
+./Example_48.exe
+./Example_49.exe
+./Example_50.exe
