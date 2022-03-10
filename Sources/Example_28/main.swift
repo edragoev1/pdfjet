46a9d422c41fb62279a712145469210c0f70d0eb
EDragoev <edragoev@protonmail.com>
2022-03-10 14:14:07 -0500

    Added the Subsetted Symbols font.

--- Sources/Example_28/main.swift
+++ Sources/Example_28/main.swift
@@ -24,8 +24,14 @@ public class Example_28 {
                     InputStream(fileAtPath: "fonts/Droid/DroidSansFallback.ttf.stream")!,
                     Font.STREAM)
 
+            let f3 = try Font(
+                    pdf,
+                    InputStream(fileAtPath: "fonts/Noto/NotoSansSymbols-Regular-Subsetted.ttf.stream")!,
+                    Font.STREAM)
+
             f1.setSize(11.0)
             f2.setSize(11.0)
+            f3.setSize(11.0)
 
             let page = Page(pdf, Letter.LANDSCAPE)
 
@@ -33,11 +39,104 @@ public class Example_28 {
                     "data/report.csv", encoding: .utf8)).trimmingCharacters(in: .newlines)
             let lines = str.components(separatedBy: "\n")
 
+            let x: Float = 50.0
             var y: Float = 40.0
             for line in lines {
                 y += 20.0
-                TextLine(f1, line).setFallbackFont(f2).setLocation(50.0, y).drawOn(page)
+                TextLine(f1, line)
+                        .setFallbackFont(f2)
+                        .setLocation(50.0, y)
+                        .drawOn(page)
             }
+
+            y = 210.0
+            let dy: Float = 22.0
+
+            let text = TextLine(f3)
+            var buf = String()
+            var count: Int = 0
+            var i: Int = 0x2200
+            while i <= 0x22FF {
+                // Draw the Math Symbols
+                if count % 80 == 0 {
+                    y += dy
+                    text.setText(buf)
+                    text.setLocation(x, y)
+                    text.drawOn(page)
+                    buf = ""
+                }
+                buf.append(Character(UnicodeScalar(i)!))
+                count += 1
+                i += 1
+            }
+            y += dy
+            text.setText(buf)
+            text.setLocation(x, y)
+            text.drawOn(page)
+
+            buf = ""
+            count = 0
+            i = 0x25A0
+            while i <= 0x25FF {
+                // Draw the Geometric Shapes
+                if count % 80 == 0 {
+                    y += dy
+                    text.setText(buf)
+                    text.setLocation(x, y)
+                    text.drawOn(page)
+                    buf = ""
+                }
+                buf.append(Character(UnicodeScalar(i)!))
+                count += 1
+                i += 1
+            }
+
+            y += dy
+            text.setText(buf)
+            text.setLocation(x, y)
+            text.drawOn(page)
+
+            buf = ""
+            count = 0
+            i = 0x2701
+            while i <= 0x27ff {
+                // Draw the Dingbats
+                if count % 80 == 0 {
+                    y += dy
+                    text.setText(buf)
+                    text.setLocation(x, y)
+                    text.drawOn(page)
+                    buf = ""
+                }
+                buf.append(Character(UnicodeScalar(i)!))
+                count += 1
+                i += 1
+            }
+
+            y += dy
+            text.setText(buf)
+            text.setLocation(x, y)
+            text.drawOn(page)
+
+            buf = ""
+            count = 0
+            i = 0x2800
+            while i <= 0x28FF {
+                // Draw the Braille Patterns
+                if count % 80 == 0 {
+                    y += dy
+                    text.setText(buf)
+                    text.setLocation(x, y)
+                    text.drawOn(page)
+                    buf = ""
+                }
+                buf.append(Character(UnicodeScalar(i)!))
+                count += 1
+                i += 1
+            }
+            text.setText(buf)
+            text.setLocation(x, y)
+            text.drawOn(page)
 
             pdf.complete()
         }
