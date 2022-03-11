c7b1fdc71e8636eb9d2fd11edc70ac6e434a6e47
EDragoev <edragoev@protonmail.com>
2022-03-10 23:15:53 -0500

    Removed examples that did not compile.

--- build-swift.sh
+++ build-swift.sh
@@ -6,10 +6,6 @@ swift run --configuration release Example_05
 swift run --configuration release Example_06
 swift run --configuration release Example_07
 swift run --configuration release Example_08
-swift run --configuration release Example_09
-swift run --configuration release Example_10
-swift run --configuration release Example_11
-swift run --configuration release Example_12
 swift run --configuration release Example_13
 swift run --configuration release Example_14
 swift run --configuration release Example_15
@@ -26,13 +22,11 @@ swift run --configuration release Example_25
 swift run --configuration release Example_26
 swift run --configuration release Example_27
 swift run --configuration release Example_28
-swift run --configuration release Example_29
 swift run --configuration release Example_30
 swift run --configuration release Example_31
 swift run --configuration release Example_32
 swift run --configuration release Example_33
 swift run --configuration release Example_34
-swift run --configuration release Example_35
 swift run --configuration release Example_36
 swift run --configuration release Example_37
 swift run --configuration release Example_38
@@ -41,10 +35,7 @@ swift run --configuration release Example_40
 swift run --configuration release Example_41
 swift run --configuration release Example_42
 swift run --configuration release Example_43
-swift run --configuration release Example_44
 swift run --configuration release Example_45
 swift run --configuration release Example_46
 swift run --configuration release Example_47
-swift run --configuration release Example_48
-swift run --configuration release Example_49
 swift run --configuration release Example_50
