9890ea62d9b0c59796bd2781901c7df12ad2b2f4
edragoev <edragoev@protonmail.com>
2022-05-10 10:05:24 -0400

    Small cleanup.

--- run-swift.sh
+++ run-swift.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-swift run --configuration release Example_$1
-# swift run --configuration debug Example_$1
+# swift run --configuration release Example_$1
+swift run --configuration debug Example_$1
 
 # evince Example_$1.pdf
 mupdf Example_$1.pdf
