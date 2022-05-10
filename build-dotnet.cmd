d0bc657ff98dd1c24ad5535e1e3d10e7711168a5
edragoev <edragoev@protonmail.com>
2022-05-10 10:47:31 -0400

    More cleanup ...

--- build-dotnet.cmd
+++ build-dotnet.cmd
@@ -1,104 +1,200 @@
-REM call csc /o /out:PDFjet.dll /target:library net\pdfjet\*.cs
-call csc /out:PDFjet.dll /target:library net\pdfjet\*.cs
+rd/s/q bin
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_01
+dotnet run
 
-call csc examples\Example_01.cs /reference:PDFjet.dll
-call csc examples\Example_02.cs /reference:PDFjet.dll
-call csc examples\Example_03.cs /reference:PDFjet.dll
-call csc examples\Example_04.cs /reference:PDFjet.dll
-call csc examples\Example_05.cs /reference:PDFjet.dll
-call csc examples\Example_06.cs /reference:PDFjet.dll
-call csc examples\Example_07.cs /reference:PDFjet.dll
-call csc examples\Example_08.cs /reference:PDFjet.dll
-call csc examples\Example_09.cs /reference:PDFjet.dll
-call csc examples\Example_10.cs /reference:PDFjet.dll
-call csc examples\Example_11.cs /reference:PDFjet.dll
-call csc examples\Example_12.cs /reference:PDFjet.dll
-call csc examples\Example_13.cs /reference:PDFjet.dll
-call csc examples\Example_14.cs /reference:PDFjet.dll
-call csc examples\Example_15.cs /reference:PDFjet.dll
-call csc examples\Example_16.cs /reference:PDFjet.dll
-call csc examples\Example_17.cs /reference:PDFjet.dll
-call csc examples\Example_18.cs /reference:PDFjet.dll
-call csc examples\Example_19.cs /reference:PDFjet.dll
-call csc examples\Example_20.cs /reference:PDFjet.dll
-call csc examples\Example_21.cs /reference:PDFjet.dll
-call csc examples\Example_22.cs /reference:PDFjet.dll
-call csc examples\Example_23.cs /reference:PDFjet.dll
-call csc examples\Example_24.cs /reference:PDFjet.dll
-call csc examples\Example_25.cs /reference:PDFjet.dll
-call csc examples\Example_26.cs /reference:PDFjet.dll
-call csc examples\Example_27.cs /reference:PDFjet.dll
-call csc examples\Example_28.cs /reference:PDFjet.dll
-call csc examples\Example_29.cs /reference:PDFjet.dll
-call csc examples\Example_30.cs /reference:PDFjet.dll
-call csc examples\Example_31.cs /reference:PDFjet.dll
-call csc examples\Example_32.cs /reference:PDFjet.dll
-call csc examples\Example_33.cs /reference:PDFjet.dll
-call csc examples\Example_34.cs /reference:PDFjet.dll
-call csc examples\Example_35.cs /reference:PDFjet.dll
-call csc examples\Example_36.cs /reference:PDFjet.dll
-call csc examples\Example_37.cs /reference:PDFjet.dll
-call csc examples\Example_38.cs /reference:PDFjet.dll
-call csc examples\Example_39.cs /reference:PDFjet.dll
-call csc examples\Example_40.cs /reference:PDFjet.dll
-call csc examples\Example_41.cs /reference:PDFjet.dll
-call csc examples\Example_42.cs /reference:PDFjet.dll
-call csc examples\Example_43.cs /reference:PDFjet.dll
-call csc examples\Example_44.cs /reference:PDFjet.dll
-call csc examples\Example_45.cs /reference:PDFjet.dll
-call csc examples\Example_46.cs /reference:PDFjet.dll
-call csc examples\Example_47.cs /reference:PDFjet.dll
-call csc examples\Example_48.cs /reference:PDFjet.dll
-call csc examples\Example_49.cs /reference:PDFjet.dll
-call csc examples\Example_50.cs /reference:PDFjet.dll
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_02
+dotnet run
 
-Example_01
-Example_02
-Example_03
-Example_04
-Example_05
-Example_06
-Example_07
-Example_08
-Example_09
-Example_10
-Example_11
-Example_12
-Example_13
-Example_14
-Example_15
-Example_16
-Example_17
-Example_18
-Example_19
-Example_20
-Example_21
-Example_22
-Example_23
-Example_24
-Example_25
-Example_26
-Example_27
-Example_28
-Example_29
-Example_30
-Example_31
-Example_32
-Example_33
-Example_34
-Example_35
-Example_36
-Example_37
-Example_38
-Example_39
-Example_40
-Example_41
-Example_42
-Example_43
-Example_44
-Example_45
-Example_46
-Example_47
-Example_48
-Example_49
-Example_50
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_03
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_04
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_05
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_06
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_07
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_08
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_09
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_10
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_11
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_12
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_13
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_14
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_15
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_16
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_17
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_18
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_19
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_20
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_21
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_22
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_23
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_24
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_25
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_26
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_27
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_28
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_29
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_30
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_31
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_32
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_33
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_34
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_35
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_36
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_37
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_38
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_39
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_40
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_41
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_42
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_43
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_44
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_45
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_46
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_47
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_48
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_49
+dotnet run
+
+rd/s/q obj
+dotnet build PDFjet.csproj /p:StartupObject=Example_50
+dotnet run
