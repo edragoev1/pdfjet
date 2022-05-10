c9cc2b534fe807b4a80dd43a3ba6a77a94aff899
edragoev <edragoev@protonmail.com>
2022-05-10 09:25:11 -0400

    Removed old scripts.

--- rundotnetcore.sh
+++
@@ -1,5 +0,0 @@
-rm -rf bin
-rm -rf obj
-dotnet build PDFjet.csproj /p:StartupObject=Example_$1
-dotnet run
-mupdf-gl Example_$1.pdf
--- runswift.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# swift run --configuration release Example_$1
-swift run --configuration debug Example_$1
-
-# mupdf-gl Example_$1.pdf
-evince Example_$1.pdf
