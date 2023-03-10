97d4c8ee3323233306e546785968cc32d9222198
EDragoev <edragoev@protonmail.com>
2023-03-10 12:23:39 -0500

    Refactored the code.

--- generate-documentation.sh
+++ generate-documentation.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
-
 rm docs/java/com/pdfjet/*.html
-rm docs/_net/net/pdfjet/*.html
 rm docs/java/*.html
-rm docs/_net/*.html
-
-# /opt/jdk1.5.0_22/bin/javadoc -public -notree -noindex -nonavbar com/pdfjet/*.java -d docs/java
-# /opt/jdk1.5.0_22/bin/javac util/Translate.java
-# /opt/jdk1.5.0_22/bin/java util.Translate
 
 javadoc -public com/pdfjet/*.java -d docs/java
+rm -rf docs/_net
+cp -r docs/java docs/_net
+mv docs/_net/com docs/_net/net
+javac util/Translate.java
+java util.Translate
