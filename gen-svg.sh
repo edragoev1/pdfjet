3c0fdc11c298213574b8f971edf57e70636e8ae6
EDragoev <edragoev@protonmail.com>
2023-03-15 18:29:30 -0400

    Cleaned up and improved.

--- gen-svg.sh
+++ gen-svg.sh
@@ -1,4 +1,4 @@
-javac -encoding utf-8 -Xlint com/pdfjet/*.java
-jar cf PDFjet.jar com/pdfjet/*.class
+javac -O -encoding utf-8 -Xlint com/pdfjet/*.java -d out/production
+jar cf PDFjet.jar -C out/production .
 
 java -cp .:PDFjet.jar com.pdfjet.SVG $1
