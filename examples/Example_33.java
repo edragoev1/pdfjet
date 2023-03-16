fa02e2a4c74a07bdade4eaf042c95f3b5e02f22e
EDragoev <edragoev@protonmail.com>
2023-03-16 10:10:10 -0400

    Fixed the example. The input stream is closed by the image classes

--- examples/Example_33.java
+++ examples/Example_33.java
@@ -21,7 +21,6 @@ public class Example_33 {
 
         FileInputStream stream = new FileInputStream("images/photoshop.jpg");
         Image image = new Image(pdf, stream, ImageType.JPG);
-        stream.close();
         image.setLocation(10f, 10f);
         image.scaleBy(0.25f);
         image.drawOn(page);
@@ -29,28 +28,24 @@ public class Example_33 {
         stream = new FileInputStream(
             "images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg");
         SVGImage icon = new SVGImage(stream);
-        stream.close();
         icon.setLocation(20f, 670f);
         icon.drawOn(page);
 
         stream = new FileInputStream(
             "images/svg/add_circle_FILL0_wght400_GRAD0_opsz48.svg");
         icon = new SVGImage(stream);
-        stream.close();
         icon.setLocation(120f, 670f);
         icon.drawOn(page);
 
         stream = new FileInputStream(
             "images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg");
         icon = new SVGImage(stream);
-        stream.close();
         icon.setLocation(220f, 670f);
         icon.drawOn(page);
 
         stream = new FileInputStream(
             "images/svg/auto_stories_FILL0_wght400_GRAD0_opsz48.svg");
         icon = new SVGImage(stream);
-        stream.close();
         icon.setLocation(320f, 670f);
         icon.drawOn(page);
 
