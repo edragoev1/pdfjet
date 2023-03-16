3deb3cf6b473c39a5859dd8fa62f281cbfe1e885
EDragoev <edragoev@protonmail.com>
2023-03-16 10:02:22 -0400

    Added more test code.

--- examples/Example_33.java
+++ examples/Example_33.java
@@ -30,7 +30,28 @@ public class Example_33 {
             "images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg");
         SVGImage icon = new SVGImage(stream);
         stream.close();
-        icon.setLocation(20f, 700f);
+        icon.setLocation(20f, 670f);
+        icon.drawOn(page);
+
+        stream = new FileInputStream(
+            "images/svg/add_circle_FILL0_wght400_GRAD0_opsz48.svg");
+        icon = new SVGImage(stream);
+        stream.close();
+        icon.setLocation(120f, 670f);
+        icon.drawOn(page);
+
+        stream = new FileInputStream(
+            "images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg");
+        icon = new SVGImage(stream);
+        stream.close();
+        icon.setLocation(220f, 670f);
+        icon.drawOn(page);
+
+        stream = new FileInputStream(
+            "images/svg/auto_stories_FILL0_wght400_GRAD0_opsz48.svg");
+        icon = new SVGImage(stream);
+        stream.close();
+        icon.setLocation(320f, 670f);
         icon.drawOn(page);
 
         pdf.complete();
