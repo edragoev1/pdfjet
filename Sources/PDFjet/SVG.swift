d66404ed92f8a4bc7abb937aa304abaf55d22ec1
EDragoev <edragoev@protonmail.com>
2023-03-15 23:51:33 -0400

    Added one more method.

--- Sources/PDFjet/SVG.swift
+++ Sources/PDFjet/SVG.swift
@@ -70,4 +70,52 @@ public class SVG {
         }
         return false
     }
+
+    public func getSVGPathOps(_ paths: [String]) -> [PathOp] {
+        var operations = [PathOp]()
+        var op: PathOp?
+        for path in paths {
+            // Path example:
+            // "M22.65 34h3v-8.3H34v-3h-8.35V14h-3v8.7H14v3h8.65ZM24 44z"
+            // System.out.println(path);
+            // System.out.println();
+            var buf = String()
+            var token = false
+            for ch in path {
+                if (isCommand(ch)) {                    // open path
+                    if (token) {
+                        op!.args.append(buf)
+                        buf = ""
+                    }
+                    token = false
+                    op = PathOp(ch)
+                    operations.append(op!);
+                } else if (ch == " " || ch == ",") {
+                    if (token) {
+                        op!.args.append(buf)
+                        buf = ""
+                    }
+                    token = false;
+                } else if (ch == "-") {
+                    if (token) {
+                        op!.args.append(buf)
+                        buf = ""
+                    }
+                    token = true;
+                    buf.append(ch);
+                } else if (ch == ".") {
+                    if (buf.contains(".")) {
+                        op!.args.append(buf)
+                        buf = ""
+                    }
+                    token = true
+                    buf.append(ch)
+                } else {
+                    token = true
+                    buf.append(ch)
+                }
+            }
+        }
+        return operations
+    }
 }
