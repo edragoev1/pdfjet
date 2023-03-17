735df4d93349b00cb18962ea7983a21c22591221
EDragoev <edragoev@protonmail.com>
2023-03-17 07:50:23 -0400

    Updated the example

--- src/examples/example33/main.go
+++ src/examples/example33/main.go
@@ -99,15 +99,25 @@ func Example33() {
 	image2.SetLocation(xy[0], 670.0)
 	xy = image2.DrawOn(page)
 
-	// file2, err = os.Open("images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer file2.Close()
-	// reader2 = bufio.NewReader(file2)
-	// image2 = pdfjet.NewSVGImage(reader2)
-	// image2.SetLocation(xy[0], 670.0)
-	// image2.DrawOn(page)
+	file2, err = os.Open("images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file2.Close()
+	reader2 = bufio.NewReader(file2)
+	image2 = pdfjet.NewSVGImage(reader2)
+	image2.SetLocation(xy[0], 670.0)
+	xy = image2.DrawOn(page)
+
+	file2, err = os.Open("images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file2.Close()
+	reader2 = bufio.NewReader(file2)
+	image2 = pdfjet.NewSVGImage(reader2)
+	image2.SetLocation(xy[0], 670.0)
+	image2.DrawOn(page)
 
 	pdf.Complete()
 }
