4e61dd9fda74e19c65dfee4b4406230d422e79c7
EDragoev <edragoev@protonmail.com>
2023-03-18 10:19:42 -0400

    Added to the project.

--- com/pdfjet/PDFPath.java
+++ com/pdfjet/PDFPath.java
@@ -1,11 +1,13 @@
 package com.pdfjet;
 
-public class SVGPath {
-    String data;            // The path data
+import java.util.*;
+
+public class PDFPath {
+    List<PathOp> data;      // The path operations
     int fill = Color.black; // The fill color or -1 (don't fill)
     int stroke = -1;        // The stroke color or -1 (don't stroke)
     float strokeWidth;      // The stroke width
 
-    public SVGPath() {
+    public PDFPath() {
     }
 }
