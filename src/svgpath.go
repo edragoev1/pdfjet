dc2edb1dc6b0ff4f1292e3b74886854a27b14203
EDragoev <edragoev@protonmail.com>
2023-03-18 13:24:42 -0400

    First added to the project

---
+++ src/svgpath.go
@@ -0,0 +1,41 @@
+package pdfjet
+
+/**
+ * svgpath.go
+ *
+Copyright 2023 Innovatics Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+import (
+	"io"
+)
+
+type SVGPath struct {
+	data        string    // The SVG path data
+	operations  []*PathOp // The PDF path operations
+	fill        uint32    // The fill color or -1 (don't fill)
+	stroke      uint32    // The stroke color or -1 (don't stroke)
+	strokeWidth float32   // The stroke width
+}
+
+func NewSVGPath(reader io.Reader) *SVGPath {
+	path := new(SVGPath)
+	return path
+}
