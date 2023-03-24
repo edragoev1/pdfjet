7d3aaf6aabf49de3c5e5545a3219e99aa8c2fd19
EDragoev <edragoev@protonmail.com>
2023-03-24 13:31:05 -0400

    Shortened the script

--- build-go.sh
+++ build-go.sh
@@ -1,107 +1,21 @@
 cd src
 
-go build -o ../Example_01.exe examples/example01/main.go
-go build -o ../Example_02.exe examples/example02/main.go
-go build -o ../Example_03.exe examples/example03/main.go
-go build -o ../Example_04.exe examples/example04/main.go
-go build -o ../Example_05.exe examples/example05/main.go
-go build -o ../Example_06.exe examples/example06/main.go
-go build -o ../Example_07.exe examples/example07/main.go
-go build -o ../Example_08.exe examples/example08/main.go
-go build -o ../Example_09.exe examples/example09/main.go
-go build -o ../Example_10.exe examples/example10/main.go
-go build -o ../Example_11.exe examples/example11/main.go
-go build -o ../Example_12.exe examples/example12/main.go
-go build -o ../Example_13.exe examples/example13/main.go
-go build -o ../Example_14.exe examples/example14/main.go
-go build -o ../Example_15.exe examples/example15/main.go
-go build -o ../Example_16.exe examples/example16/main.go
-go build -o ../Example_17.exe examples/example17/main.go
-go build -o ../Example_18.exe examples/example18/main.go
-go build -o ../Example_19.exe examples/example19/main.go
-go build -o ../Example_20.exe examples/example20/main.go
-go build -o ../Example_21.exe examples/example21/main.go
-go build -o ../Example_22.exe examples/example22/main.go
-go build -o ../Example_23.exe examples/example23/main.go
-go build -o ../Example_24.exe examples/example24/main.go
-go build -o ../Example_25.exe examples/example25/main.go
-go build -o ../Example_26.exe examples/example26/main.go
-go build -o ../Example_27.exe examples/example27/main.go
-go build -o ../Example_28.exe examples/example28/main.go
-go build -o ../Example_29.exe examples/example29/main.go
-go build -o ../Example_30.exe examples/example30/main.go
-go build -o ../Example_31.exe examples/example31/main.go
-go build -o ../Example_32.exe examples/example32/main.go
-go build -o ../Example_33.exe examples/example33/main.go
-go build -o ../Example_34.exe examples/example34/main.go
-go build -o ../Example_35.exe examples/example35/main.go
-go build -o ../Example_36.exe examples/example36/main.go
-go build -o ../Example_37.exe examples/example37/main.go
-go build -o ../Example_38.exe examples/example38/main.go
-go build -o ../Example_39.exe examples/example39/main.go
-go build -o ../Example_40.exe examples/example40/main.go
-go build -o ../Example_41.exe examples/example41/main.go
-go build -o ../Example_42.exe examples/example42/main.go
-go build -o ../Example_43.exe examples/example43/main.go
-go build -o ../Example_44.exe examples/example44/main.go
-go build -o ../Example_45.exe examples/example45/main.go
-go build -o ../Example_46.exe examples/example46/main.go
-go build -o ../Example_47.exe examples/example47/main.go
-go build -o ../Example_48.exe examples/example48/main.go
-go build -o ../Example_49.exe examples/example49/main.go
-go build -o ../Example_50.exe examples/example50/main.go
-go build -o ../Example_51.exe examples/example51/main.go
+for i in $(seq 1 51);
+do
+    if [ $i -lt 10 ]; then
+        go build -o ../Example_0$i.exe examples/example0$i/main.go
+    else
+        go build -o ../Example_$i.exe examples/example$i/main.go
+    fi
+done
 
 cd ..
 
-./Example_01.exe
-./Example_02.exe
-./Example_03.exe
-./Example_04.exe
-./Example_05.exe
-./Example_06.exe
-./Example_07.exe
-./Example_08.exe
-./Example_09.exe
-./Example_10.exe
-./Example_11.exe
-./Example_12.exe
-./Example_13.exe
-./Example_14.exe
-./Example_15.exe
-./Example_16.exe
-./Example_17.exe
-./Example_18.exe
-./Example_19.exe
-./Example_20.exe
-./Example_21.exe
-./Example_22.exe
-./Example_23.exe
-./Example_24.exe
-./Example_25.exe
-./Example_26.exe
-./Example_27.exe
-./Example_28.exe
-./Example_29.exe
-./Example_30.exe
-./Example_31.exe
-./Example_32.exe
-./Example_33.exe
-./Example_34.exe
-./Example_35.exe
-./Example_36.exe
-./Example_37.exe
-./Example_38.exe
-./Example_39.exe
-./Example_40.exe
-./Example_41.exe
-./Example_42.exe
-./Example_43.exe
-./Example_44.exe
-./Example_45.exe
-./Example_46.exe
-./Example_47.exe
-./Example_48.exe
-./Example_49.exe
-./Example_50.exe
-./Example_51.exe
+for i in $(seq 1 51);
+do
+    if [ $i -lt 10 ]; then
+        ./Example_0$i.exe
+    else
+        ./Example_$i.exe
+    fi
+done
