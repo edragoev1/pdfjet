83f48f22792d0cc1994ac06bf36564b7d20ae429
EDragoev <edragoev@protonmail.com>
2023-03-24 13:16:27 -0400

    Simplifed the script

--- build-java-1.5.sh
+++ build-java-1.5.sh
@@ -1,106 +1,20 @@
 /opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint com/pdfjet/*.java -d out/production
 /opt/jdk1.5.0_22/bin/jar cf PDFjet.jar -C out/production .
 
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_01.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_02.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_03.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_04.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_05.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_06.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_07.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_08.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_09.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_10.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_11.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_12.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_13.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_14.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_15.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_16.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_17.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_18.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_19.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_20.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_21.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_22.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_23.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_24.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_25.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_26.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_27.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_28.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_29.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_30.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_31.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_32.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_33.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_34.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_35.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_36.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_37.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_38.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_39.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_40.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_41.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_42.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_43.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_44.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_45.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_46.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_47.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_48.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_49.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_50.java -d out/production
-/opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_51.java -d out/production
+for i in $(seq 1 51);
+do
+    if [ $i -lt 10 ]; then
+        /opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_0$i.java -d out/production
+    else
+        /opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_$i.java -d out/production
+    fi
+done
 
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_01
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_02
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_03
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_04
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_05
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_06
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_07
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_08
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_09
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_10
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_11
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_12
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_13
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_14
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_15
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_16
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_17
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_18
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_19
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_20
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_21
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_22
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_23
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_24
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_25
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_26
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_27
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_28
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_29
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_30
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_31
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_32
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_33
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_34
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_35
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_36
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_37
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_38
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_39
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_40
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_41
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_42
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_43
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_44
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_45
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_46
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_47
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_48
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_49
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_50
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_51
+for i in $(seq 1 51);
+do
+    if [ $i -lt 10 ]; then
+        /opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_0$i
+    else
+        /opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_$i
+    fi
+done
