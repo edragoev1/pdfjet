f24a784b79a610d4cd91574709077b35f3cc1be2
EDragoev <edragoev@protonmail.com>
2023-03-23 23:39:17 -0400

    Completed the example

--- src/examples/example18/main.go
+++ src/examples/example18/main.go
@@ -8,9 +8,9 @@ import (
 	"time"
 
 	pdfjet "github.com/edragoev1/pdfjet/src"
+	"github.com/edragoev1/pdfjet/src/a4"
 	"github.com/edragoev1/pdfjet/src/color"
 	"github.com/edragoev1/pdfjet/src/compliance"
-	"github.com/edragoev1/pdfjet/src/letter"
 )
 
 /**
@@ -26,29 +26,59 @@ func Example18() {
 	w := bufio.NewWriter(file)
 	pdf := pdfjet.NewPDF(w, compliance.PDF15)
 
-	page := pdfjet.NewPageAddTo(pdf, letter.Portrait)
+	file, err = os.Open("fonts/RedHatText/RedHatText-Regular.ttf.stream")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	font := pdfjet.NewFontStream1(pdf, reader)
+	font.SetSize(12.0)
+
+	pages := make([]*pdfjet.Page, 0)
+
+	page := pdfjet.NewPage(pdf, a4.Portrait)
 	box := pdfjet.NewBox()
 	box.SetLocation(50.0, 50.0)
 	box.SetSize(100.0, 100.0)
 	box.SetColor(color.Red)
 	box.SetFillShape(true)
 	box.DrawOn(page)
+	pages = append(pages, page)
 
-	page = pdfjet.NewPageAddTo(pdf, letter.Portrait)
+	page = pdfjet.NewPage(pdf, a4.Portrait)
 	box = pdfjet.NewBox()
 	box.SetLocation(50.0, 50.0)
 	box.SetSize(100.0, 100.0)
 	box.SetColor(color.Green)
 	box.SetFillShape(true)
 	box.DrawOn(page)
+	pages = append(pages, page)
 
-	page = pdfjet.NewPageAddTo(pdf, letter.Portrait)
+	page = pdfjet.NewPage(pdf, a4.Portrait)
 	box = pdfjet.NewBox()
 	box.SetLocation(50.0, 50.0)
 	box.SetSize(100.0, 100.0)
 	box.SetColor(color.Blue)
 	box.SetFillShape(true)
 	box.DrawOn(page)
+	pages = append(pages, page)
+
+	for i := 0; i < len(pages); i++ {
+		page := pages[i]
+		footer := "Page " + fmt.Sprint(i+1) + " of " + fmt.Sprint((len(pages)))
+		page.SetBrushColor(color.Black)
+		page.DrawString(
+			font,
+			nil,
+			footer,
+			(page.GetWidth()-font.StringWidth(font, footer))/2.0,
+			(page.GetHeight() - 5.0))
+	}
+
+	for i := 0; i < len(pages); i++ {
+		pdf.AddPage(pages[i])
+	}
 
 	pdf.Complete()
 }
