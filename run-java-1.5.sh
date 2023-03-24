1873fec8217dfe4f3898a602d92f7186c2ef3203
EDragoev <edragoev@protonmail.com>
2023-03-24 11:54:00 -0400

    Updated so that the classes are not created next to the .java files

--- run-java-1.5.sh
+++ run-java-1.5.sh
@@ -3,7 +3,8 @@
 /opt/jdk1.5.0_22/bin/jar cf PDFjet.jar -C out/production .
 
 # Compile and run the Example_?? program.
-/opt/jdk1.5.0_22/bin/javac -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_$1.java
+/opt/jdk1.5.0_22/bin/javac -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_$1.java -d out/production
 /opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_$1
 
-mupdf Example_$1.pdf
+# mupdf Example_$1.pdf
+evince Example_$1.pdf
