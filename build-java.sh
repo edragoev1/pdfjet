7a25abb3103ebbd954762cf1565d0f24ba0affee
EDragoev <edragoev@protonmail.com>
2023-03-24 12:12:49 -0400

    Improved the script

--- build-java.sh
+++ build-java.sh
@@ -1,57 +1,57 @@
 javac -O -encoding utf-8 -Xlint com/pdfjet/*.java -d out/production
 jar --create --file PDFjet.jar -C out/production .
 
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_01.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_02.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_03.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_04.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_05.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_06.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_07.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_08.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_09.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_10.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_11.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_12.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_13.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_14.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_15.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_16.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_17.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_18.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_19.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_20.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_21.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_22.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_23.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_24.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_25.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_26.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_27.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_28.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_29.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_30.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_31.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_32.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_33.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_34.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_35.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_36.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_37.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_38.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_39.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_40.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_41.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_42.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_43.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_44.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_45.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_46.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_47.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_48.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_49.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_50.java
-javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_51.java
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_01.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_02.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_03.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_04.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_05.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_06.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_07.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_08.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_09.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_10.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_11.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_12.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_13.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_14.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_15.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_16.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_17.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_18.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_19.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_20.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_21.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_22.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_23.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_24.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_25.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_26.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_27.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_28.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_29.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_30.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_31.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_32.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_33.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_34.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_35.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_36.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_37.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_38.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_39.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_40.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_41.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_42.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_43.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_44.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_45.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_46.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_47.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_48.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_49.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_50.java -d out/production
+javac -O -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_51.java -d out/production
 
 java -cp .:PDFjet.jar examples.Example_01
 java -cp .:PDFjet.jar examples.Example_02
