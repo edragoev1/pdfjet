a8ddad7567919acf6e95a924662215574da37c69
EDragoev <edragoev@protonmail.com>
2023-03-23 23:19:28 -0400

    Improved.

--- run-swift.sh
+++ run-swift.sh
@@ -1,5 +1,5 @@
 # swift run --configuration release Example_$1
 swift run --configuration debug Example_$1
 
-# evince Example_$1.pdf
-mupdf Example_$1.pdf
+# mupdf Example_$1.pdf
+evince Example_$1.pdf
