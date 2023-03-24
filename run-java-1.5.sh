65e903db54b3fe7d768b6e5c03f821db8fda5255
EDragoev <edragoev@protonmail.com>
2023-03-24 14:40:59 -0400

    Improved the script

--- run-java-1.5.sh
+++ run-java-1.5.sh
@@ -1,10 +1,13 @@
+rm out/production/com/pdfjet/*.class
+rm out/production/examples/*.class
+
 # Compile and package the library.
 /opt/jdk1.5.0_22/bin/javac -O -encoding utf-8 -Xlint com/pdfjet/*.java -d out/production
 /opt/jdk1.5.0_22/bin/jar cf PDFjet.jar -C out/production .
 
 # Compile and run the Example_?? program.
 /opt/jdk1.5.0_22/bin/javac -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_$1.java -d out/production
-/opt/jdk1.5.0_22/bin/java -cp .:PDFjet.jar examples.Example_$1
+/opt/jdk1.5.0_22/bin/java -cp PDFjet.jar:out/production examples.Example_$1
 
 # mupdf Example_$1.pdf
 evince Example_$1.pdf
