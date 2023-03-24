2c6e2bd48f79442b1077562c3c4d0f16bd16505c
EDragoev <edragoev@protonmail.com>
2023-03-24 01:09:54 -0400

    Finished the example.

--- src/examples/example28/main.go
+++ src/examples/example28/main.go
@@ -5,137 +5,153 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	pdfjet "github.com/edragoev1/pdfjet/src"
-	"github.com/edragoev1/pdfjet/src/color"
 	"github.com/edragoev1/pdfjet/src/compliance"
-	"github.com/edragoev1/pdfjet/src/corefont"
-	"github.com/edragoev1/pdfjet/src/imagetype"
 	"github.com/edragoev1/pdfjet/src/letter"
 )
 
-// Example28 draws the Canadian flag using a Path object that contains both lines
-// and curve segments. Every curve segment must have exactly 2 control points.
 func Example28() {
 	f, err := os.Create("Example_28.pdf")
 	if err != nil {
 		log.Fatal(err)
 	}
 	w := bufio.NewWriter(f)
-
 	pdf := pdfjet.NewPDF(w, compliance.PDF15)
 
-	font := pdfjet.NewCoreFont(pdf, corefont.Helvetica())
-
-	f, err = os.Open("fonts/Droid/DroidSerif-Regular.ttf.stream")
+	f, err = os.Open("fonts/Droid/DroidSans.ttf.stream")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 	reader := bufio.NewReader(f)
-	font2 := pdfjet.NewFontStream1(pdf, reader)
+	f1 := pdfjet.NewFontStream1(pdf, reader)
 
-	f, err = os.Open("fonts/Droid/DroidSansMono.ttf.stream")
+	f, err = os.Open("fonts/Droid/DroidSansFallback.ttf.stream")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 	reader = bufio.NewReader(f)
-	font3 := pdfjet.NewFontStream1(pdf, reader)
+	f2 := pdfjet.NewFontStream1(pdf, reader)
 
-	f, err = os.Open("images/ee-map.png")
+	f, err = os.Open("fonts/Noto/NotoSansSymbols-Regular-Subsetted.ttf.stream")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 	reader = bufio.NewReader(f)
-	image := pdfjet.NewImage(pdf, reader, imagetype.PNG)
-	f.Close()
+	f3 := pdfjet.NewFontStream1(pdf, reader)
 
-	page := pdfjet.NewPageAddTo(pdf, letter.Portrait)
+	page := pdfjet.NewPageAddTo(pdf, letter.Landscape)
 
-	flag := pdfjet.NewBoxAt(85.0, 85.0, 64.0, 32.0)
+	f, err = os.Open("data/report.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-	path := pdfjet.NewPath()
+	lines := make([]string, 0)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-	path.Add(pdfjet.NewPoint(13.0, 0.0))
-	path.Add(pdfjet.NewPoint(15.5, 4.5))
+	var y float32 = 40.0
+	for _, line := range lines {
+		textLine := pdfjet.NewTextLine(f1, line)
+		textLine.SetFallbackFont(f2)
+		textLine.SetLocation(50, y)
+		textLine.DrawOn(page)
+		y += 20.0
+	}
 
-	path.Add(pdfjet.NewPoint(18.0, 3.5))
-	path.Add(pdfjet.NewControlPoint(15.5, 13.5))
-	path.Add(pdfjet.NewControlPoint(15.5, 13.5))
-	path.Add(pdfjet.NewPoint(20.5, 7.5))
+	var x float32 = 50.0
+	y = 210.0
+	var dy float32 = 22.0
 
-	path.Add(pdfjet.NewPoint(21.0, 9.5))
-	path.Add(pdfjet.NewPoint(25.0, 9.0))
-	path.Add(pdfjet.NewPoint(24.0, 13.0))
-	path.Add(pdfjet.NewPoint(25.5, 14.0))
-	path.Add(pdfjet.NewPoint(19.0, 19.0))
-	path.Add(pdfjet.NewPoint(20.0, 21.5))
-	path.Add(pdfjet.NewPoint(13.5, 20.5))
-	path.Add(pdfjet.NewPoint(13.5, 27.0))
-	path.Add(pdfjet.NewPoint(12.5, 27.0))
-	path.Add(pdfjet.NewPoint(12.5, 20.5))
-	path.Add(pdfjet.NewPoint(6.0, 21.5))
-	path.Add(pdfjet.NewPoint(7.0, 19.0))
-	path.Add(pdfjet.NewPoint(0.5, 14.0))
-	path.Add(pdfjet.NewPoint(2.0, 13.0))
-	path.Add(pdfjet.NewPoint(1.0, 9.0))
-	path.Add(pdfjet.NewPoint(5.0, 9.5))
+	var buf strings.Builder
+	var text = pdfjet.NewTextLine(f3, buf.String())
+	var count = 0
+	for i := 0x2200; i <= 0x22FF; i++ {
+		// Draw the Math Symbols
+		if count%80 == 0 {
+			text.SetText(buf.String())
+			text.SetLocation(x, y)
+			text.DrawOn(page)
+			buf.Reset()
+			y += dy
+		}
+		buf.WriteRune(rune(i))
+		count++
+	}
+	text.SetText(buf.String())
+	text.SetLocation(x, y)
+	text.DrawOn(page)
+	buf.Reset()
+	y += dy
 
-	path.Add(pdfjet.NewPoint(5.5, 7.5))
-	path.Add(pdfjet.NewControlPoint(10.5, 13.5))
-	path.Add(pdfjet.NewControlPoint(10.5, 13.5))
-	path.Add(pdfjet.NewPoint(8.0, 3.5))
+	count = 0
+	for i := 0x25A0; i <= 0x25FF; i++ {
+		// Draw the Geometric Shapes
+		if count%80 == 0 {
+			text.SetText(buf.String())
+			text.SetLocation(x, y)
+			text.DrawOn(page)
+			buf.Reset()
+			y += dy
+		}
+		buf.WriteRune(rune(i))
+		count++
+	}
+	text.SetText(buf.String())
+	text.SetLocation(x, y)
+	text.DrawOn(page)
+	buf.Reset()
+	y += dy
 
-	path.Add(pdfjet.NewPoint(10.5, 4.5))
-	path.SetClosePath(true)
-	path.SetColor(color.Red)
-	path.SetFillShape(true)
-	path.PlaceIn(flag, 19.0, 3.0)
+	count = 0
+	for i := 0x2701; i <= 0x27ff; i++ {
+		// Draw the Dingbats
+		if count%80 == 0 {
+			text.SetText(buf.String())
+			text.SetLocation(x, y)
+			text.DrawOn(page)
+			buf.Reset()
+			y += dy
+		}
+		buf.WriteRune(rune(i))
+		count++
+	}
+	text.SetText(buf.String())
+	text.SetLocation(x, y)
+	text.DrawOn(page)
+	y += dy
+	buf.Reset()
 
-	path.DrawOn(page)
-
-	box := pdfjet.NewBox()
-	box.SetSize(16.0, 32.0)
-	box.SetColor(color.Red)
-	box.SetFillShape(true)
-	box.PlaceIn(flag, 0.0, 0.0)
-	box.DrawOn(page)
-	box.PlaceIn(flag, 48.0, 0.0)
-	box.DrawOn(page)
-
-	path.ScaleBy(15.0)
-	path.SetFillShape(false)
-	xy := path.DrawOn(page)
-
-	box = pdfjet.NewBox()
-	box.SetLocation(xy[0], xy[1])
-	box.SetSize(20.0, 20.0)
-	box.DrawOn(page)
-
-	font.SetSize(24.0)
-	textField := pdfjet.NewTextLine(font, "Hello, World!")
-	textField.SetLocation(300.0, 300.0)
-	textField.SetColor(color.Blanchedalmond)
-	textField.DrawOn(page)
-
-	font2.SetSize(24.0)
-	textField2 := pdfjet.NewTextLine(font2, "This is great!")
-	textField2.SetLocation(400.0, 400.0)
-	textField2.SetColor(color.Blue)
-	textField2.SetStrikeout(true)
-	textField2.SetUnderline(true)
-	textField2.DrawOn(page)
-
-	font2.SetSize(14.0)
-	textField2 = pdfjet.NewTextLine(font2, "This is great!")
-	textField2.SetLocation(400.0, 500.0)
-	textField2.SetColor(color.Blue)
-	textField2.DrawOn(page)
-
-	font3.SetSize(24.0)
-	textField2 = pdfjet.NewTextLine(font3, "This is great!")
-	textField2.SetLocation(400.0, 600.0)
-	textField2.SetColor(color.Blue)
-	textField2.DrawOn(page)
-
-	image.SetLocation(100.0, 500.0)
-	image.ScaleBy(0.5)
-	image.DrawOn(page)
+	count = 0
+	for i := 0x2800; i <= 0x28FF; i++ {
+		// Draw the Braille Patterns
+		if count%80 == 0 {
+			text.SetText(buf.String())
+			text.SetLocation(x, y)
+			text.DrawOn(page)
+			buf.Reset()
+			y += dy
+		}
+		buf.WriteRune(rune(i))
+		count++
+	}
+	text.SetText(buf.String())
+	text.SetLocation(x, y)
+	text.DrawOn(page)
 
 	pdf.Complete()
-
-	f.Close()
 }
 
 func main() {
