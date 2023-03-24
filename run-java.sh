347363a67e61dce677d55f88c59849ea60afe989
EDragoev <edragoev@protonmail.com>
2023-03-24 12:13:26 -0400

    Improved the script

--- run-java.sh
+++ run-java.sh
@@ -3,7 +3,7 @@ javac -O -encoding utf-8 -Xlint com/pdfjet/*.java -d out/production
 jar cf PDFjet.jar -C out/production .
 
 # Compile and run the Example_?? program.
-javac -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_$1.java
+javac -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_$1.java -d out/production
 java -cp .:PDFjet.jar examples.Example_$1
 
 # mupdf Example_$1.pdf
