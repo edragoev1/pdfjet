5a99617f49b99122f6d663867eb66c69491eba2d
EDragoev <edragoev@protonmail.com>
2023-03-24 23:36:58 -0400

    Improved the example

--- examples/Example_01.java
+++ examples/Example_01.java
@@ -17,15 +17,18 @@ class Example_01 {
         PDF pdf = new PDF(
                 new BufferedOutputStream(new FileOutputStream("Example_01.pdf")));
 
-        FileInputStream stream = new FileInputStream("fonts/OpenSans/OpenSans-Regular.ttf.stream");
-        Font font1 = new Font(pdf, stream, Font.STREAM);
-        font1.setSize(12f);
-        stream.close();
+        Font font1 = new Font(
+                pdf,
+                new FileInputStream("fonts/Droid/DroidSans.ttf.stream"),
+                Font.STREAM);
 
-        stream = new FileInputStream("fonts/Droid/DroidSansFallback.ttf.stream");
-        Font font2 = new Font(pdf, stream, Font.STREAM);
+        Font font2 = new Font(
+                pdf,
+                new FileInputStream("fonts/Droid/DroidSansFallback.ttf.stream"),
+                Font.STREAM);
+
+        font1.setSize(12f);
         font2.setSize(12f);
-        stream.close();
 
         Page page = new Page(pdf, Letter.PORTRAIT);
 
