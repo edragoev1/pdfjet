1ee3c219abcbfd6fd347039bf485613e004e89b8
EDragoev <edragoev@protonmail.com>
2023-03-26 19:32:44 -0400

    Removed from the project

--- tests/run-mono.sh
+++ tests/run-mono.sh
@@ -1,3 +1,3 @@
-mcs -debug -sdk:4 Example_$1.cs -reference:../PDFjet.dll
-mono --debug Example_$1.exe
-evince Example_$1.pdf
+mcs -debug -sdk:4 Test_$1.cs -reference:../PDFjet.dll
+mono --debug Test_$1.exe
+evince Test_$1.pdf
