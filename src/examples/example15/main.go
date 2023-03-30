7a42688d0c90c6e88288eeee5d41b679363c961f
EDragoev <edragoev@protonmail.com>
2023-03-29 20:20:55 -0400

    Added more code to the examples

--- src/examples/example15/main.go
+++ src/examples/example15/main.go
@@ -7,113 +7,40 @@ import (
 	pdfjet "github.com/edragoev1/pdfjet/src"
 	"github.com/edragoev1/pdfjet/src/color"
 	"github.com/edragoev1/pdfjet/src/compliance"
-	"github.com/edragoev1/pdfjet/src/corefont"
-	"github.com/edragoev1/pdfjet/src/imagetype"
 	"github.com/edragoev1/pdfjet/src/letter"
 )
 
-// Example15 draws the Canadian flag using a Path object that contains both lines
-// and curve segments. Every curve segment must have exactly 2 control points.
 func Example15() {
 	pdf := pdfjet.NewPDFFile("Example_15.pdf", compliance.PDF15)
 
-	font1 := pdfjet.NewCoreFont(pdf, corefont.Helvetica())
-	font2 := pdfjet.NewFontFromFile(pdf, "fonts/Droid/DroidSerif-Regular.ttf.stream")
-	font3 := pdfjet.NewFontFromFile(pdf, "fonts/Droid/DroidSansMono.ttf.stream")
+	f1 := pdfjet.NewFontFromFile(pdf, "fonts/Droid/DroidSans.ttf.stream")
+	f2 := pdfjet.NewFontFromFile(pdf, "fonts/Droid/DroidSansFallback.ttf.stream")
 
-	image := pdfjet.NewImageFromFile(pdf, "images/ee-map.png", imagetype.PNG)
+	f1.SetSize(12.0)
+	f2.SetSize(12.0)
 
 	page := pdfjet.NewPageAddTo(pdf, letter.Portrait)
 
-	flag := pdfjet.NewBoxAt(85.0, 85.0, 64.0, 32.0)
+	colors := make(map[string]int32)
+	colors["Lorem"] = color.Blue
+	colors["ipsum"] = color.Red
+	colors["dolor"] = color.Green
+	colors["ullamcorper"] = color.Gray
 
-	path := pdfjet.NewPath()
-
-	path.Add(pdfjet.NewPoint(13.0, 0.0))
-	path.Add(pdfjet.NewPoint(15.5, 4.5))
-
-	path.Add(pdfjet.NewPoint(18.0, 3.5))
-	path.Add(pdfjet.NewControlPoint(15.5, 13.5))
-	path.Add(pdfjet.NewControlPoint(15.5, 13.5))
-	path.Add(pdfjet.NewPoint(20.5, 7.5))
-
-	path.Add(pdfjet.NewPoint(21.0, 9.5))
-	path.Add(pdfjet.NewPoint(25.0, 9.0))
-	path.Add(pdfjet.NewPoint(24.0, 13.0))
-	path.Add(pdfjet.NewPoint(25.5, 14.0))
-	path.Add(pdfjet.NewPoint(19.0, 19.0))
-	path.Add(pdfjet.NewPoint(20.0, 21.5))
-	path.Add(pdfjet.NewPoint(13.5, 20.5))
-	path.Add(pdfjet.NewPoint(13.5, 27.0))
-	path.Add(pdfjet.NewPoint(12.5, 27.0))
-	path.Add(pdfjet.NewPoint(12.5, 20.5))
-	path.Add(pdfjet.NewPoint(6.0, 21.5))
-	path.Add(pdfjet.NewPoint(7.0, 19.0))
-	path.Add(pdfjet.NewPoint(0.5, 14.0))
-	path.Add(pdfjet.NewPoint(2.0, 13.0))
-	path.Add(pdfjet.NewPoint(1.0, 9.0))
-	path.Add(pdfjet.NewPoint(5.0, 9.5))
-
-	path.Add(pdfjet.NewPoint(5.5, 7.5))
-	path.Add(pdfjet.NewControlPoint(10.5, 13.5))
-	path.Add(pdfjet.NewControlPoint(10.5, 13.5))
-	path.Add(pdfjet.NewPoint(8.0, 3.5))
-
-	path.Add(pdfjet.NewPoint(10.5, 4.5))
-	path.SetClosePath(true)
-	path.SetColor(color.Red)
-	path.SetFillShape(true)
-	path.PlaceIn(flag, 19.0, 3.0)
-
-	path.DrawOn(page)
+	textBox := pdfjet.NewTextBox(f1)
+	textBox.SetFallbackFont(f2)
+	textBox.SetText("保健所によると、女性は１３日に旅行先のタイから札幌に戻り、１６日午後５～８時ごろ同店を訪れ、帰宅後に発熱などの症状 が出て、２３日に医療機関ではしかと診断された。はしかのウイルスは発症日の１日前から感染者の呼吸などから放出され、本人がいなくなっても、２時間>程度空気中に漂い、空気感染する。保健所は１６日午後５～１１時に同店を訪れた人に、発熱などの異常が出た場合、早期にマスクをして医療機関を受診す>るよう呼びかけている。（本郷由美子）")
+	textBox.SetLocation(50.0, 50.0)
+	textBox.SetMargin(20.0)
+	textBox.SetWidth(300.0)
+	textBox.SetBgColor(color.Lightblue)
+	textBox.SetTextColors(colors)
+	xy := textBox.DrawOn(page)
 
 	box := pdfjet.NewBox()
-	box.SetSize(16.0, 32.0)
-	box.SetColor(color.Red)
-	box.SetFillShape(true)
-	box.PlaceIn(flag, 0.0, 0.0)
-	box.DrawOn(page)
-	box.PlaceIn(flag, 48.0, 0.0)
-	box.DrawOn(page)
-
-	path.ScaleBy(15.0)
-	path.SetFillShape(false)
-	xy := path.DrawOn(page)
-
-	box = pdfjet.NewBox()
 	box.SetLocation(xy[0], xy[1])
 	box.SetSize(20.0, 20.0)
 	box.DrawOn(page)
-
-	font1.SetSize(24.0)
-	textField := pdfjet.NewTextLine(font1, "Hello, World!")
-	textField.SetLocation(300.0, 300.0)
-	textField.SetColor(color.Blanchedalmond)
-	textField.DrawOn(page)
-
-	font2.SetSize(24.0)
-	textField2 := pdfjet.NewTextLine(font2, "This is great!")
-	textField2.SetLocation(400.0, 400.0)
-	textField2.SetColor(color.Blue)
-	textField2.SetStrikeout(true)
-	textField2.SetUnderline(true)
-	textField2.DrawOn(page)
-
-	font2.SetSize(14.0)
-	textField2 = pdfjet.NewTextLine(font2, "This is great!")
-	textField2.SetLocation(400.0, 500.0)
-	textField2.SetColor(color.Blue)
-	textField2.DrawOn(page)
-
-	font3.SetSize(24.0)
-	textField2 = pdfjet.NewTextLine(font3, "This is great!")
-	textField2.SetLocation(400.0, 600.0)
-	textField2.SetColor(color.Blue)
-	textField2.DrawOn(page)
-
-	image.SetLocation(100.0, 500.0)
-	image.ScaleBy(0.5)
-	image.DrawOn(page)
 
 	pdf.Complete()
 }
