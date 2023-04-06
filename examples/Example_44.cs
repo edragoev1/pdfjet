0f97c8e1ba8d84552512fed7358e3f0b6a8fcb9f
EDragoev <edragoev@protonmail.com>
2023-04-05 23:49:54 -0400

    Fixed small issue

--- examples/Example_44.cs
+++ examples/Example_44.cs
@@ -1,31 +1,26 @@
 using System;
 using System.IO;
 using System.Diagnostics;
-
 using PDFjet.NET;
-
 
 /**
  *  Example_44.cs
- *
  */
 public class Example_44 {
-
     public Example_44() {
-
-        int rotate = 0;
-
         PDF pdf = new PDF(new BufferedStream(
                 new FileStream("Example_44.pdf", FileMode.Create)));
 
         Font f1 = new Font(pdf, CoreFont.HELVETICA);
-        f1.SetSize(12f);
+        // Chinese (Simplified) font
+        Font f2 = new Font(pdf, CJKFont.ST_HEITI_SC_LIGHT);
 
-        Font f2 = new Font(pdf, "STHeitiSC-Light"); // Chinese (Simplified) font
+        f1.SetSize(12f);
         f2.SetSize(12f);
 
         Page page = new Page(pdf, Letter.PORTRAIT);
 
+        int rotate = 0;
         TextColumn column = new TextColumn(rotate);
         column.SetLocation(70f, 70f);
         column.SetWidth(500f);
@@ -39,12 +34,10 @@ public class Example_44 {
 
         String chinese = "用于附属装置选择器应用程序的分析方法和计算基于与物料、附属装置和机器有关的不同参数和变量。 此类参数和变量的准确性会影响附属装置选择 应用程序结果的准确性。 虽然沃尔沃建筑设备公司、其子公司和附属公司（统称“沃尔沃建筑设备”）已尽力确保附属装置选择应用程序的准确性，但沃尔沃建筑设备无法保证结果准确无误。 因此，沃尔沃建筑设备并未对附属装置选择应用程序结果的准确性作出保证或声明（无论明示或暗示）。 沃尔沃建筑设备特此声明不对有关附属装置选择应用程序报告，此处获得的结果，或用于附属装置选择应用程序的设备的适宜性的所有明示或暗示保证，包括但不限于用于特定目的的适销性或合适性的任何保证承担任何责任。 在任何情况下，沃尔沃建筑设备均不对为实体或由实体创建的结果（或任何其它实体）以及由此造成的任何间接、意外、必然或特殊损害，包括但不限于损失收益或利润承担任何责任。";
         column.AddChineseParagraph(f2, chinese);
-
         column.DrawOn(page);
 
         pdf.Complete();
     }
-
 
     public static void Main(String[] args) {
         Stopwatch sw = Stopwatch.StartNew();
@@ -54,5 +47,4 @@ public class Example_44 {
         sw.Stop();
         Console.WriteLine("Example_44 => " + (time1 - time0));
     }
-
 }   // End of Example_44.cs
