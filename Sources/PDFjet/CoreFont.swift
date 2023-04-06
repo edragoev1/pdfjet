069fb05b79702c99430920022060e6839a5a5ca4
EDragoev <edragoev@protonmail.com>
2023-04-05 22:37:12 -0400

    Switched to using 4 spaces per tab

--- Sources/PDFjet/CoreFont.swift
+++ Sources/PDFjet/CoreFont.swift
@@ -27,18 +27,18 @@ SOFTWARE.
  *  See the Font class for more details.
  */
 public enum CoreFont {
-        case COURIER
-        case COURIER_BOLD
-        case COURIER_OBLIQUE
-        case COURIER_BOLD_OBLIQUE
-        case HELVETICA
-        case HELVETICA_BOLD
-        case HELVETICA_OBLIQUE
-        case HELVETICA_BOLD_OBLIQUE
-        case TIMES_ROMAN
-        case TIMES_BOLD
-        case TIMES_ITALIC
-        case TIMES_BOLD_ITALIC
-        case SYMBOL
-        case ZAPF_DINGBATS
+    case COURIER
+    case COURIER_BOLD
+    case COURIER_OBLIQUE
+    case COURIER_BOLD_OBLIQUE
+    case HELVETICA
+    case HELVETICA_BOLD
+    case HELVETICA_OBLIQUE
+    case HELVETICA_BOLD_OBLIQUE
+    case TIMES_ROMAN
+    case TIMES_BOLD
+    case TIMES_ITALIC
+    case TIMES_BOLD_ITALIC
+    case SYMBOL
+    case ZAPF_DINGBATS
 }
