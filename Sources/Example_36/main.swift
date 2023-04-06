58222dc183a03cd9da265a1ecdb0bd6f679da7df
EDragoev <edragoev@protonmail.com>
2023-04-06 14:34:42 -0400

    Improved the API

--- Sources/Example_36/main.swift
+++ Sources/Example_36/main.swift
@@ -1,93 +1,73 @@
 import Foundation
 import PDFjet
 
-
 /**
  *  Example_36.swift
- *
  */
 public class Example_36 {
-
     public init() throws {
+        let pdf = PDF(OutputStream(toFileAtPath: "Example_36.pdf", append: false)!)
+        let f1 = Font(pdf, CoreFont.HELVETICA)
 
-        if let stream = OutputStream(toFileAtPath: "Example_36.pdf", append: false) {
+        let image1 = try Image(pdf, "images/ee-map.png")
+        let image2 = try Image(pdf, "images/fruit.jpg")
+        let image3 = try Image(pdf, "images/mt-map.bmp")
+        // let image3 = try Image(pdf, "images/palette.bmp")
 
-            let pdf = PDF(stream)
+        let page1 = Page(pdf, A4.PORTRAIT, Page.DETACHED)
 
-            let page1 = Page(pdf, A4.PORTRAIT, false)
+        let text = TextLine(f1,
+                "The map below is an embedded PNG image")
+        text.setLocation(90.0, 30.0)
+        text.drawOn(page1)
 
-            let f1 = Font(pdf, CoreFont.HELVETICA)
+        image1.setLocation(90.0, 40.0)
+        image1.scaleBy(2.0/3.0)
+        image1.drawOn(page1)
 
-            let image1 = try Image(
-                    pdf,
-                    InputStream(fileAtPath: "images/ee-map.png")!,
-                    ImageType.PNG)
+        text.setText(
+                "JPG image file embedded once and drawn 3 times")
+        text.setLocation(90.0, 550.0)
+        text.drawOn(page1)
 
-            let image2 = try Image(
-                    pdf,
-                    InputStream(fileAtPath: "images/fruit.jpg")!,
-                    ImageType.JPG)
+        image2.setLocation(90.0, 560.0)
+        image2.scaleBy(0.5)
+        image2.drawOn(page1)
 
-            let image3 = try Image(
-                    pdf,
-                    // InputStream(fileAtPath: "images/mt-map.bmp")!,
-                    InputStream(fileAtPath: "images/palette.bmp")!,
-                    ImageType.BMP)
+        image2.setLocation(260.0, 560.0)
+        image2.scaleBy(0.5)
+        image2.setRotateCW90(true)
+        image2.drawOn(page1)
 
-            let text = TextLine(f1,
-                    "The map below is an embedded PNG image")
-            text.setLocation(90.0, 30.0)
-            text.drawOn(page1)
+        image2.setLocation(350.0, 560.0)
+        image2.setRotateCW90(false)
+        image2.scaleBy(0.5)
+        image2.drawOn(page1)
 
-            image1.setLocation(90.0, 40.0)
-            image1.scaleBy(2.0/3.0)
-            image1.drawOn(page1)
+        image3.setLocation(390.0, 630.0)
+        image3.scaleBy(0.5)
+        image3.drawOn(page1)
 
-            text.setText(
-                    "JPG image file embedded once and drawn 3 times")
-            text.setLocation(90.0, 550.0)
-            text.drawOn(page1)
+        let page2 = Page(pdf, A4.PORTRAIT, Page.DETACHED)
+        image1.drawOn(page2)
 
-            image2.setLocation(90.0, 560.0)
-            image2.scaleBy(0.5)
-            image2.drawOn(page1)
+        text.setText("Hello, World!!")
+        text.setLocation(90.0, 800.0)
+        text.drawOn(page2)
 
-            image2.setLocation(260.0, 560.0)
-            image2.scaleBy(0.5)
-            image2.setRotateCW90(true)
-            image2.drawOn(page1)
+        text.setText(
+                "The map on the right is an embedded BMP image")
+        text.setUnderline(true)
+        text.setStrikeout(true)
+        text.setTextDirection(15)
+        text.setLocation(90.0, 800.0)
+        text.drawOn(page1)
 
-            image2.setLocation(350.0, 560.0)
-            image2.setRotateCW90(false)
-            image2.scaleBy(0.5)
-            image2.drawOn(page1)
+        pdf.addPage(page2)
+        pdf.addPage(page1)
 
-            image3.setLocation(390.0, 630.0)
-            image3.scaleBy(0.5)
-            image3.drawOn(page1)
-
-            let page2 = Page(pdf, A4.PORTRAIT, false)
-            image1.drawOn(page2)
-
-            text.setText("Hello, World!!")
-            text.setLocation(90.0, 800.0)
-            text.drawOn(page2)
-
-            text.setText(
-                    "The map on the right is an embedded BMP image")
-            text.setUnderline(true)
-            text.setStrikeout(true)
-            text.setTextDirection(15)
-            text.setLocation(90.0, 800.0)
-            text.drawOn(page1)
-
-            pdf.addPage(page2)
-            pdf.addPage(page1)
-
-            pdf.complete()
-        }
+        pdf.complete()
     }
-
 }   // End of Example_36.swift
 
 let time0 = Int64(Date().timeIntervalSince1970 * 1000)
