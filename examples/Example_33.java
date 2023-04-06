6572109892196ec27f4ded50e976cbea10730e01
EDragoev <edragoev@protonmail.com>
2023-04-06 00:45:13 -0400

    Updated the example

--- examples/Example_33.java
+++ examples/Example_33.java
@@ -20,68 +20,49 @@ public class Example_33 {
         image.scaleBy(0.25f);
         image.drawOn(page);
 
-        stream = new FileInputStream(
+        SVGImage icon = new SVGImage(
             "images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg");
-        SVGImage icon = new SVGImage(stream);
         icon.setLocation(20f, 670f);
         float[] xy = icon.drawOn(page);
 
-        stream = new FileInputStream(
-            "images/svg/add_circle_FILL0_wght400_GRAD0_opsz48.svg");
-        icon = new SVGImage(stream);
+        icon = new SVGImage("images/svg/add_circle_FILL0_wght400_GRAD0_opsz48.svg");
         icon.setLocation(xy[0], 670f);
         xy = icon.drawOn(page);
 
-        stream = new FileInputStream(
-            "images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg");
-        icon = new SVGImage(stream);
+        icon = new SVGImage("images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg");
         icon.setLocation(xy[0], 670f);
         xy = icon.drawOn(page);
 
-        stream = new FileInputStream(
-            "images/svg/auto_stories_FILL0_wght400_GRAD0_opsz48.svg");
-        icon = new SVGImage(stream);
+        icon = new SVGImage("images/svg/auto_stories_FILL0_wght400_GRAD0_opsz48.svg");
         icon.setLocation(xy[0], 670f);
         xy = icon.drawOn(page);
 
-        stream = new FileInputStream(
-            "images/svg/star_FILL0_wght400_GRAD0_opsz48.svg");
-        icon = new SVGImage(stream);
+        icon = new SVGImage("images/svg/star_FILL0_wght400_GRAD0_opsz48.svg");
         icon.setLocation(xy[0], 670);
         xy = icon.drawOn(page);
 
-        stream = new FileInputStream(
-            "images/svg-test/test-CS.svg");
-        icon = new SVGImage(stream);
+        icon = new SVGImage("images/svg-test/test-CS.svg");
         icon.setLocation(xy[0], 670);
         xy = icon.drawOn(page);
 
-        stream = new FileInputStream(
-            "images/svg-test/test-QQ.svg");
-        icon = new SVGImage(stream);
+        icon = new SVGImage("images/svg-test/test-QQ.svg");
         icon.setLocation(xy[0], 670);
         xy = icon.drawOn(page);
 
-        stream = new FileInputStream(
-            "images/svg-test/menu-icon.svg");
-        icon = new SVGImage(stream);
+        icon = new SVGImage("images/svg-test/menu-icon.svg");
         icon.setLocation(xy[0], 670);
         xy = icon.drawOn(page);
 
-        stream = new FileInputStream(
-            "images/svg-test/menu-icon-close.svg");
-        icon = new SVGImage(stream);
+        icon = new SVGImage("images/svg-test/menu-icon-close.svg");
         icon.setLocation(xy[0], 670);
         xy = icon.drawOn(page);
 
-        stream = new FileInputStream("images/svg-test/europe.svg");
-        icon = new SVGImage(stream);
+        icon = new SVGImage("images/svg-test/europe.svg");
         icon.setLocation(0f, 0f);
         icon.drawOn(page);
 
         pdf.complete();
     }
-
 
     public static void main(String[] args) throws Exception {
         long t0 = System.currentTimeMillis();
@@ -89,5 +70,4 @@ public class Example_33 {
         long t1 = System.currentTimeMillis();
         System.out.println("Example_33 => " + (t1 - t0));
     }
-
 }   // End of Example_33.java
