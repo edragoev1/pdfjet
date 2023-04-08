75a4c07621c1ae72d114b59a5aaf539c3005772d
EDragoev <edragoev@protonmail.com>
2023-04-08 11:55:35 -0400

    Added more code.

---
+++ Sources/PDFjet/Content.swift
@@ -0,0 +1,45 @@
+/**
+ *  Content.swift
+ *
+Copyright 2023 Innovatics Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+import Foundation
+
+///
+///
+///
+public class Content {
+    public static func ofTextFile( _ fileName: String) -> String {
+        var buffer = ""
+        var buf = [UInt8](repeating: 0, count: 4096)
+        let stream = InputStream(fileAtPath: fileName)!
+        stream.read(&buf, maxLength: buf.count)
+        stream.close()
+        return buffer
+    }
+
+
+        //     var buf = [UInt8](repeating: 0, count: length)
+        // if stream.read(&buf, maxLength: buf.count) == buf.count {
+        //     return buf
+        // }
+
+}   // End of Content.swift
