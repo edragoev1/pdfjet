028c8fcc41f6075846c34dd13dc4020e60e96507
EDragoev <edragoev@protonmail.com>
2023-04-08 20:53:52 -0400

    Nice cleanup.

--- Sources/Example_26/main.swift
+++ Sources/Example_26/main.swift
@@ -1,83 +1,74 @@
 import Foundation
 import PDFjet
 
-
 /**
  *  Example_26.swift
- *
  */
 public class Example_26 {
-
     public init() {
+        let pdf = PDF(OutputStream(toFileAtPath: "Example_26.pdf", append: false)!)
+        let f1 = Font(pdf, CoreFont.HELVETICA_BOLD)
+        let page = Page(pdf, Letter.PORTRAIT)
+        f1.setSize(10.0)
 
-        if let stream = OutputStream(toFileAtPath: "Example_26.pdf", append: false) {
+        let x: Float = 50.0
+        var y: Float = 50.0
 
-            let pdf = PDF(stream)
-            let page = Page(pdf, Letter.PORTRAIT)
+        CheckBox(f1, "Hello")
+                .setLocation(x, y)
+                .setCheckmark(Color.blue)
+                .check(Mark.CHECK)
+                .drawOn(page)
 
-            let f1 = Font(pdf, CoreFont.HELVETICA_BOLD)
-            f1.setSize(10.0)
+        y += 30.0
+        CheckBox(f1, "World!")
+                .setLocation(x, y)
+                .setCheckmark(Color.blue)
+                .setURIAction("http://pdfjet.com")
+                .check(Mark.CHECK)
+                .drawOn(page)
 
-            let x: Float = 50.0
-            var y: Float = 50.0
+        y += 30.0
+        CheckBox(f1, "This is a test.")
+                .setLocation(x, y)
+                .setURIAction("http://pdfjet.com")
+                .drawOn(page)
 
-            CheckBox(f1, "Hello")
-                    .setLocation(x, y)
-                    .setCheckmark(Color.blue)
-                    .check(Mark.CHECK)
-                    .drawOn(page)
+        y += 30.0
+        RadioButton(f1, "Hello, World!")
+                .setLocation(x, y)
+                .select(true)
+                .drawOn(page)
 
-            y += 30.0
-            CheckBox(f1, "World!")
-                    .setLocation(x, y)
-                    .setCheckmark(Color.blue)
-                    .setURIAction("http://pdfjet.com")
-                    .check(Mark.CHECK)
-                    .drawOn(page)
+        var xy = (RadioButton(f1, "Yes"))
+                .setLocation(x + 100.0, 50.0)
+                .setURIAction("http://pdfjet.com")
+                .select(true)
+                .drawOn(page)
 
-            y += 30.0
-            CheckBox(f1, "This is a test.")
-                    .setLocation(x, y)
-                    .setURIAction("http://pdfjet.com")
-                    .drawOn(page)
+        xy = (RadioButton(f1, "No"))
+                .setLocation(xy[0], 50.0)
+                .drawOn(page)
 
-            y += 30.0
-            RadioButton(f1, "Hello, World!")
-                    .setLocation(x, y)
-                    .select(true)
-                    .drawOn(page)
+        xy = (CheckBox(f1, "Hello"))
+                .setLocation(xy[0], 50.0)
+                .setCheckmark(Color.blue)
+                .check(Mark.X)
+                .drawOn(page)
 
-            var xy = (RadioButton(f1, "Yes"))
-                    .setLocation(x + 100.0, 50.0)
-                    .setURIAction("http://pdfjet.com")
-                    .select(true)
-                    .drawOn(page)
+        xy = (CheckBox(f1, "Yahoo")
+                .setLocation(xy[0], 50.0)
+                .setCheckmark(Color.blue)
+                .check(Mark.CHECK)
+                .drawOn(page))
 
-            xy = (RadioButton(f1, "No"))
-                    .setLocation(xy[0], 50.0)
-                    .drawOn(page)
+        let box = Box()
+        box.setLocation(xy[0], xy[1])
+        box.setSize(20.0, 20.0)
+        box.drawOn(page)
 
-            xy = (CheckBox(f1, "Hello"))
-                    .setLocation(xy[0], 50.0)
-                    .setCheckmark(Color.blue)
-                    .check(Mark.X)
-                    .drawOn(page)
-
-            xy = (CheckBox(f1, "Yahoo")
-                    .setLocation(xy[0], 50.0)
-                    .setCheckmark(Color.blue)
-                    .check(Mark.CHECK)
-                    .drawOn(page))
-
-            let box = Box()
-            box.setLocation(xy[0], xy[1])
-            box.setSize(20.0, 20.0)
-            box.drawOn(page)
-
-            pdf.complete()
-        }
+        pdf.complete()
     }
-
 }   // End of Example_26.swift
 
 let time0 = Int64(Date().timeIntervalSince1970 * 1000)
