287c83e4c3262d3d68dcaab13c7ff91772a65f42
EDragoev <edragoev@protonmail.com>
2023-04-09 20:42:18 -0400

    First added to the project.

---
+++ com/pdfjet/Token.java
@@ -0,0 +1,9 @@
+package com.pdfjet;
+
+public class Token {
+    public static byte[] stream = "\nstream\n".getBytes();
+    public static byte[] endstream = "\nendstream\n".getBytes();
+    public static byte[] space = " ".getBytes();
+    public static byte[] newline = "\n".getBytes();
+    public static byte[] endobj = "endobj\n".getBytes();
+}
