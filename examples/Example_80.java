e93120e53da591c1898b72c267221618c9f19f0f
EDragoev <edragoev@protonmail.com>
2023-04-13 14:09:13 -0400

    Created executable version of the font optimizer

---
+++ examples/Example_80.java
@@ -0,0 +1,8 @@
+package examples;
+import com.pdfjet.OptimizeOTF;
+
+public class Example_80 {
+    public static void main(String[] args) throws Exception {
+        OptimizeOTF.convertFontFile(args[0]);
+    }
+}
