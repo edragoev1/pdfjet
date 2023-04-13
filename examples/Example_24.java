147bdb86e993e2767459186a5235cd24dd7e3728
EDragoev <edragoev@protonmail.com>
2023-04-13 14:45:31 -0400

    Improved the example and adde the zopflipng optimizer

--- examples/Example_24.java
+++ examples/Example_24.java
@@ -1,39 +1,24 @@
 package examples;
 
 import java.io.*;
-
 import com.pdfjet.*;
-
 
 /**
  *  Example_24.java
- *
  */
 public class Example_24 {
-
     public Example_24() throws Exception {
-
         PDF pdf = new PDF(
                 new BufferedOutputStream(
                         new FileOutputStream("Example_24.pdf")));
 
         Font font = new Font(pdf, CoreFont.HELVETICA);
 
-        FileInputStream stream = new FileInputStream("images/gr-map.jpg");
-        Image image_00 = new Image(pdf, stream, ImageType.JPG);
-        stream.close();
-
-        stream = new FileInputStream("images/linux-logo.png.stream");
-        Image image_01 = new Image(pdf, stream, ImageType.PNG_STREAM);
-        stream.close();
-
-        stream = new FileInputStream("images/ee-map.png");
-        Image image_02 = new Image(pdf, stream, ImageType.PNG);
-        stream.close();
-
-        stream = new FileInputStream("images/rgb24pal.bmp");
-        Image image_03 = new Image(pdf, stream, ImageType.BMP);
-        stream.close();
+        Image image_00 = new Image(pdf, "images/gr-map.jpg");
+        // Image image_01 = new Image(pdf, "images/linux-logo.png.stream");
+        Image image_01 = new Image(pdf, "images/linux-logo.png");
+        Image image_02 = new Image(pdf, "images/ee-map.png");
+        Image image_03 = new Image(pdf, "images/rgb24pal.bmp");
 
         Page page = new Page(pdf, Letter.PORTRAIT);
         TextLine textline_00 = new TextLine(font, "This is a JPEG image.");
