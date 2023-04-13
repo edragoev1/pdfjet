a8d45d6b9c33a72891b193c9e67815042ac9c8f9
EDragoev <edragoev@protonmail.com>
2023-04-13 09:56:51 -0400

    Improved the code

--- Sources/PDFjet/ZLibEncode.swift
+++ Sources/PDFjet/ZLibEncode.swift
@@ -27,8 +27,7 @@ public class ZLibEncode {
     private var bitBuffer: UInt32 = 0
     private var bitsInBuffer: UInt8 = 0
     private let BUFSIZE = 32768
-    //private let mask = 0x7FFF
-    private let mask = 0x1FFF
+    private let mask = 0x7FFF
     private var hashtable: [Int?]
 
     @discardableResult
@@ -85,28 +84,7 @@ public class ZLibEncode {
             _ input: [UInt8],
             _ i: Int,
             _ hashtable: inout [Int?]) -> Int? {
-        // // FNV-1a inline hash routine
-        // var hash: UInt32 = 2166136261
-        // let prime: UInt32 = 16777619
-        // hash ^= UInt32(input[i])
-        // hash = hash &* prime
-        // hash ^= UInt32(input[i + 1])
-        // hash = hash &* prime
-        // hash ^= UInt32(input[i + 2])
-        // hash = hash &* prime
-        // // Perform xor-folding operation
-        // let index = Int((hash >> 19) ^ hash) & mask
-        // if hashtable[index] != nil &&
-        //         i - hashtable[index]! <= 4096 &&
-        //         input[index] == input[i] &&
-        //         input[index + 1] == input[i + 1] &&
-        //         input[index + 2] == input[i + 2] {
-        //     return hashtable[index]
-        // }
-        // hashtable[index] = i
-
-
-
+        // FNV-1a inline hash routine
         var hash: UInt32 = 2166136261
         let prime: UInt32 = 16777619
         hash ^= UInt32(input[i])
@@ -118,7 +96,7 @@ public class ZLibEncode {
         // Perform xor-folding operation
         let index = Int((hash >> 19) ^ hash) & mask
         if hashtable[index] != nil &&
-                i - hashtable[index]! <= 4096 {
+                i - hashtable[index]! < BUFSIZE {
             let j = hashtable[index]!
             if input[j] == input[i] &&
                     input[j + 1] == input[i + 1] &&
@@ -127,7 +105,6 @@ public class ZLibEncode {
             }
         }
         hashtable[index] = i
-
         return nil
     }
 
