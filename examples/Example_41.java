0bc621a34b1c0278a7dee5783ef682060efedb08
EDragoev <edragoev@protonmail.com>
2023-04-14 11:59:08 -0400

    More code cleanup

--- examples/Example_41.java
+++ examples/Example_41.java
@@ -24,46 +24,46 @@ public class Example_41 {
         Page page = new Page(pdf, Letter.PORTRAIT);
 
         List<Paragraph> paragraphs = new ArrayList<Paragraph>();
+        Paragraph paragraph = new Paragraph()
+                .add(new TextLine(f1,
+"The small business centres offer practical resources, from step-by-step info on setting up your business to sample business plans to a range of business-related articles and books in our resource libraries.")
+                        .setUnderline(true))
+                .add(new TextLine(f2, "This text is bold!").setColor(Color.blue));
+        paragraphs.add(paragraph);
 
-//         Paragraph paragraph = new Paragraph()
-//                 .add(new TextLine(f1,
-// "The small business centres offer practical resources, from step-by-step info on setting up your business to sample business plans to a range of business-related articles and books in our resource libraries.")
-//                         .setUnderline(true))
-//                 .add(new TextLine(f2, "This text is bold!").setColor(Color.blue));
-//         paragraphs.add(paragraph);
+        paragraph = new Paragraph()
+                .add(new TextLine(f1,
+"The centres also offer free one-on-one consultations with business advisors who can review your business plan and make recommendations to improve it.")
+                        .setUnderline(true))
+                .add(new TextLine(f3, "This text is using italic font.").setColor(Color.green));
+        paragraphs.add(paragraph);
 
-//         paragraph = new Paragraph()
-//                 .add(new TextLine(f1,
-// "The centres also offer free one-on-one consultations with business advisors who can review your business plan and make recommendations to improve it.")
-//                         .setUnderline(true))
-//                 .add(new TextLine(f3, "This text is using italic font.").setColor(Color.green));
-//         paragraphs.add(paragraph);
+        Text text = new Text(paragraphs);
+        text.setLocation(70f, 50f);
+        text.setWidth(500f);
+        text.setBorder(true);
+        text.drawOn(page);
 
-//         Text text = new Text(paragraphs);
-//         text.setLocation(70f, 50f);
-//         text.setWidth(500f);
-//         text.setBorder(true);
-//         float[] xy = text.drawOn(page);
-
-//         List<float[]> beginParagraphPoints = text.getBeginParagraphPoints();
-//         int paragraphNumber = 1;
-//         for (int i = 0; i < beginParagraphPoints.size(); i++) {
-//             if (!paragraphs.get(i).getTextLines().get(0).getText().startsWith("**")) {
-//                 float[] point = beginParagraphPoints.get(i);
-//                 new TextLine(f2, String.valueOf(paragraphNumber) + ".")
-//                         .setLocation(point[0] - 15f, point[1])
-//                         .drawOn(page);
-//                 paragraphNumber++;
-//             }
-//         }
+        int paragraphNumber = 1;
+        for (Paragraph p : paragraphs) {
+            if (p.startsWith("**")) {
+                paragraphNumber = 1;
+            } else {
+                float[] point = p.beginParagraphPoint;
+                new TextLine(f2, String.valueOf(paragraphNumber) + ".")
+                        .setLocation(point[0] - 15f, point[1])
+                        .drawOn(page);
+                paragraphNumber++;
+            }
+        }
 
         paragraphs = Text.paragraphsFromFile(f1, "data/physics.txt");
-        Text text = new Text(paragraphs);
+        text = new Text(paragraphs);
         text.setLocation(70f, 150f);
         text.setWidth(500f);
         text.drawOn(page);
 
-        int paragraphNumber = 1;
+        paragraphNumber = 1;
         for (Paragraph p : paragraphs) {
             if (p.startsWith("**")) {
                 paragraphNumber = 1;
