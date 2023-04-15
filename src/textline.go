f0adec0e6ee39360a64a1a00ee9fa1dec0686fd0
EDragoev <edragoev@protonmail.com>
2023-04-14 20:41:00 -0400

    Fixed couple of issues

--- src/textline.go
+++ src/textline.go
@@ -407,7 +407,7 @@ func (textLine *TextLine) DrawOn(page *Page) []float32 {
 
 	page.SetBrushColor(textLine.color)
 	page.AddBMC(textLine.structureType, textLine.language, textLine.actualText, textLine.altDescription)
-	page.DrawString(textLine.font, textLine.fallbackFont, textLine.text, textLine.x, textLine.y)
+	page.DrawStringUsingColorMap(textLine.font, textLine.fallbackFont, textLine.text, textLine.x, textLine.y, textLine.color, textLine.colorMap)
 	page.AddEMC()
 
 	radians := float64(math.Pi) * float64(textLine.degrees) / float64(180.0)
