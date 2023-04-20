daf833f111ccc9155f14a15c8eaaa2fe7e65ef9e
EDragoev <edragoev@protonmail.com>
2023-04-20 15:45:02 -0400

    Nice reformatting changes.

--- src/examples/example33/main.go
+++ src/examples/example33/main.go
@@ -15,55 +15,55 @@ func Example33() {
 	page := pdfjet.NewPage(pdf, a4.Portrait)
 
 	// SVG test
-	image2 := pdfjet.NewSVGImageFromFile("images/svg-test/test-CC.svg")
-	image2.SetLocation(20.0, 670.0)
-	xy := image2.DrawOn(page)
+	image := pdfjet.NewSVGImageFromFile("images/svg-test/europe.svg")
+	image.SetLocation(-150.0, 0.0)
+	xy := image.DrawOn(page)
 
-	image2 = pdfjet.NewSVGImageFromFile("images/svg-test/test-QQ.svg")
-	image2.SetLocation(xy[0], 670.0)
-	xy = image2.DrawOn(page)
+	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-CC.svg")
+	image.SetLocation(20.0, 670.0)
+	xy = image.DrawOn(page)
 
-	image2 = pdfjet.NewSVGImageFromFile("images/svg-test/test-qt.svg")
-	image2.SetLocation(xy[0], 670.0)
-	xy = image2.DrawOn(page)
+	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-QQ.svg")
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
 
-	image2 = pdfjet.NewSVGImageFromFile("images/svg-test/test-qT.svg")
-	image2.SetLocation(xy[0], 670.0)
-	xy = image2.DrawOn(page)
+	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-qt.svg")
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
 
-	image2 = pdfjet.NewSVGImageFromFile("images/svg-test/test-QT.svg")
-	image2.SetLocation(xy[0], 670.0)
-	xy = image2.DrawOn(page)
+	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-qT.svg")
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
 
-	image2 = pdfjet.NewSVGImageFromFile("images/svg-test/test-qq.svg")
-	image2.SetLocation(xy[0], 670.0)
-	xy = image2.DrawOn(page)
+	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-QT.svg")
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
 
-	image2 = pdfjet.NewSVGImageFromFile("images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg")
-	image2.SetLocation(xy[0], 670.0)
-	xy = image2.DrawOn(page)
+	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-qq.svg")
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
 
-	image2 = pdfjet.NewSVGImageFromFile(
+	image = pdfjet.NewSVGImageFromFile("images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg")
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
+
+	image = pdfjet.NewSVGImageFromFile(
 		"images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg")
-	image2.SetLocation(xy[0], 670.0)
-	xy = image2.DrawOn(page)
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
 
-	image2 = pdfjet.NewSVGImageFromFile("images/svg-test/test-CS.svg")
-	image2.SetLocation(xy[0], 670.0)
-	xy = image2.DrawOn(page)
+	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-CS.svg")
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
 
-	image2 = pdfjet.NewSVGImageFromFile("images/svg-test/menu-icon.svg")
-	image2.SetLocation(xy[0], 670.0)
-	xy = image2.DrawOn(page)
+	image = pdfjet.NewSVGImageFromFile("images/svg-test/menu-icon.svg")
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
 
-	image2 = pdfjet.NewSVGImageFromFile("images/svg-test/menu-icon-close.svg")
-	image2.SetLocation(xy[0], 670.0)
-	image2.ScaleBy(2.0)
-	image2.DrawOn(page)
-
-	image2 = pdfjet.NewSVGImageFromFile("images/svg-test/europe.svg")
-	image2.SetLocation(-150.0, 0.0)
-	image2.DrawOn(page)
+	image = pdfjet.NewSVGImageFromFile("images/svg-test/menu-icon-close.svg")
+	image.SetLocation(xy[0], 670.0)
+	image.ScaleBy(2.0)
+	image.DrawOn(page)
 
 	pdf.Complete()
 }
