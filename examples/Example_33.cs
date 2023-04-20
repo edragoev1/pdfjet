a1def2329171ab599174908a855dd65bbc36c3ba
EDragoev <edragoev@protonmail.com>
2023-04-20 15:51:37 -0400

    Nice cleanup

--- examples/Example_33.cs
+++ examples/Example_33.cs
@@ -12,46 +12,46 @@ public class Example_33 {
                 new FileStream("Example_33.pdf", FileMode.Create)));
         Page page = new Page(pdf, A4.PORTRAIT);
 
-        SVGImage icon = new SVGImage("images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg");
-        icon.SetLocation(20f, 670f);
-        float[] xy = icon.DrawOn(page);
+        SVGImage image = new SVGImage("images/svg-test/europe.svg");
+        image.SetLocation(-150f, 0f);
+        float[] xy = image.DrawOn(page);
+        
+        image = new SVGImage("images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg");
+        image.SetLocation(20f, 670f);
+        xy = image.DrawOn(page);
 
-        icon = new SVGImage("images/svg/add_circle_FILL0_wght400_GRAD0_opsz48.svg");
-        icon.SetLocation(xy[0], 670f);
-        xy = icon.DrawOn(page);
+        image = new SVGImage("images/svg/add_circle_FILL0_wght400_GRAD0_opsz48.svg");
+        image.SetLocation(xy[0], 670f);
+        xy = image.DrawOn(page);
 
-        icon = new SVGImage("images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg");
-        icon.SetLocation(xy[0], 670f);
-        xy = icon.DrawOn(page);
+        image = new SVGImage("images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg");
+        image.SetLocation(xy[0], 670f);
+        xy = image.DrawOn(page);
 
-        icon = new SVGImage("images/svg/auto_stories_FILL0_wght400_GRAD0_opsz48.svg");
-        icon.SetLocation(xy[0], 670f);
-        xy = icon.DrawOn(page);
+        image = new SVGImage("images/svg/auto_stories_FILL0_wght400_GRAD0_opsz48.svg");
+        image.SetLocation(xy[0], 670f);
+        xy = image.DrawOn(page);
 
-        icon = new SVGImage("images/svg/star_FILL0_wght400_GRAD0_opsz48.svg");
-        icon.SetLocation(xy[0], 670);
-        xy = icon.DrawOn(page);
+        image = new SVGImage("images/svg/star_FILL0_wght400_GRAD0_opsz48.svg");
+        image.SetLocation(xy[0], 670);
+        xy = image.DrawOn(page);
 
-        icon = new SVGImage("images/svg-test/test-CS.svg");
-        icon.SetLocation(xy[0], 670);
-        xy = icon.DrawOn(page);
+        image = new SVGImage("images/svg-test/test-CS.svg");
+        image.SetLocation(xy[0], 670);
+        xy = image.DrawOn(page);
 
-        icon = new SVGImage("images/svg-test/test-QQ.svg");
-        icon.SetLocation(xy[0], 670);
-        xy = icon.DrawOn(page);
+        image = new SVGImage("images/svg-test/test-QQ.svg");
+        image.SetLocation(xy[0], 670);
+        xy = image.DrawOn(page);
 
-        icon = new SVGImage("images/svg-test/menu-icon.svg");
-        icon.SetLocation(xy[0], 670);
-        xy = icon.DrawOn(page);
+        image = new SVGImage("images/svg-test/menu-icon.svg");
+        image.SetLocation(xy[0], 670);
+        xy = image.DrawOn(page);
 
-        icon = new SVGImage("images/svg-test/menu-icon-close.svg");
-        icon.SetLocation(xy[0], 670);
-        icon.ScaleBy(2.0f);
-        xy = icon.DrawOn(page);
-
-        icon = new SVGImage("images/svg-test/europe.svg");
-        icon.SetLocation(-150f, 0f);
-        icon.DrawOn(page);
+        image = new SVGImage("images/svg-test/menu-icon-close.svg");
+        image.SetLocation(xy[0], 670);
+        image.ScaleBy(2.0f);
+        xy = image.DrawOn(page);
 
         pdf.Complete();
     }
--- examples/Example_33.java
+++ examples/Example_33.java
@@ -11,55 +11,49 @@ public class Example_33 {
         PDF pdf = new PDF(
                 new BufferedOutputStream(
                         new FileOutputStream("Example_33.pdf")));
-
         Page page = new Page(pdf, A4.PORTRAIT);
-/*
-        Image image = new Image(pdf, "images/photoshop.jpg");
-        image.setLocation(10f, 10f);
-        image.scaleBy(0.25f);
-        image.drawOn(page);
-*/
-        SVGImage icon = new SVGImage(
+
+        SVGImage image = new SVGImage("images/svg-test/europe.svg");
+        image.setLocation(-150f, 0f);
+        float[] xy = image.drawOn(page);
+
+        image = new SVGImage(
             "images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg");
-        icon.setLocation(20f, 670f);
-        float[] xy = icon.drawOn(page);
+        image.setLocation(20f, 670f);
+        xy = image.drawOn(page);
 
-        icon = new SVGImage("images/svg/add_circle_FILL0_wght400_GRAD0_opsz48.svg");
-        icon.setLocation(xy[0], 670f);
-        xy = icon.drawOn(page);
+        image = new SVGImage("images/svg/add_circle_FILL0_wght400_GRAD0_opsz48.svg");
+        image.setLocation(xy[0], 670f);
+        xy = image.drawOn(page);
 
-        icon = new SVGImage("images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg");
-        icon.setLocation(xy[0], 670f);
-        xy = icon.drawOn(page);
+        image = new SVGImage("images/svg/palette_FILL0_wght400_GRAD0_opsz48.svg");
+        image.setLocation(xy[0], 670f);
+        xy = image.drawOn(page);
 
-        icon = new SVGImage("images/svg/auto_stories_FILL0_wght400_GRAD0_opsz48.svg");
-        icon.setLocation(xy[0], 670f);
-        xy = icon.drawOn(page);
+        image = new SVGImage("images/svg/auto_stories_FILL0_wght400_GRAD0_opsz48.svg");
+        image.setLocation(xy[0], 670f);
+        xy = image.drawOn(page);
 
-        icon = new SVGImage("images/svg/star_FILL0_wght400_GRAD0_opsz48.svg");
-        icon.setLocation(xy[0], 670);
-        xy = icon.drawOn(page);
+        image = new SVGImage("images/svg/star_FILL0_wght400_GRAD0_opsz48.svg");
+        image.setLocation(xy[0], 670);
+        xy = image.drawOn(page);
 
-        icon = new SVGImage("images/svg-test/test-CS.svg");
-        icon.setLocation(xy[0], 670);
-        xy = icon.drawOn(page);
+        image = new SVGImage("images/svg-test/test-CS.svg");
+        image.setLocation(xy[0], 670);
+        xy = image.drawOn(page);
 
-        icon = new SVGImage("images/svg-test/test-QQ.svg");
-        icon.setLocation(xy[0], 670);
-        xy = icon.drawOn(page);
+        image = new SVGImage("images/svg-test/test-QQ.svg");
+        image.setLocation(xy[0], 670);
+        xy = image.drawOn(page);
 
-        icon = new SVGImage("images/svg-test/menu-icon.svg");
-        icon.setLocation(xy[0], 670);
-        xy = icon.drawOn(page);
+        image = new SVGImage("images/svg-test/menu-icon.svg");
+        image.setLocation(xy[0], 670);
+        xy = image.drawOn(page);
 
-        icon = new SVGImage("images/svg-test/menu-icon-close.svg");
-        icon.setLocation(xy[0], 670);
-        icon.scaleBy(2.0f);
-        xy = icon.drawOn(page);
-
-        icon = new SVGImage("images/svg-test/europe.svg");
-        icon.setLocation(-150f, 0f);
-        icon.drawOn(page);
+        image = new SVGImage("images/svg-test/menu-icon-close.svg");
+        image.setLocation(xy[0], 670);
+        image.scaleBy(2.0f);
+        image.drawOn(page);
 
         pdf.complete();
     }
