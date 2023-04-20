a8ff9b46823504251822fff2400db987b0842a48
EDragoev <edragoev@protonmail.com>
2023-04-20 14:19:03 -0400

    More cleanup.

--- run-mono.sh
+++ run-mono.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-mcs -warn:2 -debug -sdk:4 net/pdfjet/*.cs -reference:System.Drawing.dll -target:library -out:PDFjet.dll
+mcs -debug -sdk:4 -warn:2 net/pdfjet/*.cs -reference:System.Drawing.dll -target:library -out:PDFjet.dll
 mcs -debug -sdk:4 examples/Example_$1.cs -reference:PDFjet.dll
 mv examples/Example_$1.exe .
 chmod 777 Example_$1.exe
