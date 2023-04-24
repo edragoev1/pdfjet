c536fa2c4a0ad8920ac2a3f6382088d6cfa1cab6
EDragoev <edragoev@protonmail.com>
2023-04-24 12:46:21 -0400

    Small cleanup

--- examples/Example_08.java
+++ examples/Example_08.java
@@ -14,8 +14,7 @@ public class Example_08 {
 
     public Example_08() throws Exception {
         PDF pdf = new PDF(
-                new BufferedOutputStream(new FileOutputStream("Example_08.pdf")),
-                Compliance.PDF_UA);
+                new BufferedOutputStream(new FileOutputStream("Example_08.pdf")));
 
         // Font f1 = new Font(pdf, CoreFont.HELVETICA_BOLD);
         // Font f2 = new Font(pdf, CoreFont.HELVETICA);
