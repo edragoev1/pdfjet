1a34fc3593062d6be69bb126b0e45937a2c6419e
EDragoev <edragoev@protonmail.com>
2023-04-24 10:12:10 -0400

    Updated

--- clean.sh
+++ clean.sh
@@ -4,7 +4,7 @@ rm -f com/pdfjet/*.class
 rm -f net/pdfjet/*.exe.mdb
 rm -f examples/*.class
 rm -f examples/*.exe.mdb
-rm -r tests/*.exe.mdb
+rm -f tests/*.exe.mdb
 rm -f util/*.class
 rm -f *.jar
 rm -f *.exe
--- run-swift.sh
+++ run-swift.sh
@@ -3,8 +3,8 @@ if [ $# -eq 0 ]; then
     echo "./run-swift.sh 33"
     exit 1
 fi
-# swift run --configuration release Example_$1
-swift run --configuration debug Example_$1
+swift run --configuration release Example_$1
+# swift run --configuration debug Example_$1
 
 # mupdf Example_$1.pdf
 evince Example_$1.pdf
