820a81ab926ea319218218f63dc016dc63b9e9b3
EDragoev <edragoev@protonmail.com>
2023-04-24 16:11:28 -0400

    Small cleanup

--- src/textutils.go
+++ src/textutils.go
@@ -79,10 +79,8 @@ func ReadTextLines(filePath string) []string {
 func PrintDuration(example string, duration time.Duration) {
 	durationAsString := fmt.Sprintf("%.2f", float32(duration.Microseconds())/float32(1000.0))
 	if len(durationAsString) == 4 {
-		durationAsString = "   " + durationAsString
-	} else if len(durationAsString) == 5 {
 		durationAsString = "  " + durationAsString
-	} else if len(durationAsString) == 6 {
+	} else if len(durationAsString) == 5 {
 		durationAsString = " " + durationAsString
 	}
 	fmt.Print(example + " => " + durationAsString + "\n")
