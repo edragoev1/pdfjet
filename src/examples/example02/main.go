6ce6d52c9012b11a92af0aee815d85024d67b895
EDragoev <edragoev@protonmail.com>
2025-07-29 07:57:13 -0400

    Updated the example02 in Go.

--- src/examples/example02/main.go
+++ src/examples/example02/main.go
@@ -5,80 +5,40 @@ import (
 
 	pdfjet "github.com/edragoev1/pdfjet/src"
 	"github.com/edragoev1/pdfjet/src/color"
+	"github.com/edragoev1/pdfjet/src/content"
 	"github.com/edragoev1/pdfjet/src/letter"
 )
 
-// Example02 draws the Canadian Maple Leaf using a Path object that contains both
-// lines and curve segments. Every curve segment must have exactly 2 control points.
 func Example02() {
+	// Create a new PDF file
 	pdf := pdfjet.NewPDFFile("Example_02.pdf")
 
+	// Load Japanese font and set size
+	font1 := pdfjet.NewFontFromFile(pdf, "fonts/NotoSansJP/NotoSansJP-Regular.ttf.stream")
+	font1.SetSize(12.0)
+
+	// Load Korean font and set size
+	font2 := pdfjet.NewFontFromFile(pdf, "fonts/NotoSansKR/NotoSansKR-Regular.ttf.stream")
+	font2.SetSize(12.0)
+
+	// Add a new page in portrait Letter size
 	page := pdfjet.NewPage(pdf, letter.Portrait)
-	path := pdfjet.NewPath()
-	path.SetLocation(100.0, 50.0)
 
-	path.Add(pdfjet.NewPoint(13.0, 0.0))
-	path.Add(pdfjet.NewPoint(15.5, 4.5))
+	// Japanese text block
+	textBlock := pdfjet.NewTextBlock(font1, content.OfTextFile("data/languages/japanese.txt"))
+	textBlock.SetLocation(50.0, 50.0)
+	textBlock.SetWidth(415.0)
+	textBlock.SetBorderColor(color.None)
+	textBlock.DrawOn(page)
 
-	path.Add(pdfjet.NewPoint(18.0, 3.5))
-	path.Add(pdfjet.NewControlPoint(15.5, 13.5))
-	path.Add(pdfjet.NewControlPoint(15.5, 13.5))
-	path.Add(pdfjet.NewPoint(20.5, 7.5))
+	// Korean text block
+	textBlock = pdfjet.NewTextBlock(font2, content.OfTextFile("data/languages/korean.txt"))
+	textBlock.SetLocation(50.0, 450.0)
+	textBlock.SetWidth(415.0)
+	textBlock.SetBorderColor(color.None)
+	textBlock.DrawOn(page)
 
-	path.Add(pdfjet.NewPoint(21.0, 9.5))
-	path.Add(pdfjet.NewPoint(25.0, 9.0))
-	path.Add(pdfjet.NewPoint(24.0, 13.0))
-	path.Add(pdfjet.NewPoint(25.5, 14.0))
-	path.Add(pdfjet.NewPoint(19.0, 19.0))
-	path.Add(pdfjet.NewPoint(20.0, 21.5))
-	path.Add(pdfjet.NewPoint(13.5, 20.5))
-	path.Add(pdfjet.NewPoint(13.5, 27.0))
-	path.Add(pdfjet.NewPoint(12.5, 27.0))
-	path.Add(pdfjet.NewPoint(12.5, 20.5))
-	path.Add(pdfjet.NewPoint(6.0, 21.5))
-	path.Add(pdfjet.NewPoint(7.0, 19.0))
-	path.Add(pdfjet.NewPoint(0.5, 14.0))
-	path.Add(pdfjet.NewPoint(2.0, 13.0))
-	path.Add(pdfjet.NewPoint(1.0, 9.0))
-	path.Add(pdfjet.NewPoint(5.0, 9.5))
-
-	path.Add(pdfjet.NewPoint(5.5, 7.5))
-	path.Add(pdfjet.NewControlPoint(10.5, 13.5))
-	path.Add(pdfjet.NewControlPoint(10.5, 13.5))
-	path.Add(pdfjet.NewPoint(8.0, 3.5))
-
-	path.Add(pdfjet.NewPoint(10.5, 4.5))
-	path.SetClosePath(true)
-	path.SetColor(color.Red)
-	path.SetFillShape(true)
-	path.ScaleBy(4.0)
-	path.DrawOn(page)
-
-	path.ScaleBy(4.0)
-	path.SetFillShape(false)
-	xy := path.DrawOn(page)
-
-	box := pdfjet.NewBox()
-	box.SetLocation(xy[0], xy[1])
-	box.SetSize(20.0, 20.0)
-	box.DrawOn(page)
-
-	page.SetPenColorCMYK(1.0, 0.0, 0.0, 0.0)
-	page.SetPenWidth(5.0)
-	page.DrawLine(50.0, 500.0, 300.0, 500.0)
-
-	page.SetPenColorCMYK(0.0, 1.0, 0.0, 0.0)
-	page.SetPenWidth(5.0)
-	page.DrawLine(50.0, 550.0, 300.0, 550.0)
-
-	page.SetPenColorCMYK(0.0, 0.0, 1.0, 0.0)
-	page.SetPenWidth(5.0)
-	page.DrawLine(50.0, 600.0, 300.0, 600.0)
-
-	page.SetPenColorCMYK(0.0, 0.0, 0.0, 1.0)
-	page.SetPenWidth(5.0)
-	page.DrawLine(50.0, 650.0, 300.0, 650.0)
-
+	// Finalize the PDF
 	pdf.Complete()
 }
 
