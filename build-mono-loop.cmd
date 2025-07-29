f6078bd473ea37cad0032f693893237796680880
EDragoev <edragoev@protonmail.com>
2025-07-28 23:00:03 -0400

    Fixed the script.

--- build-mono-loop.cmd
+++ build-mono-loop.cmd
@@ -1,12 +1,13 @@
 @echo off
 
-mcs -warn:2 -debug -sdk:4.0 net/pdfjet/*.cs -reference:System.Drawing.dll -target:library -out:PDFjet.dll
+# mcs -warn:2 -debug -sdk:4.0 net/pdfjet/*.cs -reference:System.Drawing.dll -target:library -out:PDFjet.dll
+call dmcs /o /warn:2 /out:PDFjet.dll /target:library net/pdfjet/*.cs
 
 for /L %%i in (1,1,50) do (
     if %%i lss 10 (
-        mcs -warn:2 -optimize examples\Example_0%%i.cs -reference:PDFjet.dll
+        call dmcs -warn:2 -optimize examples\Example_0%%i.cs -reference:PDFjet.dll
     ) else (
-        mcs -warn:2 -optimize examples\Example_%%i.cs -reference:PDFjet.dll
+        call dmcs -warn:2 -optimize examples\Example_%%i.cs -reference:PDFjet.dll
     )
 )
 
@@ -14,8 +15,8 @@ move examples\Example_??.exe .
 
 for /L %%i in (1,1,50) do (
     if %%i lss 10 (
-        mono --debug Example_0%%i.exe
+        call mono --debug Example_0%%i.exe
     ) else (
-        mono --debug Example_%%i.exe
+        call mono --debug Example_%%i.exe
     )
 )
