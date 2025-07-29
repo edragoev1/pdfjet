d8ecec69ede18dbd985b309f3625946be8a68f73
EDragoev <edragoev@protonmail.com>
2025-07-28 23:29:01 -0400

    Update the script.

--- build-mono-loop.cmd
+++ build-mono-loop.cmd
@@ -1,13 +1,12 @@
 @echo off
 
-# mcs -warn:2 -debug -sdk:4.0 net/pdfjet/*.cs -reference:System.Drawing.dll -target:library -out:PDFjet.dll
 call dmcs /o /warn:2 /out:PDFjet.dll /target:library net/pdfjet/*.cs
 
 for /L %%i in (1,1,50) do (
     if %%i lss 10 (
-        call dmcs -warn:2 -optimize examples\Example_0%%i.cs -reference:PDFjet.dll
+        call dmcs /o /warn:2 examples\Example_0%%i.cs -reference:PDFjet.dll
     ) else (
-        call dmcs -warn:2 -optimize examples\Example_%%i.cs -reference:PDFjet.dll
+        call dmcs /o /warn:2 examples\Example_%%i.cs -reference:PDFjet.dll
     )
 )
 
