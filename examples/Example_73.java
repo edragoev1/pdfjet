7199405b7bdf73cf9bf544e241fc590c13f4586e
EDragoev <edragoev@protonmail.com>
2025-07-29 08:15:22 -0400

    Fixed this test case example for TextBox. More work required ...

--- examples/Example_73.java
+++ examples/Example_73.java
@@ -12,10 +12,10 @@ public class Example_73 {
                 new BufferedOutputStream(
                     new FileOutputStream("Example_73.pdf")), Compliance.PDF_UA);
 
-        Font f1 = new Font(pdf, "fonts/Droid/DroidSans.ttf.stream");
-        Font f2 = new Font(pdf, "fonts/Droid/DroidSansFallback.ttf.stream");
-
+        Font f1 = new Font(pdf, "fonts/IBMPlexSans/IBMPlexSans-Regular.ttf.stream");
         f1.setSize(12f);
+
+        Font f2 = new Font(pdf, "fonts/NotoSansTC/NotoSansTC-Regular.ttf.stream");
         f2.setSize(12f);
 
         Page page = new Page(pdf, Letter.PORTRAIT);
