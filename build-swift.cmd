192053b536a3f59675868d016cf501cb2345e735
EDragoev <edragoev@protonmail.com>
2025-07-29 00:14:17 -0400

    First added to the project build-swift.cmd

---
+++ build-swift.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+for /l %%i in (1,1,50) do (
+    if %%i lss 10 (
+        swift run --configuration release Example_0%%i
+    ) else (
+        swift run --configuration release Example_%%i
+    )
+)
+
+pause
