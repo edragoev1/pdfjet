dd52cedd6283a931e566489a568279ac4ff0d6fd
EDragoev <edragoev@protonmail.com>
2025-07-30 17:49:35 -0400

    Updated the example to use the new BitTable

--- examples/Example_43.cs
+++ examples/Example_43.cs
@@ -1,51 +1,33 @@
 using System;
 using System.IO;
-using System.Diagnostics;
 using System.Collections.Generic;
+using System.Diagnostics;
 using PDFjet.NET;
 
-/**
- * Example_43.cs
- */
-public class Example_43 {
-    public Example_43() {
+public class Example_43
+{
+    public Example_43()
+    {
         PDF pdf = new PDF(
                 new BufferedStream(new FileStream("Example_43.pdf", FileMode.Create)));
         pdf.SetCompliance(Compliance.PDF_UA);
 
+        // Used for performance testing. Results in 2000+ pages PDF.
         // String fileName = "data/Electric_Vehicle_Population_Data.csv";
         String fileName = "data/Electric_Vehicle_Population_10_Pages.csv";
 
         Font f1 = new Font(pdf, "fonts/IBMPlexSans/IBMPlexSans-SemiBold.ttf.stream");
+        f1.SetSize(10f);
+
         Font f2 = new Font(pdf, "fonts/IBMPlexSans/IBMPlexSans-Regular.ttf.stream");
-        f1.SetSize(9f);
         f2.SetSize(9f);
+
         BigTable table = new BigTable(pdf, f1, f2, Letter.LANDSCAPE);
-        List<float> widths = table.getColumnWidths(fileName);
-        // Optionally you can fine tune the widths of the columns:
-        widths[8] = 60f;    // Override the calculated width
-        widths[9] = 70f;    // Override the calculated width
-        table.SetColumnSpacing(7f);
-        table.SetLocation(20f, 15f);
-        table.SetBottomMargin(15f);
-        table.SetColumnWidths(widths);
-
-        // You can override that auto column alignments if required:
-        // final int LEFT = 0;                  // Align Left
-        // final int RIGHT = 1;                 // Align Right
-        // table.SetTextAlignment(1, RIGHT);    // Override the auto alignment
-        // table.SetTextAlignment(5, LEFT);     // Override the auto alignment
-
-        StreamReader reader = new StreamReader(fileName);
-        bool headerRow = true;
-        String line = null;
-        while ((line = reader.ReadLine()) != null) {
-            String[] fields = System.Text.RegularExpressions.Regex.Split(line, @",");
-            DrawRow(table, fields, headerRow);
-            headerRow = false;
-        }
-        table.Complete();
-        reader.Close();
+        table.SetNumberOfColumns(9);        // The order of the
+        table.SetTableData(fileName, ",");  // these statements
+        table.SetLocation(0f, 0f);          // is
+        table.SetBottomMargin(20f);         // very
+        table.Complete();                   // important!
 
         List<Page> pages = table.GetPages();
         for (int i = 0; i < pages.Count; i++) {
@@ -55,49 +37,6 @@ public class Example_43 {
         }
 
         pdf.Complete();
-    }
-
-    private void DrawRow(BigTable table, String[] fields, bool headerRow) {
-        List<String> row = new List<String>();
-        for (int i = 0; i < 10; i++) {
-            String field = fields[i];
-            if (i == 8) {
-                if (headerRow) {
-                    row.Add("Vehicle Type");
-                } else {
-                    if (field[0] == 'B') {
-                        row.Add("BEV");
-                    } else if (field[0] == 'P') {
-                        row.Add("PHEV");
-                    } else {
-                        row.Add(field);
-                    }
-                }
-            } else if (i == 9) {
-                if (headerRow) {
-                    row.Add("Green Vehicle");
-                } else {
-                    if (field[0] == 'C') {
-                        row.Add("YES");
-                    } else if (field[0] == 'N') {
-                        row.Add("NO");
-                    } else {
-                        row.Add("UNKNOWN");
-                    }
-                }
-            } else {
-                row.Add(field);
-            }
-        }
-        if (fields[6].Equals("TOYOTA")) {
-            table.DrawRow(row, Color.red);
-        } else if (fields[6].Equals("JEEP")) {
-            table.DrawRow(row, Color.green);
-        } else if (fields[6].Equals("FORD")) {
-            table.DrawRow(row, Color.blue);
-        } else {
-            table.DrawRow(row, Color.black);
-        }
     }
 
     public static void Main(String[] args) {
