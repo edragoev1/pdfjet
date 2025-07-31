458931958605bfb6738c5a8176c436598c9abd8a
EDragoev <edragoev@protonmail.com>
2025-07-30 21:00:32 -0400

    Added to the project.

--- run-swift.sh
+++ run-swift.sh
@@ -3,8 +3,11 @@ if [ $# -eq 0 ]; then
     echo "./run-swift.sh 33"
     exit 1
 fi
-swift run --configuration release Example_$1
-# swift run --configuration debug Example_$1
+
+rm -rf .build
+
+# swift run --configuration release Example_$1
+swift run --configuration debug Example_$1
 
 # mupdf Example_$1.pdf
 evince Example_$1.pdf
