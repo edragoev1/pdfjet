0ab622e630465416c6e77d34dee958b995268bad
EDragoev <edragoev@protonmail.com>
2025-07-30 23:29:22 -0400

    Run in production mode.

--- run-swift.sh
+++ run-swift.sh
@@ -6,8 +6,8 @@ fi
 
 rm -rf .build
 
-# swift run --configuration release Example_$1
-swift run --configuration debug Example_$1
+swift run --configuration release Example_$1
+# swift run --configuration debug Example_$1
 
 # mupdf Example_$1.pdf
 evince Example_$1.pdf
