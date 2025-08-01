d6f6f8c496e23c6ec1cbcf0d5efd1b782d44d0cb
EDragoev <edragoev@protonmail.com>
2025-08-01 13:39:23 -0400

    Added one more font helper class.

---
+++ net/pdfjet/IBMPlexSans.cs
@@ -0,0 +1,16 @@
+namespace PDFjet.NET {
+    public static class IBMPlexSans {
+        public const string Bold             = "fonts/IBMPlexSans/IBMPlexSans-Bold.ttf.stream";
+        public const string BoldItalic       = "fonts/IBMPlexSans/IBMPlexSans-BoldItalic.ttf.stream";
+        public const string ExtraLight       = "fonts/IBMPlexSans/IBMPlexSans-ExtraLight.ttf.stream";
+        public const string ExtraLightItalic = "fonts/IBMPlexSans/IBMPlexSans-ExtraLightItalic.ttf.stream";
+        public const string Italic           = "fonts/IBMPlexSans/IBMPlexSans-Italic.ttf.stream";
+        public const string Light            = "fonts/IBMPlexSans/IBMPlexSans-Light.ttf.stream";
+        public const string LightItalic      = "fonts/IBMPlexSans/IBMPlexSans-LightItalic.ttf.stream";
+        public const string Medium           = "fonts/IBMPlexSans/IBMPlexSans-Medium.ttf.stream";
+        public const string MediumItalic     = "fonts/IBMPlexSans/IBMPlexSans-MediumItalic.ttf.stream";
+        public const string Regular          = "fonts/IBMPlexSans/IBMPlexSans-Regular.ttf.stream";
+        public const string SemiBold         = "fonts/IBMPlexSans/IBMPlexSans-SemiBold.ttf.stream";
+        public const string SemiBoldItalic   = "fonts/IBMPlexSans/IBMPlexSans-SemiBoldItalic.ttf.stream";
+    }
+}
