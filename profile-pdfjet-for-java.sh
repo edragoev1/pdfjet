bc0bd9f0e30718838c6f0a247ab975ff9bc320f4
EDragoev <edragoev@protonmail.com>
2025-08-01 00:26:29 -0400

    Added to the project.

--- profile-pdfjet-for-java.sh
+++ profile-pdfjet-for-java.sh
@@ -1,2 +1,4 @@
 javac -g -O -encoding utf-8 -Xlint com/pdfjet/*.java -d out/production
+jar cf PDFjet.jar -C out/production .
+javac -g -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_43.java -d out/production
 java -XX:+FlightRecorder -XX:StartFlightRecording=duration=60s,filename=recording.jfr -cp PDFjet.jar:out/production examples.Example_43
