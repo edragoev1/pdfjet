e6dd98377673b928b5d2058db8faaf70f5a842a1
EDragoev <edragoev@protonmail.com>
2025-08-01 00:26:53 -0400

    Removed ...

--- profile-java-example.sh
+++
@@ -1,7 +0,0 @@
-javac -O -encoding utf-8 -Xlint com/pdfjet/*.java -d out/production
-jar cf PDFjet.jar -C out/production .
-
-javac -encoding utf-8 -Xlint -cp PDFjet.jar examples/Example_$1.java -d out/production
-java -cp .:PDFjet.jar:out/production examples.Example_$1
-
-java -XX:+FlightRecorder -XX:StartFlightRecording=filename=recording.jfr -cp .:PDFjet.jar:out/production examples.Example_$1
