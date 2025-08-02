3eebb15a4ed416bfefce1382e4ee127b95076759
EDragoev <edragoev@protonmail.com>
2025-08-02 07:39:30 -0400

    Updated ...

--- run-java.sh
+++ run-java.sh
@@ -9,9 +9,8 @@ fi
 
 mkdir -p out/production
 
-# Compile and package the library.
+# Compile the PDFjet library.
 javac -O -encoding utf-8 -Xlint com/pdfjet/*.java com/pdfjet/font/*.java -d out/production
-# jar cf PDFjet.jar -C out/production .
 
 # Compile and run the Example_?? program.
 javac -encoding utf-8 -Xlint -cp out/production examples/Example_$1.java -d out/production
