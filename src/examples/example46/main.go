dda445799879f368767d3b0cfb70fc3e1e6f8c74
Evgeni Dragoev <edragoev@protonmail.com>
2026-06-02 00:13:22 -0400

    Added more code ...

--- src/examples/example46/main.go
+++ src/examples/example46/main.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	pdfjet "github.com/edragoev1/pdfjet/src"
+	"github.com/edragoev1/pdfjet/src/compress"
 	"github.com/edragoev1/pdfjet/src/corefont"
 	"github.com/edragoev1/pdfjet/src/encryption"
 	"github.com/edragoev1/pdfjet/src/letter"
@@ -34,6 +35,8 @@ func Example46() {
 
 	image := pdfjet.NewImageFromFile(pdf, "images/ee-map.png")
 
+	file1 := pdfjet.NewEmbeddedFileAtPath(pdf, "images/linux-logo.png", compress.No)
+
 	page := pdfjet.NewPage(pdf, letter.Portrait)
 
 	textLine := pdfjet.NewTextLine(f1, "Hello, World!")
@@ -43,6 +46,16 @@ func Example46() {
 	image.SetLocation(100, 150)
 	image.ScaleBy(0.5)
 	image.DrawOn(page)
+
+	// File attachment functionality
+	attachment := pdfjet.NewFileAttachment(pdf, file1)
+	attachment.SetLocation(100.0, 550.0)
+	attachment.SetIconPushPin()
+	attachment.SetIconSize(24.0)
+	attachment.SetTitle("Attached File: " + file1.GetFileName())
+	attachment.SetDescription(
+		"Right mouse click on the icon to save the attached file.")
+	attachment.DrawOn(page)
 
 	pdf.Complete()
 }
