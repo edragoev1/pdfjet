4e95f022d0e96f1b29a82651b67068e6a00779d0
Evgeni Dragoev <edragoev@protonmail.com>
2026-06-05 11:55:46 -0400

    Improved the Example_28.cs

--- examples/Example_28/Example_28.cs
+++ examples/Example_28/Example_28.cs
@@ -6,111 +6,65 @@ using PDFjet.NET;
 
 /**
  *  Example_28.cs
- *  Example that shows how to use fallback font and the NotoSans symbols font.
+ *  Example that shows how to use the NotoSansSymbols font.
  */
 public class Example_28 {
     public Example_28() {
         PDF pdf = new PDF(new BufferedStream(
                 new FileStream("Example_28.pdf", FileMode.Create)));
 
-        Font f1 = new Font(pdf, "fonts/NotoSans/NotoSans-Regular.ttf.stream");
-        f1.SetSize(11f);
-
-        Font f2 = new Font(pdf, "fonts/NotoSansTC/NotoSansTC-Regular.ttf.stream");
-        f2.SetSize(11f);
-
-        Font f3 = new Font(pdf, "fonts/NotoSansSymbols/NotoSansSymbols-Regular.ttf.stream");
-        f3.SetSize(11f);
+        Font f1 = new Font(pdf, "fonts/NotoSansSymbols/NotoSansSymbols-Regular.ttf.stream");
+        f1.SetSize(28f);
 
         Page page = new Page(pdf, Letter.LANDSCAPE);
 
-        StreamReader reader = new StreamReader(
-                new FileStream("data/report.csv", FileMode.Open, FileAccess.Read));
+        float x = 35f;
+        float y = 55f;
+        float dy = 35f;
 
-        float y = 40f;
-        String str = null;
-        while ((str = reader.ReadLine()) != null) {
-            new TextLine(f1, str)
-                    .SetFallbackFont(f2)
-                    .SetLocation(50f, y += 20f)
-                    .DrawOn(page);
-        }
-        reader.Close();
+        DrawLineOfText(page, f1, x, y, 0x0041, 0x005A);
+        y += dy;
 
-        float x = 50f;
-        y = 210f;
-        float dy = 22f;
+        DrawLineOfText(page, f1, x, y, 0x0061, 0x007A);
+        y += dy;
 
-        TextLine text = new TextLine(f3);
+        DrawLineOfText(page, f1, x, y, 0x24B6, 0x24CF);
+        y += dy;
+
+        DrawLineOfText(page, f1, x, y, 0x24D0, 0x24E9);
+        y += dy;
+
+        DrawLineOfText(page, f1, x, y, 0x24F5, 0x24FE);
+        y += dy;
+
+        DrawLineOfText(page, f1, x, y, 0x2624, 0x262F);
+        y += dy;
+
+        DrawLineOfText(page, f1, x, y, 0x2638, 0x2653);
+        y += dy;
+
+        DrawLineOfText(page, f1, x, y, 0x2669, 0x267E);
+        y += dy;
+
+        DrawLineOfText(page, f1, x, y, 0x2690, 0x26A9);
+        y += dy;
+
+        DrawLineOfText(page, f1, x, y, 0x26AD, 0x26BC);
+        y += dy;
+
+        pdf.Complete();
+    }
+
+    private void DrawLineOfText(
+            Page page, Font f1, float x, float y, int c1, int c2) {
         StringBuilder buf = new StringBuilder();
-        int count = 0;
-        for (int i = 0x2200; i <= 0x22FF; i++) {
-            // Draw the Math Symbols
-            if (count % 80 == 0) {
-                text.SetText(buf.ToString());
-                text.SetLocation(x, y += dy);
-                text.DrawOn(page);
-                buf.Length = 0;
-            }
+        for (int i = c1; i <= c2; i++) {
             buf.Append((char) i);
-            count++;
         }
-        text.SetText(buf.ToString());
-        text.SetLocation(x, y += dy);
-        text.DrawOn(page);
-        buf.Length = 0;
-
-        count = 0;
-        for (int i = 0x25A0; i <= 0x25FF; i++) {
-            // Draw the Geometric Shapes
-            if (count % 80 == 0) {
-                text.SetText(buf.ToString());
-                text.SetLocation(x, y += dy);
-                text.DrawOn(page);
-                buf.Length = 0;
-            }
-            buf.Append((char) i);
-            count++;
-        }
-        text.SetText(buf.ToString());
-        text.SetLocation(x, y += dy);
-        text.DrawOn(page);
-        buf.Length = 0;
-
-        count = 0;
-        for (int i = 0x2701; i <= 0x27ff; i++) {
-            // Draw the Dingbats
-            if (count % 80 == 0) {
-                text.SetText(buf.ToString());
-                text.SetLocation(x, y += dy);
-                text.DrawOn(page);
-                buf.Length = 0;
-            }
-            buf.Append((char) i);
-            count++;
-        }
-        text.SetText(buf.ToString());
-        text.SetLocation(x, y += dy);
-        text.DrawOn(page);
-        buf.Length = 0;
-
-        count = 0;
-        for (int i = 0x2800; i <= 0x28FF; i++) {
-            // Draw the Braille Patterns
-            if (count % 80 == 0) {
-                text.SetText(buf.ToString());
-                text.SetLocation(x, y += dy);
-                text.DrawOn(page);
-                buf.Length = 0;
-            }
-            buf.Append((char) i);
-            count++;
-        }
+        TextLine text = new TextLine(f1);
         text.SetText(buf.ToString());
         text.SetLocation(x, y);
         text.DrawOn(page);
-
-        pdf.Complete();
     }
 
     public static void Main(String[] args) {
