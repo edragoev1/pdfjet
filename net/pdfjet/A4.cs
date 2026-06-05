0fb21462ada9a7cccd7434a53dbe2ff1cd44acba
Evgeni Dragoev <edragoev@protonmail.com>
2026-06-04 21:48:35 -0400

    Changed the copyright to 2026

--- net/pdfjet/A4.cs
+++ net/pdfjet/A4.cs
@@ -1,7 +1,7 @@
 /**
  * A4.cs
  *
- * Copyright (c) 2025 PDFjet Software
+ * Copyright (c) 2026 PDFjet Software
  * Licensed under the MIT License. See LICENSE file in the project root.
  */
 using System;
--- net/pdfjet/QRCode/QRCode.cs
+++ net/pdfjet/QRCode/QRCode.cs
@@ -1,7 +1,7 @@
 /**
  * QRCode.cs
  *
- * Copyright (c) 2025 PDFjet Software
+ * Copyright (c) 2026 PDFjet Software
  * Licensed under the MIT License. See LICENSE file in the project root.
  *
  * Original author: Kazuhiko Arase, 2009
