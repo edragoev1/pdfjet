9a991fb54f835c6820405570388c5ba0e255e6d1
Evgeni Dragoev <edragoev@protonmail.com>
2026-06-04 21:54:07 -0400

    Changed the copyright to 2026

--- Sources/PDFjet/TextUtils.swift
+++ Sources/PDFjet/TextUtils.swift
@@ -1,7 +1,7 @@
 /**
  * TextUtils.swift
  *
- * Copyright (c) 2025 PDFjet Software
+ * Copyright (c) 2026 PDFjet Software
  * Licensed under the MIT License. See LICENSE file in the project root.
  */
 import Foundation
