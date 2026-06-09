7130c7dcf1c7ba7cabf6c50c9b93f935ad02b5a4
Evgeni Dragoev <edragoev@protonmail.com>
2026-06-09 13:50:37 -0400

    Cleaned up the code ...

--- examples/Example_31.java
+++ examples/Example_31.java
@@ -17,11 +17,13 @@ public class Example_31 {
 
         Page page = new Page(pdf, Letter.PORTRAIT);
 
-        TextBox textBox = new TextBox(f1, Content.ofTextFile("data/languages/marathi.txt"), 500f, 300f);
-        textBox.setLocation(50f, 50f);
-        textBox.setBorder(Border.LEFT);
-        textBox.setBorder(Border.RIGHT);
-        textBox.drawOn(page);
+        TextBlock textBlock = new TextBlock(
+                f1, Content.ofTextFile("data/languages/marathi.txt"));
+        textBlock.setLocation(50f, 50f);
+        textBlock.setWidth(500f);
+//         textBlock.setBorder(Border.LEFT);
+//         textBlock.setBorder(Border.RIGHT);
+        textBlock.drawOn(page);
 
         String str = "असम के बाद UP में भी CM कैंडिडेट का ऐलान करेगी BJP?";
         TextLine textLine = new TextLine(f1, str);
@@ -31,28 +33,28 @@ public class Example_31 {
         page.setPenColor(Color.blue);
         page.setBrushColor(Color.blue);
         page.fillRect(50f, 200f, 200f, 200f);
-
-        GraphicsState gs = new GraphicsState();
-        gs.setAlphaStroking(0.5f);      // The stroking alpha constant
-        gs.setAlphaNonStroking(0.5f);   // The non-stroking alpha constant
-        page.setGraphicsState(gs);
-        page.setPenColor(Color.green);
-        page.setBrushColor(Color.green);
-        page.fillRect(100f, 250f, 200f, 200f);
-
-        page.setPenColor(Color.red);
-        page.setBrushColor(Color.red);
-        page.fillRect(150, 300, 200f, 200f);
-
-        // Reset the parameters to the default values
-        page.setGraphicsState(new GraphicsState());
-
-        page.setPenColor(Color.orange);
-        page.setBrushColor(Color.orange);
-        page.fillRect(200, 350, 200f, 200f);
-
-        page.setBrushColor(0x00003865);
-        page.fillRect(50, 550, 200f, 200f);
+//
+//         GraphicsState gs = new GraphicsState();
+//         gs.setAlphaStroking(0.5f);      // The stroking alpha constant
+//         gs.setAlphaNonStroking(0.5f);   // The non-stroking alpha constant
+//         page.setGraphicsState(gs);
+//         page.setPenColor(Color.green);
+//         page.setBrushColor(Color.green);
+//         page.fillRect(100f, 250f, 200f, 200f);
+//
+//         page.setPenColor(Color.red);
+//         page.setBrushColor(Color.red);
+//         page.fillRect(150, 300, 200f, 200f);
+//
+//         // Reset the parameters to the default values
+//         page.setGraphicsState(new GraphicsState());
+//
+//         page.setPenColor(Color.orange);
+//         page.setBrushColor(Color.orange);
+//         page.fillRect(200, 350, 200f, 200f);
+//
+//         page.setBrushColor(0x00003865);
+//         page.fillRect(50, 550, 200f, 200f);
 
         pdf.complete();
     }
