b41ccd1d41736614b348f40636c4ddaec46c4643
Evgeni Dragoev <edragoev@protonmail.com>
2026-06-10 15:57:48 -0400

    Small code cleanup ...

--- examples/Example_27/Example_27.cs
+++ examples/Example_27/Example_27.cs
@@ -98,20 +98,7 @@ public class Example_27 {
         textLine = new TextLine(f3, str);
         textLine.SetLocation(x - f3.StringWidth(str), y += 20f);
         textLine.DrawOn(page);
-/*
-        y += 15f;
-        f3.SetSize(14);
-        // Arabic text from a file
-        textBlock = new TextBlock(f3,
-                Content.OfTextFile("data/languages/arabic.txt"));
-        textBlock.SetLocation(50f, y);
-        textBlock.SetBorderColor(Color.blue);
-        textBlock.SetWidth(520f);
-        textBlock.SetTextIsArabic();
-        textBlock.SetTextAlignment(Alignment.RIGHT);
-        textBlock.SetTextPadding(10f);
-        textBlock.DrawOn(page);
-*/
+
         pdf.Complete();
     }
 
