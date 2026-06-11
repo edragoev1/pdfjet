e0494bd256f17e6f94b9e2833fd584ab1b657f95
Evgeni Dragoev <edragoev@protonmail.com>
2026-06-10 20:21:39 -0400

    Simplified the code ...

--- Sources/Example_27/main.swift
+++ Sources/Example_27/main.swift
@@ -35,70 +35,70 @@ public class Example_27 {
         var str = "כך נראית תחתית הטבלה עם סיום הפלייאוף התחתון:"
         str = Bidi.reorderVisually(str)
         var textLine = TextLine(f2, str)
-        textLine.setLocation(x - f2.stringWidth(f2, str), y)
+        textLine.setLocation(x - f2.stringWidth(str), y)
         textLine.drawOn(page)
 
         y += 20.0
         str = "10. הפועל כפר סבא 38 נקודות (הפרש שערים 14-)"
         str = Bidi.reorderVisually(str)
         textLine = TextLine(f2, str)
-        textLine.setLocation(x - f2.stringWidth(f2, str), y)
+        textLine.setLocation(x - f2.stringWidth(str), y)
         textLine.drawOn(page)
 
         y += 20.0
         str = "11. הפועל קריית שמונה 36 נקודות (הפרש שערים 7-)"
         str = Bidi.reorderVisually(str)
         textLine = TextLine(f2, str)
-        textLine.setLocation(x - f2.stringWidth(f2, str), y)
+        textLine.setLocation(x - f2.stringWidth(str), y)
         textLine.drawOn(page)
 
         y += 20.0
         str = "12. הפועל חיפה 34 נקודות (הפרש שערים 10-)"
         str = Bidi.reorderVisually(str)
         textLine = TextLine(f2, str)
-        textLine.setLocation(x - f2.stringWidth(f2, str), y)
+        textLine.setLocation(x - f2.stringWidth(str), y)
         textLine.drawOn(page)
 
         y += 20.0
         str = "13. הפועל עכו 34 נקודות (הפרש שערים 21-)"
         str = Bidi.reorderVisually(str)
         textLine = TextLine(f2, str)
-        textLine.setLocation(x - f2.stringWidth(f2, str), y)
+        textLine.setLocation(x - f2.stringWidth(str), y)
         textLine.drawOn(page)
 
         y += 65.0
         str = Bidi.reorderVisually(
                 "قالت شركة PSA بيجو ستروين الفرنسية وشريكتها الصينية شركة دونغفينغ موترز الاربعاء إنهما اتفقتا")
         textLine = TextLine(f3, str)
-        textLine.setLocation(x - f3.stringWidth(f3, str), y)
+        textLine.setLocation(x - f3.stringWidth(str), y)
         textLine.drawOn(page)
 
         y += 20.0
         str = Bidi.reorderVisually(
                 "على التعاون في تطوير السيارات التي تعمل بالطاقة الكهربائية اعتبارا من عام 2019.")
         textLine = TextLine(f3, str)
-        textLine.setLocation(x - f3.stringWidth(f3, str), y)
+        textLine.setLocation(x - f3.stringWidth(str), y)
         textLine.drawOn(page)
 
         y += 20.0
         str = Bidi.reorderVisually(
                 "وجاء في تصريح اصدرته في باريس الشركة الفرنسية ان الشركتين ستنتجان نموذجا كهربائيا مشتركا تستخدمه كل")
         textLine = TextLine(f3, str)
-        textLine.setLocation(x - f3.stringWidth(f3, str), y)
+        textLine.setLocation(x - f3.stringWidth(str), y)
         textLine.drawOn(page)
 
         y += 20.0
         str = Bidi.reorderVisually(
                 "من بيجو وسيتروين ودونغفينغ.")
         textLine = TextLine(f3, str)
-        textLine.setLocation(x - f3.stringWidth(f3, str), y)
+        textLine.setLocation(x - f3.stringWidth(str), y)
         textLine.drawOn(page)
 
         y += 20.0
         str = Bidi.reorderVisually(
                 "وقالت إن الخطة تهدف الى تحقيق عائد يزيد على 100 مليار يوان (15,4 مليار دولار) بحلول عام 2020.")
         textLine = TextLine(f3, str)
-        textLine.setLocation(x - f3.stringWidth(f3, str), y)
+        textLine.setLocation(x - f3.stringWidth(str), y)
         textLine.drawOn(page)
 
         pdf.complete()
