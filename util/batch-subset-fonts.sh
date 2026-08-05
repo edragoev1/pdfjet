0448ecb2f90d656a89f63e432eb9f79f8319d0b2
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-05 16:47:13 -0400

    Updated the script

--- util/batch-subset-fonts.sh
+++ util/batch-subset-fonts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FONT_DIR="../fonts/NotoSansSC/"
+CJK_PUNCTUATION="U+3000-303F"  # CJK Symbols and Punctuation
 UNICODES_FILE="../data/languages/SC_3500.txt"
 
 # Process each font file
@@ -18,6 +19,7 @@ for font in "$FONT_DIR"/*.ttf; do
     echo "Processing: $filename → $output"
     
     pyftsubset "$font" \
+	--unicodes="$CJK_PUNCTUATION" \
         --unicodes-file="$UNICODES_FILE" \
         --output-file="$FONT_DIR/$output"
 done
