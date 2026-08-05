2039a3beef2eede8edef2b9093a77204e27a9ab0
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-05 17:57:17 -0400

    Updated the script

--- util/batch-subset-fonts.sh
+++ util/batch-subset-fonts.sh
@@ -2,12 +2,7 @@
 
 FONT_DIR="../fonts/NotoSansSC/"
 UNICODES_FILE="../data/languages/SC_3500.txt"
-
-# Explicitly force the commas and periods that were disappearing
-# We combine the range with specific codepoints to ensure they are hit
-# U+3001 (Ideographic Comma), U+3002 (Ideographic Full Stop)
-# U+FF0C (Fullwidth Comma), U+FF0E (Fullwidth Full Stop)
-EXPLICIT_PUNCTUATION="U+3001,U+3002,U+FF0C,U+FF0E,U+3000-303F,U+FF00-FFEF,U+2000-206F"
+CJK_PUNCTUATION="U+2000-206F,U+3000-303F,U+FF00-FFEF"
 
 # Process each font file
 for font in "$FONT_DIR"/*.ttf; do
@@ -24,7 +19,7 @@ for font in "$FONT_DIR"/*.ttf; do
     echo "Processing: $filename → $output"
     
     pyftsubset "$font" \
-        --unicodes-file="$UNICODES_FILE" \
 	--unicodes="$CJK_PUNCTUATION" \
+        --unicodes-file="$UNICODES_FILE" \
         --output-file="$FONT_DIR/$output"
 done
