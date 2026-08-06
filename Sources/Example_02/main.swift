f894b2d37ee88de0f64615535b1585fd754c885d
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-06 13:05:12 -0400

    Added more code

--- Sources/Example_02/main.swift
+++ Sources/Example_02/main.swift
@@ -9,23 +9,47 @@ public class Example_02 {
         let stream = OutputStream(toFileAtPath: "Example_02.pdf", append: false)
         let pdf = PDF(stream!)
 
-        let font1 = try Font(pdf, "fonts/NotoSansJP/NotoSansJP-Regular.ttf.stream")
-        font1.setSize(12.0)
+        let f1 = try Font(pdf, "fonts/NotoSansJP/NotoSansJP-Regular.ttf.stream")
+        f1.setSize(14.0)
 
-        let font2 = try Font(pdf, "fonts/NotoSansKR/NotoSansKR-Regular.ttf.stream")
-        font2.setSize(12.0)
+        let f2 = try Font(pdf, "fonts/NotoSansKR/NotoSansKR-Regular.ttf.stream")
+        f2.setSize(14.0)
 
-        let page = Page(pdf, Letter.PORTRAIT)
+        let f3 = try Font(pdf, "fonts/NotoSansSC/NotoSansSC-Regular.ttf.stream")
+        f3.setSize(14.0)
+
+        let f4 = try Font(pdf, "fonts/NotoSansTC/NotoSansTC-Regular.ttf.stream")
+        f4.setSize(14.0)
+
+        var page = Page(pdf, Letter.PORTRAIT)
 
         var text = try String(contentsOfFile: "data/languages/japanese.txt", encoding: .utf8)
-        var textBlock = TextBlock(font1, text)
+        var textBlock = TextBlock(f1, text)
         textBlock.setLocation(50.0, 50.0)
         textBlock.setWidth(415.0)
         textBlock.drawOn(page)
 
+        page = Page(pdf, Letter.PORTRAIT)
+
         text = try String(contentsOfFile: "data/languages/korean.txt", encoding: .utf8)
-        textBlock = TextBlock(font2, text)
-        textBlock.setLocation(50.0, 450.0)
+        textBlock = TextBlock(f2, text)
+        textBlock.setLocation(50.0, 50.0)
+        textBlock.setWidth(415.0)
+        textBlock.drawOn(page)
+
+        page = Page(pdf, Letter.PORTRAIT)
+
+        text = try String(contentsOfFile: "data/languages/simplified-chinese.txt", encoding: .utf8)
+        textBlock = TextBlock(f3, text)
+        textBlock.setLocation(50.0, 50.0)
+        textBlock.setWidth(415.0)
+        textBlock.drawOn(page)
+
+        page = Page(pdf, Letter.PORTRAIT)
+
+        text = try String(contentsOfFile: "data/languages/traditional-chinese.txt", encoding: .utf8)
+        textBlock = TextBlock(f4, text)
+        textBlock.setLocation(50.0, 50.0)
         textBlock.setWidth(415.0)
         textBlock.drawOn(page)
 
