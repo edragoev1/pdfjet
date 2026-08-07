facb5a00cce0e85302bf8ecc856e65c22a275156
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-07 16:14:05 -0400

    Simplified the example code

--- Sources/Example_18/main.swift
+++ Sources/Example_18/main.swift
@@ -9,32 +9,8 @@ public class Example_18 {
     public init() throws {
         let pdf = PDF(OutputStream(toFileAtPath: "Example_18.pdf", append: false)!)
 
-        // let buf1 = try Content.ofBinaryFile("images/svg-test/europe.svg")
-        // print()
-        // print("Original file size:")
-        // print(buf1.count)
-        // print()
-
-        // var buf2 = [UInt8]()
-        // var time0 = Int64(Date().timeIntervalSince1970 * 1000)
-        // FlateEncode(&buf2, buf1)
-        // var time1 = Int64(Date().timeIntervalSince1970 * 1000)
-        // print("DEFLATE size:")
-        // print(buf2.count)
-        // print("Time to DEFLATE: \(time1 - time0)")
-        // print()
-
-        // buf2 = [UInt8]()
-        // time0 = Int64(Date().timeIntervalSince1970 * 1000)
-        // LZWEncode(&buf2, buf1)
-        // time1 = Int64(Date().timeIntervalSince1970 * 1000)
-        // print("LZWEncode size:")
-        // print(buf2.count)
-        // print("Time to LZWEncode: \(time1 - time0)")
-        // print()
-
         let font = try Font(pdf, "fonts/IBMPlexSans/IBMPlexSans-Regular.ttf.stream")
-        font.setSize(12.0)
+        font.setSize(14.0)
 
         var pages = [Page]()
         var page = Page(pdf, A4.PORTRAIT, Page.DETACHED)
@@ -78,10 +54,7 @@ public class Example_18 {
                     (page.getHeight() - 5.0))
             i += 1
         }
-
-        for page in pages {
-            pdf.addPage(page)
-        }
+        pdf.addPages(pages)
 
         pdf.complete()
     }
