a73816e51cc26731e685d1426a261b7077695311
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 06:51:54 -0400

    Updated the font helper classes

--- com/pdfjet/fonts/IBMPlexSansTC.java
+++ com/pdfjet/fonts/IBMPlexSansTC.java
@@ -1,12 +1,12 @@
 package com.pdfjet;
 
 public class IBMPlexSansTC {
-    public static final String Bold         = "fonts/IBMPlexSansTC/IBMPlexSansTC-Bold.otf";
-    public static final String ExtraLight   = "fonts/IBMPlexSansTC/IBMPlexSansTC-ExtraLight.otf";
-    public static final String Light        = "fonts/IBMPlexSansTC/IBMPlexSansTC-Light.otf";
-    public static final String Medium       = "fonts/IBMPlexSansTC/IBMPlexSansTC-Medium.otf";
-    public static final String Regular      = "fonts/IBMPlexSansTC/IBMPlexSansTC-Regular.otf";
-    public static final String SemiBold     = "fonts/IBMPlexSansTC/IBMPlexSansTC-SemiBold.otf";
-    public static final String Text         = "fonts/IBMPlexSansTC/IBMPlexSansTC-Text.otf";
-    public static final String Thin         = "fonts/IBMPlexSansTC/IBMPlexSansTC-Thin.otf";
+    public static final String Bold         = "fonts/IBMPlexSansTC/IBMPlexSansTC-Bold.otf.stream";
+    public static final String ExtraLight   = "fonts/IBMPlexSansTC/IBMPlexSansTC-ExtraLight.otf.stream";
+    public static final String Light        = "fonts/IBMPlexSansTC/IBMPlexSansTC-Light.otf.stream";
+    public static final String Medium       = "fonts/IBMPlexSansTC/IBMPlexSansTC-Medium.otf.stream";
+    public static final String Regular      = "fonts/IBMPlexSansTC/IBMPlexSansTC-Regular.otf.stream";
+    public static final String SemiBold     = "fonts/IBMPlexSansTC/IBMPlexSansTC-SemiBold.otf.stream";
+    public static final String Text         = "fonts/IBMPlexSansTC/IBMPlexSansTC-Text.otf.stream";
+    public static final String Thin         = "fonts/IBMPlexSansTC/IBMPlexSansTC-Thin.otf.stream";
 }
