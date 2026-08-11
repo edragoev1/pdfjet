8c274df0416fcf29c517e1951690e5d6283e59ef
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 08:54:41 -0400

    Added two more font helper classes

---
+++ com/pdfjet/fonts/IBMPlexSansArabic.java
@@ -0,0 +1,12 @@
+package com.pdfjet;
+
+public class IBMPlexSansArabic {
+    public static final String Bold         = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Bold.otf.stream";
+    public static final String ExtraLight   = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-ExtraLight.otf.stream";
+    public static final String Light        = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Light.otf.stream";
+    public static final String Medium       = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Medium.otf.stream";
+    public static final String Regular      = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Regular.otf.stream";
+    public static final String SemiBold     = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-SemiBold.otf.stream";
+    public static final String Text         = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Text.otf.stream";
+    public static final String Thin         = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Thin.otf.stream";
+}
---
+++ com/pdfjet/fonts/IBMPlexSansHebrew.java
@@ -0,0 +1,12 @@
+package com.pdfjet;
+
+public class IBMPlexSansHebrew {
+    public static final String Bold         = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Bold.otf.stream";
+    public static final String ExtraLight   = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-ExtraLight.otf.stream";
+    public static final String Light        = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Light.otf.stream";
+    public static final String Medium       = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Medium.otf.stream";
+    public static final String Regular      = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Regular.otf.stream";
+    public static final String SemiBold     = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-SemiBold.otf.stream";
+    public static final String Text         = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Text.otf.stream";
+    public static final String Thin         = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Thin.otf.stream";
+}
