7d7638d3cf18be955d8293ef94968140dff286f5
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 08:56:37 -0400

    Added one more font helper class

---
+++ net/pdfjet/Fonts/IBMPlexSansArabic.cs
@@ -0,0 +1,12 @@
+namespace PDFjet.NET {
+        public static class IBMPlexSansArabic {
+        public const string Bold        = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Bold.otf.stream";
+        public const string ExtraLight  = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-ExtraLight.otf.stream";
+        public const string Light       = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Light.otf.stream";
+        public const string Medium      = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Medium.otf.stream";
+        public const string Regular     = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Regular.otf.stream";
+        public const string SemiBold    = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-SemiBold.otf.stream";
+        public const string Text        = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Text.otf.stream";
+        public const string Thin        = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Thin.otf.stream";
+    }
+}
