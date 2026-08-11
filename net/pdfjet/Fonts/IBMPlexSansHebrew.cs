e11a0eda3203e315764ec0d99abe224fedce1f50
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 08:57:31 -0400

    Added one more helper class

---
+++ net/pdfjet/Fonts/IBMPlexSansHebrew.cs
@@ -0,0 +1,12 @@
+namespace PDFjet.NET {
+        public static class IBMPlexSansHebrew {
+        public const string Bold        = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Bold.otf.stream";
+        public const string ExtraLight  = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-ExtraLight.otf.stream";
+        public const string Light       = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Light.otf.stream";
+        public const string Medium      = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Medium.otf.stream";
+        public const string Regular     = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Regular.otf.stream";
+        public const string SemiBold    = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-SemiBold.otf.stream";
+        public const string Text        = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Text.otf.stream";
+        public const string Thin        = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Thin.otf.stream";
+    }
+}
