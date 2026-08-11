2bd7b1173a05a495fb14fd2c1f478a1cec0463b4
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 09:02:20 -0400

    Added two more font helper classes

---
+++ src/IBMPlexSansArabic/ibmplexsansarabic.go
@@ -0,0 +1,13 @@
+package IBMPlexSansArabic
+
+// IBMPlexSansArabic provides the paths to the IBM Plex Sans Arabic fonts.
+const (
+	Bold       = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Bold.otf.stream"
+	ExtraLight = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-ExtraLight.otf.stream"
+	Light      = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Light.otf.stream"
+	Medium     = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Medium.otf.stream"
+	Regular    = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Regular.otf.stream"
+	SemiBold   = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-SemiBold.otf.stream"
+	Text       = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Text.otf.stream"
+	Thin       = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Thin.otf.stream"
+)
---
+++ src/IBMPlexSansHebrew/ibmplexsanshebrew.go
@@ -0,0 +1,13 @@
+package IBMPlexSansHebrew
+
+// IBMPlexSansHebrew provides the paths to the IBM Plex Sans Hebrew fonts.
+const (
+	Bold       = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Bold.otf.stream"
+	ExtraLight = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-ExtraLight.otf.stream"
+	Light      = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Light.otf.stream"
+	Medium     = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Medium.otf.stream"
+	Regular    = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Regular.otf.stream"
+	SemiBold   = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-SemiBold.otf.stream"
+	Text       = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Text.otf.stream"
+	Thin       = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Thin.otf.stream"
+)
