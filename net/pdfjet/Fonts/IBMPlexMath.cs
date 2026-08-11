89a82bb1281cea99934782efb156e11792fbfd4e
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 07:29:23 -0400

    Added two more font helper classes

---
+++ net/pdfjet/Fonts/IBMPlexMath.cs
@@ -0,0 +1,5 @@
+namespace PDFjet.NET {
+    public static class IBMPlexMath {
+        public const string Regular          = "fonts/IBMPlexMath/IBMPlexMath-Regular.otf.stream";
+    }
+}
---
+++ net/pdfjet/Fonts/IBMPlexMono.cs
@@ -0,0 +1,20 @@
+namespace PDFjet.NET {
+    public static class IBMPlexMono {
+        public const string BoldItalic       = "fonts/IBMPlexMono/IBMPlexMono-BoldItalic.otf.stream";
+        public const string Bold             = "fonts/IBMPlexMono/IBMPlexMono-Bold.otf.stream";
+        public const string ExtraLightItalic = "fonts/IBMPlexMono/IBMPlexMono-ExtraLightItalic.otf.stream";
+        public const string ExtraLight       = "fonts/IBMPlexMono/IBMPlexMono-ExtraLight.otf.stream";
+        public const string Italic           = "fonts/IBMPlexMono/IBMPlexMono-Italic.otf.stream";
+        public const string LightItalic      = "fonts/IBMPlexMono/IBMPlexMono-LightItalic.otf.stream";
+        public const string Light            = "fonts/IBMPlexMono/IBMPlexMono-Light.otf.stream";
+        public const string MediumItalic     = "fonts/IBMPlexMono/IBMPlexMono-MediumItalic.otf.stream";
+        public const string Medium           = "fonts/IBMPlexMono/IBMPlexMono-Medium.otf.stream";
+        public const string Regular          = "fonts/IBMPlexMono/IBMPlexMono-Regular.otf.stream";
+        public const string SemiBoldItalic   = "fonts/IBMPlexMono/IBMPlexMono-SemiBoldItalic.otf.stream";
+        public const string SemiBold         = "fonts/IBMPlexMono/IBMPlexMono-SemiBold.otf.stream";
+        public const string TextItalic       = "fonts/IBMPlexMono/IBMPlexMono-TextItalic.otf.stream";
+        public const string Text             = "fonts/IBMPlexMono/IBMPlexMono-Text.otf.stream";
+        public const string ThinItalic       = "fonts/IBMPlexMono/IBMPlexMono-ThinItalic.otf.stream";
+        public const string Thin             = "fonts/IBMPlexMono/IBMPlexMono-Thin.otf.stream";
+    }
+}
