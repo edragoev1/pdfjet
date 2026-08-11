3d597a9ab481978222db017b9ba83fe35bcdcd1f
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 14:06:53 -0400

    Added two more font helper classes

---
+++ net/pdfjet/Fonts/IBMPlexSansDevanagari.cs
@@ -0,0 +1,12 @@
+namespace PDFjet.NET {
+        public static class IBMPlexSansDevanagari {
+        public const string Bold        = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Bold.otf.stream";
+        public const string ExtraLight  = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-ExtraLight.otf.stream";
+        public const string Light       = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Light.otf.stream";
+        public const string Medium      = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Medium.otf.stream";
+        public const string Regular     = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Regular.otf.stream";
+        public const string SemiBold    = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-SemiBold.otf.stream";
+        public const string Text        = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Text.otf.stream";
+        public const string Thin        = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Thin.otf.stream";
+    }
+}
---
+++ net/pdfjet/Fonts/IBMPlexSansThai.cs
@@ -0,0 +1,12 @@
+namespace PDFjet.NET {
+        public static class IBMPlexSansThai {
+        public const string Bold        = "fonts/IBMPlexSansThai/IBMPlexSansThai-Bold.otf.stream";
+        public const string ExtraLight  = "fonts/IBMPlexSansThai/IBMPlexSansThai-ExtraLight.otf.stream";
+        public const string Light       = "fonts/IBMPlexSansThai/IBMPlexSansThai-Light.otf.stream";
+        public const string Medium      = "fonts/IBMPlexSansThai/IBMPlexSansThai-Medium.otf.stream";
+        public const string Regular     = "fonts/IBMPlexSansThai/IBMPlexSansThai-Regular.otf.stream";
+        public const string SemiBold    = "fonts/IBMPlexSansThai/IBMPlexSansThai-SemiBold.otf.stream";
+        public const string Text        = "fonts/IBMPlexSansThai/IBMPlexSansThai-Text.otf.stream";
+        public const string Thin        = "fonts/IBMPlexSansThai/IBMPlexSansThai-Thin.otf.stream";
+    }
+}
