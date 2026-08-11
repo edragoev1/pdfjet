c4c04f368a64e80f73fe5e9fcd6d5c68eb153a6a
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 14:17:09 -0400

    Added two more font helper classes

---
+++ src/IBMPlexSansDevanagari/ibmplexsans.go
@@ -0,0 +1,13 @@
+package IBMPlexSansDevanagari
+
+// IBMPlexSansDevanagari provides the paths to the IBM Plex Sans Devanagari fonts.
+const (
+	Bold       = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Bold.otf.stream"
+	ExtraLight = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-ExtraLight.otf.stream"
+	Light      = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Light.otf.stream"
+	Medium     = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Medium.otf.stream"
+	Regular    = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Regular.otf.stream"
+	SemiBold   = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-SemiBold.otf.stream"
+	Text       = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Text.otf.stream"
+	Thin       = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Thin.otf.stream"
+)
---
+++ src/IBMPlexSansThai/ibmplexsans.go
@@ -0,0 +1,13 @@
+package IBMPlexSansThai
+
+// IBMPlexSansThai provides the paths to the IBM Plex Sans Thai fonts.
+const (
+	Bold       = "fonts/IBMPlexSansThai/IBMPlexSansThai-Bold.otf.stream"
+	ExtraLight = "fonts/IBMPlexSansThai/IBMPlexSansThai-ExtraLight.otf.stream"
+	Light      = "fonts/IBMPlexSansThai/IBMPlexSansThai-Light.otf.stream"
+	Medium     = "fonts/IBMPlexSansThai/IBMPlexSansThai-Medium.otf.stream"
+	Regular    = "fonts/IBMPlexSansThai/IBMPlexSansThai-Regular.otf.stream"
+	SemiBold   = "fonts/IBMPlexSansThai/IBMPlexSansThai-SemiBold.otf.stream"
+	Text       = "fonts/IBMPlexSansThai/IBMPlexSansThai-Text.otf.stream"
+	Thin       = "fonts/IBMPlexSansThai/IBMPlexSansThai-Thin.otf.stream"
+)
