1b9956762c8e07e753b6da3091ca5083df38bf84
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 07:35:21 -0400

    Added two more font helper classes

---
+++ src/IBMPlexMath/ibmplexmath.go
@@ -0,0 +1,6 @@
+package IBMPlexMath
+
+// IBMPlexMath provides the paths to the IBM Plex Math fonts.
+const (
+	Regular = "fonts/IBMPlexMath/IBMPlexMath-Regular.otf.stream"
+)
---
+++ src/IBMPlexMono/ibmplexmono.go
@@ -0,0 +1,21 @@
+package IBMPlexMono
+
+// IBMPlexMono provides the paths to the IBM Plex Mono fonts.
+const (
+	BoldItalic       = "fonts/IBMPlexMono/IBMPlexMono-BoldItalic.otf.stream"
+	Bold             = "fonts/IBMPlexMono/IBMPlexMono-Bold.otf.stream"
+	ExtraLightItalic = "fonts/IBMPlexMono/IBMPlexMono-ExtraLightItalic.otf.stream"
+	ExtraLight       = "fonts/IBMPlexMono/IBMPlexMono-ExtraLight.otf.stream"
+	Italic           = "fonts/IBMPlexMono/IBMPlexMono-Italic.otf.stream"
+	LightItalic      = "fonts/IBMPlexMono/IBMPlexMono-LightItalic.otf.stream"
+	Light            = "fonts/IBMPlexMono/IBMPlexMono-Light.otf.stream"
+	MediumItalic     = "fonts/IBMPlexMono/IBMPlexMono-MediumItalic.otf.stream"
+	Medium           = "fonts/IBMPlexMono/IBMPlexMono-Medium.otf.stream"
+	Regular          = "fonts/IBMPlexMono/IBMPlexMono-Regular.otf.stream"
+	SemiBoldItalic   = "fonts/IBMPlexMono/IBMPlexMono-SemiBoldItalic.otf.stream"
+	SemiBold         = "fonts/IBMPlexMono/IBMPlexMono-SemiBold.otf.stream"
+	TextItalic       = "fonts/IBMPlexMono/IBMPlexMono-TextItalic.otf.stream"
+	Text             = "fonts/IBMPlexMono/IBMPlexMono-Text.otf.stream"
+	ThinItalic       = "fonts/IBMPlexMono/IBMPlexMono-ThinItalic.otf.stream"
+	Thin             = "fonts/IBMPlexMono/IBMPlexMono-Thin.otf.stream"
+)
