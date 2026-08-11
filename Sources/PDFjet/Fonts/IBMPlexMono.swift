65f1346999f9a5385b1e871b1ee106a0d56a5e1c
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 07:38:00 -0400

    Added one more font helper class

---
+++ Sources/PDFjet/Fonts/IBMPlexMono.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public struct IBMPlexMono {
+	public static let BoldItalic       = "fonts/IBMPlexMono/IBMPlexMono-BoldItalic.otf.stream";
+	public static let Bold             = "fonts/IBMPlexMono/IBMPlexMono-Bold.otf.stream";
+	public static let ExtraLightItalic = "fonts/IBMPlexMono/IBMPlexMono-ExtraLightItalic.otf.stream";
+	public static let ExtraLight       = "fonts/IBMPlexMono/IBMPlexMono-ExtraLight.otf.stream";
+	public static let Italic           = "fonts/IBMPlexMono/IBMPlexMono-Italic.otf.stream";
+	public static let LightItalic      = "fonts/IBMPlexMono/IBMPlexMono-LightItalic.otf.stream";
+	public static let Light            = "fonts/IBMPlexMono/IBMPlexMono-Light.otf.stream";
+	public static let MediumItalic     = "fonts/IBMPlexMono/IBMPlexMono-MediumItalic.otf.stream";
+	public static let Medium           = "fonts/IBMPlexMono/IBMPlexMono-Medium.otf.stream";
+	public static let Regular          = "fonts/IBMPlexMono/IBMPlexMono-Regular.otf.stream";
+	public static let SemiBoldItalic   = "fonts/IBMPlexMono/IBMPlexMono-SemiBoldItalic.otf.stream";
+	public static let SemiBold         = "fonts/IBMPlexMono/IBMPlexMono-SemiBold.otf.stream";
+	public static let TextItalic       = "fonts/IBMPlexMono/IBMPlexMono-TextItalic.otf.stream";
+	public static let Text             = "fonts/IBMPlexMono/IBMPlexMono-Text.otf.stream";
+	public static let ThinItalic       = "fonts/IBMPlexMono/IBMPlexMono-ThinItalic.otf.stream";
+	public static let Thin             = "fonts/IBMPlexMono/IBMPlexMono-Thin.otf.stream";
+}
