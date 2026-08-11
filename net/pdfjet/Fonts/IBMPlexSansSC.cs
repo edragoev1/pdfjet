81fd1446b7b5349993efb59784ec5a843776ee49
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 06:07:32 -0400

    Added these font helper classes to the project.

---
+++ net/pdfjet/Fonts/IBMPlexSansSC.cs
@@ -0,0 +1,12 @@
+namespace PDFjet.NET {
+        public static class IBMPlexSansSC {
+        public const string Bold        = "fonts/IBMPlexSansSC/IBMPlexSansSC-Bold.otf";
+        public const string ExtraLight  = "fonts/IBMPlexSansSC/IBMPlexSansSC-ExtraLight.otf";
+        public const string Light       = "fonts/IBMPlexSansSC/IBMPlexSansSC-Light.otf";
+        public const string Medium      = "fonts/IBMPlexSansSC/IBMPlexSansSC-Medium.otf";
+        public const string Regular     = "fonts/IBMPlexSansSC/IBMPlexSansSC-Regular.otf";
+        public const string SemiBold    = "fonts/IBMPlexSansSC/IBMPlexSansSC-SemiBold.otf";
+        public const string Text        = "fonts/IBMPlexSansSC/IBMPlexSansSC-Text.otf";
+        public const string Thin        = "fonts/IBMPlexSansSC/IBMPlexSansSC-Thin.otf";
+    }
+}
---
+++ net/pdfjet/Fonts/IBMPlexSansTC.cs
@@ -0,0 +1,12 @@
+namespace PDFjet.NET {
+        public static class IBMPlexSansTC {
+        public const string Bold        = "fonts/IBMPlexSansTC/IBMPlexSansTC-Bold.otf";
+        public const string ExtraLight  = "fonts/IBMPlexSansTC/IBMPlexSansTC-ExtraLight.otf";
+        public const string Light       = "fonts/IBMPlexSansTC/IBMPlexSansTC-Light.otf";
+        public const string Medium      = "fonts/IBMPlexSansTC/IBMPlexSansTC-Medium.otf";
+        public const string Regular     = "fonts/IBMPlexSansTC/IBMPlexSansTC-Regular.otf";
+        public const string SemiBold    = "fonts/IBMPlexSansTC/IBMPlexSansTC-SemiBold.otf";
+        public const string Text        = "fonts/IBMPlexSansTC/IBMPlexSansTC-Text.otf";
+        public const string Thin        = "fonts/IBMPlexSansTC/IBMPlexSansTC-Thin.otf";
+    }
+}
