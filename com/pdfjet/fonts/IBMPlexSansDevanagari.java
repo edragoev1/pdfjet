2d29f51a7bc3a80eb2ccc324e1b2f54a7d5fd5ca
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 14:02:36 -0400

    Added two more font helper classes

---
+++ com/pdfjet/fonts/IBMPlexSansDevanagari.java
@@ -0,0 +1,12 @@
+package com.pdfjet;
+
+public class IBMPlexSansDevanagari {
+    public static final String Bold         = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Bold.otf.stream";
+    public static final String ExtraLight   = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-ExtraLight.otf.stream";
+    public static final String Light        = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Light.otf.stream";
+    public static final String Medium       = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Medium.otf.stream";
+    public static final String Regular      = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Regular.otf.stream";
+    public static final String SemiBold     = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-SemiBold.otf.stream";
+    public static final String Text         = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Text.otf.stream";
+    public static final String Thin         = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Thin.otf.stream";
+}
---
+++ com/pdfjet/fonts/IBMPlexSansThai.java
@@ -0,0 +1,12 @@
+package com.pdfjet;
+
+public class IBMPlexSansThai {
+    public static final String Bold         = "fonts/IBMPlexSansThai/IBMPlexSansThai-Bold.otf.stream";
+    public static final String ExtraLight   = "fonts/IBMPlexSansThai/IBMPlexSansThai-ExtraLight.otf.stream";
+    public static final String Light        = "fonts/IBMPlexSansThai/IBMPlexSansThai-Light.otf.stream";
+    public static final String Medium       = "fonts/IBMPlexSansThai/IBMPlexSansThai-Medium.otf.stream";
+    public static final String Regular      = "fonts/IBMPlexSansThai/IBMPlexSansThai-Regular.otf.stream";
+    public static final String SemiBold     = "fonts/IBMPlexSansThai/IBMPlexSansThai-SemiBold.otf.stream";
+    public static final String Text         = "fonts/IBMPlexSansThai/IBMPlexSansThai-Text.otf.stream";
+    public static final String Thin         = "fonts/IBMPlexSansThai/IBMPlexSansThai-Thin.otf.stream";
+}
