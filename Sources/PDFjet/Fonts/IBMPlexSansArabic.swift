a09471b2d19e4c73ebae7c72c136dd43dc1aa13d
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 09:05:00 -0400

    Added two more font helper classes

---
+++ Sources/PDFjet/Fonts/IBMPlexSansArabic.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public struct IBMPlexSansArabic {
+	public static let Bold          = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Bold.otf.stream";
+	public static let ExtraLight    = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-ExtraLight.otf.stream";
+	public static let Light         = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Light.otf.stream";
+	public static let Medium        = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Medium.otf.stream";
+	public static let Regular       = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Regular.otf.stream";
+	public static let SemiBold      = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-SemiBold.otf.stream";
+	public static let Text          = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Text.otf.stream";
+	public static let Thin          = "fonts/IBMPlexSansArabic/IBMPlexSansArabic-Thin.otf.stream";
+}
---
+++ Sources/PDFjet/Fonts/IBMPlexSansHebrew.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public struct IBMPlexSansHebrew {
+	public static let Bold          = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Bold.otf.stream";
+	public static let ExtraLight    = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-ExtraLight.otf.stream";
+	public static let Light         = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Light.otf.stream";
+	public static let Medium        = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Medium.otf.stream";
+	public static let Regular       = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Regular.otf.stream";
+	public static let SemiBold      = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-SemiBold.otf.stream";
+	public static let Text          = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Text.otf.stream";
+	public static let Thin          = "fonts/IBMPlexSansHebrew/IBMPlexSansHebrew-Thin.otf.stream";
+}
