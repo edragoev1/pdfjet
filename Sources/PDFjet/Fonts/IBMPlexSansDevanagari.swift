fbf77df181ba15e3598400c445029d9acda8c262
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-11 14:20:31 -0400

    Added the last 2 font helper files

---
+++ Sources/PDFjet/Fonts/IBMPlexSansDevanagari.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public struct IBMPlexSansDevanagari {
+	public static let Bold          = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Bold.otf.stream";
+	public static let ExtraLight    = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-ExtraLight.otf.stream";
+	public static let Light         = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Light.otf.stream";
+	public static let Medium        = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Medium.otf.stream";
+	public static let Regular       = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Regular.otf.stream";
+	public static let SemiBold      = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-SemiBold.otf.stream";
+	public static let Text          = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Text.otf.stream";
+	public static let Thin          = "fonts/IBMPlexSansDevanagari/IBMPlexSansDevanagari-Thin.otf.stream";
+}
---
+++ Sources/PDFjet/Fonts/IBMPlexSansThai.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public struct IBMPlexSansThai {
+	public static let Bold          = "fonts/IBMPlexSansThai/IBMPlexSansThai-Bold.otf.stream";
+	public static let ExtraLight    = "fonts/IBMPlexSansThai/IBMPlexSansThai-ExtraLight.otf.stream";
+	public static let Light         = "fonts/IBMPlexSansThai/IBMPlexSansThai-Light.otf.stream";
+	public static let Medium        = "fonts/IBMPlexSansThai/IBMPlexSansThai-Medium.otf.stream";
+	public static let Regular       = "fonts/IBMPlexSansThai/IBMPlexSansThai-Regular.otf.stream";
+	public static let SemiBold      = "fonts/IBMPlexSansThai/IBMPlexSansThai-SemiBold.otf.stream";
+	public static let Text          = "fonts/IBMPlexSansThai/IBMPlexSansThai-Text.otf.stream";
+	public static let Thin          = "fonts/IBMPlexSansThai/IBMPlexSansThai-Thin.otf.stream";
+}
