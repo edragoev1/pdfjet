aad43ab6b4630db85ccf35a184da348671184471
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-14 21:38:17 -0400

    Updated the example

--- examples/Example_45/Example_45.cs
+++ examples/Example_45/Example_45.cs
@@ -2,77 +2,48 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
+
 using PDFjet.NET;
 
 /**
- * Example_45.cs
+ * Example_45.java
  */
 public class Example_45 {
     public Example_45() {
         PDF pdf = new PDF(new BufferedStream(
-            new FileStream("Example_45.pdf", FileMode.Create)));
+                new FileStream("Example_45.pdf", FileMode.Create)));
 
-        Font f1 = new Font(pdf, IBMPlexSans.Bold);
-        f1.SetSize(12f);
-
-        Font f2 = new Font(pdf, IBMPlexSans.Italic);
-        f2.SetSize(12f);
-
-        Font f3 = new Font(pdf, JetBrainsMono.Regular);
-        f3.SetSize(10f);
+        Font f1 = new Font(pdf, CoreFont.HELVETICA_BOLD);
+        Font f2 = new Font(pdf, CoreFont.HELVETICA);
 
         Page page = new Page(pdf, Letter.PORTRAIT);
 
-        float w = 530f;
-        float h = 15f;
+        float w = 500f;
 
         List<Field> fields = new List<Field>();
-        fields.Add(new Field(   0f, new String[] {"Company", "Smart Widget Designs"}));
-        fields.Add(new Field(   0f, new String[] {"Street Number", "120"}));
-        fields.Add(new Field(  w/8, new String[] {"Street Name", "Oak"}));
-        fields.Add(new Field(5*w/8, new String[] {"Street Type", "Street"}));
-        fields.Add(new Field(6*w/8, new String[] {"Direction", "West"}));
-        fields.Add(new Field(7*w/8, new String[] {"Suite/Floor/Apt.", "8W"})
-                .SetAltDescription("Suite/Floor/Apartment")
-                .SetActualText("Suite/Floor/Apartment"));
-        fields.Add(new Field(   0f, new String[] {"City/Town", "Toronto"}));
-        fields.Add(new Field(  w/2, new String[] {"Province", "Ontario"}));
-        fields.Add(new Field(7*w/8, new String[] {"Postal Code", "M5M 2N2"}));
-        fields.Add(new Field(   0f, new String[] {"Telephone Number", "(416) 331-2245"}));
-        fields.Add(new Field(  w/4, new String[] {"Fax (if applicable)", "(416) 124-9879"}));
-        fields.Add(new Field(  w/2, new String[] {"Email","jsmith12345@gmail.ca"}));
-        fields.Add(new Field(   0f, new String[] {
-                "Other Information", "We don't work on weekends.", "Please send us an Email."}));
+        fields.Add(new Field(   0f, "Company", "Smart Widgets Construction Inc."));
+        fields.Add(new Field(   0f, "Street Number", "120"));
+        fields.Add(new Field(  w/8, "Street Name", "Oak"));
+        fields.Add(new Field(4*w/8, "Street Type", "Street"));
+        fields.Add(new Field(5*w/8, "Direction", "West"));
+        fields.Add(new Field(6*w/8, "Suite/Floor/Apartment", "8W"));
+        fields.Add(new Field(   0f, "City/Town", "Toronto"));
+        fields.Add(new Field(  w/2, "Province", "Ontario"));
+        fields.Add(new Field(7*w/8, "Postal Code", "M5M 2N2"));
+        fields.Add(new Field(   0f, "Telephone Number", "(416) 331-2245"));
+        fields.Add(new Field(  w/4, "Fax (if applicable)", "(416) 124-9879"));
+        fields.Add(new Field(  w/2, "Email","jsmith12345@gmail.ca"));
+        fields.Add(new Field(   0f, "Other Information", "We don't work on weekends."));
+        fields.Add(new Field(   0f, "", "Please send us an Email."));
 
-        new Form(fields)
+        float[] xy = (new Form(fields)
                 .SetLabelFont(f1)
-                .SetLabelFontSize(7f)
+                .SetLabelFontSize(8f)
                 .SetValueFont(f2)
-                .SetValueFontSize(9f)
+                .SetValueFontSize(10f)
                 .SetLocation(50f, 50f)
-                .SetRowLength(w)
-                .SetRowHeight(h)
-                .DrawOn(page);
-
-        Dictionary<String, int> colors = new Dictionary<String, int>();
-        colors["new"] = Color.red;
-        colors["ArrayList"] = Color.blue;
-        colors["List"] = Color.blue;
-        colors["String"] = Color.blue;
-        colors["Field"] = Color.blue;
-        colors["Form"] = Color.blue;
-        colors["Smart"] = Color.green;
-        colors["Widget"] = Color.green;
-        colors["Designs"] = Color.green;
-
-        float x = 50;
-        float y = 280;
-        float dy = f3.GetBodyHeight(f3.GetSize());
-        List<String> lines = Text.ReadLines("data/languages/english.txt");
-        foreach (String line in lines) {
-            page.DrawString(f3, null, f3.GetSize(), line, x, y, Color.gray, colors);
-            y += dy;
-        }
+                .SetFormWidth(w)
+                .DrawOn(page));
 
         pdf.Complete();
     }
@@ -85,5 +56,4 @@ public class Example_45 {
         sw.Stop();
         TextUtils.PrintDuration("Example_45", time0, time1);
     }
-
 }   // End of Example_45.cs
