642c4fff41ed6f6c59702f234ec239345ed23a88
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-14 23:23:33 -0400

    Fixed the example 45

--- Sources/Example_45/main.swift
+++ Sources/Example_45/main.swift
@@ -5,79 +5,46 @@ import PDFjet
  * Example_45.swift
  */
 public class Example_45 {
-    public init() throws {
-        let stream = OutputStream(toFileAtPath: "Example_45.pdf", append: false)
-        let pdf = PDF(stream!)
-        pdf.setLanguage("en-US")
-        pdf.setTitle("Hello, World!")
+    public init() {
+        let pdf = PDF(OutputStream(toFileAtPath: "Example_45.pdf", append: false)!)
 
-        let f1 = try Font(pdf, SourceSerif4.Regular)
-        f1.setSize(14.0)
-
-        let f2 = try Font(pdf, SourceSerif4.Italic)
-        f2.setSize(14.0)
-
-        let f3 = try Font(pdf, JetBrainsMono.Regular)
-        f3.setSize(10.0)
+        let f1 = Font(pdf, CoreFont.HELVETICA_BOLD)
+        let f2 = Font(pdf, CoreFont.HELVETICA)
 
         let page = Page(pdf, Letter.PORTRAIT)
 
-        let w: Float = 530.0
-        let h: Float = 15.0
+        let w: Float = 500.0
 
         var fields = [Field]()
-        fields.append(Field(  0.0, ["Company", "Smart Widget Designs"]))
-        fields.append(Field(  0.0, ["Street Number", "120"]))
-        fields.append(Field(  w/8, ["Street Name", "Oak"]))
-        fields.append(Field(5*w/8, ["Street Type", "Street"]))
-        fields.append(Field(6*w/8, ["Direction", "West"]))
-        fields.append(Field(7*w/8, ["Suite/Floor/Apt.", "8W"])
-                .setAltDescription("Suite/Floor/Apartment")
-                .setActualText("Suite/Floor/Apartment"))
-        fields.append(Field(  0.0, ["City/Town", "Toronto"]))
-        fields.append(Field(  w/2, ["Province", "Ontario"]))
-        fields.append(Field(7*w/8, ["Postal Code", "M5M 2N2"]))
-        fields.append(Field(  0.0, ["Telephone Number", "(416) 331-2245"]))
-        fields.append(Field(  w/4, ["Fax (if applicable)", "(416) 124-9879"]))
-        fields.append(Field(  w/2, ["Email","jsmith12345@gmail.ca"]))
-        fields.append(Field(  0.0, [
-                "Other Information","We don't work on weekends.", "Please send us an Email."]))
+        fields.append(Field(  0.0, "Company", "Smart Widgets Construction Inc."))
+        fields.append(Field(  0.0, "Street Number", "120"))
+        fields.append(Field(  w/8, "Street Name", "Oak"))
+        fields.append(Field(4*w/8, "Street Type", "Street"))
+        fields.append(Field(5*w/8, "Direction", "West"))
+        fields.append(Field(6*w/8, "Suite/Floor/Apartment", "8W"))
+        fields.append(Field(  0.0, "City/Town", "Toronto"))
+        fields.append(Field(4*w/8, "Province", "Ontario"))
+        fields.append(Field(7*w/8, "Postal Code", "M5M 2N2"))
+        fields.append(Field(  0.0, "Telephone Number", "(416) 331-2245"))
+        fields.append(Field(2*w/8, "Fax (if applicable)", "(416) 124-9879"))
+        fields.append(Field(4*w/8, "Email","jsmith12345@gmail.ca"))
+        fields.append(Field(  0.0, "Other Information", "We don't work on weekends."))
+        fields.append(Field(  0.0, "", "Please send us an Email."))
 
-        let form = Form(fields)
-        form.setLabelFont(f1)
-        form.setLabelFontSize(7.0)
-        form.setValueFont(f2)
-        form.setValueFontSize(9.0)
-        form.setLocation(50.0, 50.0)
-        form.setRowLength(w)
-        form.setRowHeight(h)
-        form.drawOn(page)
-
-        var colors = Dictionary<String, Int32>()
-        colors["new"] = Color.red
-        colors["ArrayList"] = Color.blue
-        colors["List"] = Color.blue
-        colors["String"] = Color.blue
-        colors["Field"] = Color.blue
-        colors["Form"] = Color.blue
-        colors["Smart"] = Color.green
-        colors["Widget"] = Color.green
-        colors["Designs"] = Color.green
-
-        let x: Float32 = 50.0
-        var y: Float32 = 280.0
-        let dy = f3.getBodyHeight()
-        let lines = try Text.readLines("data/languages/english.txt")
-        for line in lines {
-            page.drawString(f3, f3.getSize(), line, x, y, [0.0, 0.0, 0.0], colors)
-            y += dy
-        }
+        Form(fields)
+                .setLabelFont(f1)
+                .setLabelFontSize(8.0)
+                .setValueFont(f2)
+                .setValueFontSize(10.0)
+                .setLocation(50.0, 50.0)
+                .setFormWidth(w)
+                .drawOn(page)
 
         pdf.complete()
     }
 }   // End of Example_45.swift
 
 let time0 = Int64(Date().timeIntervalSince1970 * 1000)
-_ = try Example_45()
+_ = Example_45()
 let time1 = Int64(Date().timeIntervalSince1970 * 1000)
 TextUtils.printDuration("Example_45", time0, time1)
