1b66a9dba13fb80c6496dcd2e05164493d990828
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-15 13:47:27 -0400

    Nice cleanup ...

--- Sources/Example_42/main.swift
+++ Sources/Example_42/main.swift
@@ -5,31 +5,32 @@ import PDFjet
  * Example_42.swift
  */
 public class Example_42 {
-    public init() {
+    public init() throws {
         let pdf = PDF(OutputStream(toFileAtPath: "Example_42.pdf", append: false)!)
 
-        let f1 = Font(pdf, CoreFont.HELVETICA_BOLD)
-        let f2 = Font(pdf, CoreFont.HELVETICA)
+        let f1 = try Font(pdf, IBMPlexSans.Regular)
+        let f2 = try Font(pdf, IBMPlexSans.SemiBold)
 
         let page = Page(pdf, Letter.PORTRAIT)
 
         let w: Float = 500.0
 
         var fields = [Field]()
-        fields.append(Field(  0.0, "Company", "Smart Widgets Construction Inc."))
-        fields.append(Field(  0.0, "Street Number", "120"))
+        fields.append(Field(  0.0, "Company", "Smart Widgets Inc."))
+        fields.append(Field(  0.0, "Street #", "120"))
         fields.append(Field(  w/8, "Street Name", "Oak"))
         fields.append(Field(4*w/8, "Street Type", "Street"))
         fields.append(Field(5*w/8, "Direction", "West"))
         fields.append(Field(6*w/8, "Suite/Floor/Apartment", "8W"))
         fields.append(Field(  0.0, "City/Town", "Toronto"))
         fields.append(Field(4*w/8, "Province", "Ontario"))
-        fields.append(Field(7*w/8, "Postal Code", "M5M 2N2"))
-        fields.append(Field(  0.0, "Telephone Number", "(416) 331-2245"))
-        fields.append(Field(2*w/8, "Fax (if applicable)", "(416) 124-9879"))
+        fields.append(Field(7*w/8, "Postal Code", "M5M 2N2"));
+        fields.append(Field(  0.0, "Telephone #", "(416) 331-2245"))
+        fields.append(Field(2*w/8, "Fax #", "(416) 124-9879"))
         fields.append(Field(4*w/8, "Email","jsmith12345@gmail.ca"))
-        fields.append(Field(  0.0, "Other Information", "We don't work on weekends."))
-        fields.append(Field(  0.0, "", "Please send us an Email."))
+        fields.append(Field(  0.0, "Other Information",
+            "Smart Widgets Inc. designs intelligent IoT widgets that connect everyday appliances to cloud ecosystems,"))
+        fields.append(Field(  0.0, "", "enabling remote control and predictive maintenance."))
 
         let xy = Form(fields)
                 .setLabelFont(f1)
@@ -51,6 +52,6 @@ public class Example_42 {
 }   // End of Example_42.swift
 
 let time0 = Int64(Date().timeIntervalSince1970 * 1000)
-_ = Example_42()
+_ = try Example_42()
 let time1 = Int64(Date().timeIntervalSince1970 * 1000)
 TextUtils.printDuration("Example_42", time0, time1)
