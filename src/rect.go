468fa30127f7a150dc70fc50fc256f779d6620f5
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-18 13:14:25 -0400

    Improved header comment

--- src/rect.go
+++ src/rect.go
@@ -1,9 +1,7 @@
-/**
- * rect.go
- *
- * Copyright (c) 2026 PDFjet Software
- * Licensed under the MIT License. See LICENSE file in the project root.
- */
+// rect.go
+//
+// Copyright (c) 2026 PDFjet Software
+// Licensed under the MIT License. See LICENSE file in the project root.
 
 package pdfjet
 
