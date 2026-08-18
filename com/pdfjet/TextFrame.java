57170961c7561752dc9c1cb7add8b42366651f54
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-18 07:46:13 -0400

    Refactored the code to make it simpler and easier to understand

--- com/pdfjet/TextFrame.java
+++ com/pdfjet/TextFrame.java
@@ -12,10 +12,9 @@ import java.util.*;
  * Please see Example_47
  */
 public class TextFrame implements Drawable {
-    private List<String> paragraphs;
+    private List<String> list;
     private Font f1 = null;
     private Font f2 = null;
-    private float fontSize;
     private float x;
     private float y;
     private float w;
@@ -23,24 +22,20 @@ public class TextFrame implements Drawable {
     private float leading;
     private float paragraphLeading;
     private boolean border;
-//    private final List<float[]> beginParagraphPoints;
+    private List<List<String>> paragraphs;
+    // private final List<float[]> beginParagraphPoints;
 
-    public TextFrame(Font f1, List<String> paragraphs) {
+    public TextFrame(Font f1, List<String> list) {
         this.f1 = f1;
-        this.paragraphs = paragraphs;
-//         this.font = paragraphs.get(0).getFont();
-//         this.fallbackFont = paragraphs.get(0).getFallbackFont();
-//         this.fontSize = font.size;
-//         this.leading = font.getBodyHeight(fontSize);
-//        this.paragraphLeading = leading;
-//        this.beginParagraphPoints = new ArrayList<float[]>();
-//         if (f2 != null && (f2.getBodyHeight(fontSize) > this.leading)) {
-//             this.leading = f2.getBodyHeight(fontSize);
-//         }
-
-        // this.leading = f1.getBodyHeight(fontSize);   // TODO!!!!
+        this.list = list;
         this.leading = f1.getAscent() + f1.getDescent();
-        Collections.reverse(this.paragraphs);
+        Collections.reverse(this.list);
+        paragraphs = new ArrayList<List<String>>();
+        for (String text : this.list) {
+            List<String> tokens = new ArrayList<>(Arrays.asList(text.split("\\s+")));
+            Collections.reverse(tokens);
+            paragraphs.add(tokens);
+        }
     }
 
     public TextFrame setLocation(float x, float y) {
@@ -75,31 +70,31 @@ public class TextFrame implements Drawable {
         return this.h;
     }
 
-    public TextFrame setLeading(float leading) {
-        this.leading = leading;
-        return this;
-    }
+//     public TextFrame setLeading(float leading) {
+//         this.leading = leading;
+//         return this;
+//     }
 
-    public TextFrame setLeading(double leading) {
-        return setLeading((float) leading);
-    }
+//     public TextFrame setLeading(double leading) {
+//         return setLeading((float) leading);
+//     }
+//
+//     public TextFrame setParagraphLeading(float paragraphLeading) {
+//         this.paragraphLeading = paragraphLeading;
+//         return this;
+//     }
+//
+//     public TextFrame setParagraphLeading(double paragraphLeading) {
+//         return setParagraphLeading((float) paragraphLeading);
+//     }
 
-    public TextFrame setParagraphLeading(float paragraphLeading) {
-        this.paragraphLeading = paragraphLeading;
-        return this;
-    }
-
-    public TextFrame setParagraphLeading(double paragraphLeading) {
-        return setParagraphLeading((float) paragraphLeading);
-    }
-
-    public void setParagraphs(List<String> paragraphs) {
-        this.paragraphs = paragraphs;
-    }
-
-    public List<String> getParagraphs() {
-        return this.paragraphs;
-    }
+//     public void setParagraphs(List<String> paragraphs) {
+//         this.paragraphs = paragraphs;
+//     }
+//
+//     public List<String> getParagraphs() {
+//         return this.paragraphs;
+//     }
 
 //     public List<float[]> getBeginParagraphPoints() {
 //         return this.beginParagraphPoints;
@@ -117,21 +112,16 @@ public class TextFrame implements Drawable {
         this.border = border;
     }
 
-    public void SetFontSize(float fontSize) {
-        this.fontSize = fontSize;
-    }
+//     public void SetFontSize(float fontSize) {
+//         this.fontSize = fontSize;
+//     }
 
 // TODO: Create the complete structure before the drawOn method!!!!
 // Do the check for reaching the bottom of the page immediately!!
     public float[] drawOn(Page page) throws Exception {
         float yText = y + f1.getAscent();
         while (paragraphs.size() > 0) {
-            String text = paragraphs.remove(paragraphs.size() - 1);
-
-            List<String> tokens = new ArrayList<>(Arrays.asList(text.split("\\s+")));
-            Collections.reverse(tokens);
-            List<String> temp = new ArrayList<>();
-
+            List<String> tokens = paragraphs.remove(paragraphs.size() - 1);
             StringBuilder sb = new StringBuilder();
             String token = null;
             while (tokens.size() > 0) {
@@ -139,18 +129,15 @@ public class TextFrame implements Drawable {
                 if (f1.stringWidth(sb.toString() + token) < this.w) {
                     sb.append(token);
                     sb.append(Single.space);
-                    temp.add(token);
                 } else {
                     if (yText + f1.getDescent() < (y + h)) {
                         TextLine textLine = new TextLine(f1, sb.toString().trim());
                         textLine.setLocation(x, yText);
                         textLine.drawOn(page);
                         sb.setLength(0);
-                        temp.clear();
-                        temp.add(token);
+                        tokens.add(token);
                         yText += leading;
                     } else {
-                        tokens.addAll(temp);
                         return new float[] {this.x + this.w, this.y + this.h};
                     }
                 }
@@ -161,9 +148,10 @@ public class TextFrame implements Drawable {
                     textLine.setLocation(x, yText);
                     textLine.drawOn(page);
                     sb.setLength(0);
+                    tokens.add(token);
                     yText += leading;
                 } else {
-                    tokens.addAll(temp);
+                    drawBorder(page);
                     return new float[] {this.x + this.w, this.y + this.h};
                 }
             }
