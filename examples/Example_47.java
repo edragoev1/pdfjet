2c5dce96a70608476bd1a3a7cab020e87ac2d7d0
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-18 09:06:02 -0400

    More cleanup

--- examples/Example_47.java
+++ examples/Example_47.java
@@ -32,7 +32,6 @@ public class Example_47 {
             frame.setLocation(x, y);
             frame.setWidth(w);
             frame.setHeight(h);
-            frame.setBorder(true);
             frame.drawOn(page);
 
             if (frame.isNotEmpty()) {
