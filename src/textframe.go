27ba12e8a504b991b8f719f5153ea704be6ae3c0
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-18 11:42:36 -0400

    Updated the textframe.go code

--- src/textframe.go
+++ src/textframe.go
@@ -8,163 +8,167 @@ package pdfjet
  */
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 
-	"github.com/edragoev1/pdfjet/src/single"
+	"github.com/edragoev1/pdfjet/src/color"
 )
 
-// TextFrame please see Example_47
+// TextFrame Please see Example_47
 type TextFrame struct {
-	lines                    []*TextLine
-	font                     *Font
-	fallbackFont             *Font
-	fontSize                 float32
-	x, y, w, h, xText, yText float32
-	leading                  float32
-	paragraphLeading         float32
-	beginParagraphPoints     [][]float32
-	border                   bool
+	f1          *Font
+	list        []string
+	x           float32
+	y           float32
+	w           float32
+	h           float32
+	leading     float32
+	border      bool
+	borderColor int32
+	paragraphs  [][]string
 }
 
-// NewTextFrame constructs new text frame.
-func NewTextFrame(lines []*TextLine) *TextFrame {
-	textFrame := new(TextFrame)
-	if lines != nil {
-		textFrame.lines = lines
-		textFrame.font = textFrame.lines[0].GetFont()
-		textFrame.fallbackFont = textFrame.lines[0].GetFallbackFont()
-		textFrame.fontSize = textFrame.lines[0].GetFontSize()
-		textFrame.leading = textFrame.font.GetBodyHeight(textFrame.font.GetSize())
-		textFrame.paragraphLeading = 2 * textFrame.leading
-		textFrame.beginParagraphPoints = make([][]float32, 0)
-		// Reverse the lines
-		for i, j := 0, len(lines)-1; i < j; i, j = i+1, j-1 {
-			lines[i], lines[j] = lines[j], lines[i]
-		}
+func NewTextFrame(f1 *Font, list []string) *TextFrame {
+	// Clone the input list
+	copiedList := make([]string, len(list))
+	copy(copiedList, list)
+
+	tf := &TextFrame{
+		f1:          f1,
+		list:        copiedList,
+		leading:     f1.GetAscent(f1.GetSize()) + f1.GetDescent(f1.GetSize()),
+		borderColor: color.Blue,
+		paragraphs:  make([][]string, 0),
 	}
-	return textFrame
-}
 
-// SetLocation sets the location of the frame on the page.
-func (frame *TextFrame) SetLocation(x, y float32) *TextFrame {
-	frame.x = x
-	frame.y = y
-	return frame
-}
+	// Reverse the list (like Java's Collections.reverse)
+	for i, j := 0, len(tf.list)-1; i < j; i, j = i+1, j-1 {
+		tf.list[i], tf.list[j] = tf.list[j], tf.list[i]
+	}
 
-// SetWidth sets the width of the frame.
-func (frame *TextFrame) SetWidth(w float32) *TextFrame {
-	frame.w = w
-	return frame
-}
-
-// SetHeight sets the height of the frame.
-func (frame *TextFrame) SetHeight(h float32) *TextFrame {
-	frame.h = h
-	return frame
-}
-
-// SetLeading sets the text lines leading.
-func (frame *TextFrame) SetLeading(leading float32) *TextFrame {
-	frame.leading = leading
-	return frame
-}
-
-// SetParagraphLeading sets the paragraph leading.
-func (frame *TextFrame) SetParagraphLeading(paragraphLeading float32) *TextFrame {
-	frame.paragraphLeading = paragraphLeading
-	return frame
-}
-
-// GetStartParagraphPoints returns the start paragraph points.
-func (frame *TextFrame) GetStartParagraphPoints() [][]float32 {
-	return frame.beginParagraphPoints
-}
-
-// GetParagraphs returns the lines.
-func (frame *TextFrame) GetParagraphs() []*TextLine {
-	return frame.lines
-}
-
-// SetPosition sets the position of the text frame on the page.
-func (frame *TextFrame) SetPosition(x, y float32) {
-	frame.SetLocation(x, y)
-}
-
-// SetBorder sets the border variable.
-func (frame *TextFrame) SetBorder(border bool) {
-	frame.border = border
-}
-
-// SetDrawBorder sets the border variable.
-func (frame *TextFrame) SetDrawBorder(border bool) {
-	frame.border = border
-}
-
-// SetFontSize sets the font size.
-func (frame *TextFrame) SetFontSize(fontSize float32) {
-	frame.fontSize = fontSize
-}
-
-// DrawOn draws the text frame on the page.
-func (frame *TextFrame) DrawOn(page *Page) []float32 {
-	frame.xText = frame.x
-	frame.yText = frame.y + frame.font.GetAscent(frame.fontSize)
-	for len(frame.lines) > 0 {
-		// The lines are reversed so we can efficiently remove the first one:
-		textLine := frame.lines[len(frame.lines)-1]
-		textLine.SetLocation(frame.xText, frame.yText)
-		frame.lines = frame.lines[:len(frame.lines)-1]
-		frame.beginParagraphPoints = append(frame.beginParagraphPoints, []float32{frame.xText, frame.yText})
-		for {
-			textLine = frame.drawLineOnPage(page, textLine)
-			if textLine.text == "" {
-				break
-			}
-			frame.yText = textLine.advance(frame.leading)
-			if frame.yText+frame.font.GetDescent(frame.fontSize) >= (frame.y + frame.h) {
-				// The lines are reversed so we can efficiently add new first paragraph:
-				frame.lines = append(frame.lines, textLine)
-				frame.DrawBorder(page)
-				return []float32{frame.x + frame.w, frame.y + frame.h}
+	// Tokenize paragraphs
+	re := regexp.MustCompile(`\s+`)
+	for _, text := range tf.list {
+		split := re.Split(strings.TrimSpace(text), -1)
+		tokens := make([]string, 0)
+		for _, token := range split {
+			if token != "" {
+				tokens = append(tokens, token)
 			}
 		}
-		frame.xText = frame.x
-		frame.yText += frame.paragraphLeading
+		// Reverse tokens (like Java's Collections.reverse)
+		for i, j := 0, len(tokens)-1; i < j; i, j = i+1, j-1 {
+			tokens[i], tokens[j] = tokens[j], tokens[i]
+		}
+		tf.paragraphs = append(tf.paragraphs, tokens)
 	}
-	frame.DrawBorder(page)
-	return []float32{frame.x + frame.w, frame.y + frame.h}
+
+	return tf
 }
 
-func (frame *TextFrame) DrawBorder(page *Page) {
-	if page != nil && frame.border {
-		rect := NewRect(frame.x, frame.y, frame.w, frame.h)
+func (tf *TextFrame) SetLocation(x, y float32) *TextFrame {
+	tf.x = x
+	tf.y = y
+	return tf
+}
+
+func (tf *TextFrame) SetLocationDouble(x, y float64) *TextFrame {
+	return tf.SetLocation(float32(x), float32(y))
+}
+
+func (tf *TextFrame) SetWidth(w float32) *TextFrame {
+	tf.w = w
+	return tf
+}
+
+func (tf *TextFrame) SetWidthDouble(w float64) *TextFrame {
+	return tf.SetWidth(float32(w))
+}
+
+func (tf *TextFrame) SetHeight(h float32) *TextFrame {
+	tf.h = h
+	return tf
+}
+
+func (tf *TextFrame) SetHeightDouble(h float64) *TextFrame {
+	return tf.SetHeight(float32(h))
+}
+
+func (tf *TextFrame) GetHeight() float32 {
+	return tf.h
+}
+
+func (tf *TextFrame) SetPosition(x, y float32) {
+	tf.SetLocation(x, y)
+}
+
+func (tf *TextFrame) SetBorder(border bool) {
+	tf.border = border
+}
+
+func (tf *TextFrame) SetBorderColor(borderColor int32) {
+	tf.borderColor = borderColor
+}
+
+func (tf *TextFrame) DrawOn(page *Page) ([]float32, error) {
+	if page == nil {
+		return nil, fmt.Errorf("page cannot be nil")
+	}
+
+	yText := tf.y + tf.f1.GetAscent(tf.f1.GetSize())
+	for len(tf.paragraphs) > 0 {
+		tokens := tf.paragraphs[len(tf.paragraphs)-1]
+		tf.paragraphs = tf.paragraphs[:len(tf.paragraphs)-1]
+
+		var textLine *TextLine
+		sb := strings.Builder{}
+		var token string
+
+		for len(tokens) > 0 {
+			if yText+tf.f1.GetDescent(tf.f1.GetSize()) < tf.y+tf.h {
+				token = tokens[len(tokens)-1]
+				tokens = tokens[:len(tokens)-1]
+
+				if tf.f1.StringWidth(tf.f1.GetSize(), sb.String()+token) < tf.w {
+					sb.WriteString(token)
+					sb.WriteString(" ") // Single.space equivalent
+				} else {
+					textLine = NewTextLine(tf.f1, strings.TrimSpace(sb.String()))
+					textLine.SetLocation(tf.x, yText)
+					textLine.DrawOn(page)
+					sb.Reset()
+					tokens = append(tokens, token)
+					yText += tf.leading
+				}
+			} else {
+				tf.paragraphs = append(tf.paragraphs, tokens)
+				tf.drawBorder(page)
+				return []float32{tf.x + tf.w, tf.y + tf.h}, nil
+			}
+		}
+
+		if strings.TrimSpace(sb.String()) != "" {
+			textLine = NewTextLine(tf.f1, strings.TrimSpace(sb.String()))
+			textLine.SetLocation(tf.x, yText)
+			textLine.DrawOn(page)
+			yText += tf.leading
+		}
+		yText += tf.leading
+	}
+
+	tf.drawBorder(page)
+	return []float32{tf.x + tf.w, tf.y + tf.h}, nil
+}
+
+func (tf *TextFrame) drawBorder(page *Page) {
+	if tf.border {
+		rect := NewRect(tf.x, tf.y, tf.w, tf.h)
+		rect.SetBorderColor(tf.borderColor)
 		rect.DrawOn(page)
 	}
 }
 
-func (frame *TextFrame) drawLineOnPage(page *Page, textLine *TextLine) *TextLine {
-	var sb1 strings.Builder
-	var sb2 strings.Builder
-	tokens := strings.Fields(textLine.text)
-	testForFit := true
-	for _, token := range tokens {
-		if testForFit && textLine.font.StringWidth(textLine.font.size, sb1.String()+token) < frame.w {
-			sb1.WriteString(token + single.Space)
-		} else {
-			testForFit = false
-			sb2.WriteString(token + single.Space)
-		}
-	}
-	textLine.SetText(strings.TrimSpace(sb1.String()))
-	if page != nil {
-		textLine.DrawOn(page)
-	}
-	textLine.SetText(strings.TrimSpace(sb2.String()))
-	return textLine
-}
-
-// IsNotEmpty returns true if there is more text to draw, otherwise it returns false.
-func (frame *TextFrame) IsNotEmpty() bool {
-	return len(frame.lines) > 0
+func (tf *TextFrame) HasMoreText() bool {
+	return len(tf.paragraphs) > 0
 }
