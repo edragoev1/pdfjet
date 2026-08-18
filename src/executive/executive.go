5da23c362a872bbe0da1dd10fe488e62066833b9
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-18 17:18:22 -0400

    More cleanup

--- src/executive/executive.go
+++ src/executive/executive.go
@@ -1,9 +1,7 @@
-/**
- * executive.go
- *
- * Copyright (c) 2026 PDFjet Software
- * Licensed under the MIT License. See LICENSE file in the project root.
- */
+// executive.go
+//
+// Copyright (c) 2026 PDFjet Software
+// Licensed under the MIT License. See LICENSE file in the project root.
 
 package executive
 
