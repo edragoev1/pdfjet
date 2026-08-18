cf085ee412fe7d9b672be5fa73fbb88b1cdcd88a
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-18 17:55:40 -0400

    Updated the file headers

--- src/optionalcontentgroup.go
+++ src/optionalcontentgroup.go
@@ -1,12 +1,10 @@
-/**
- * optionalcontentgroup.go
- *
- * Copyright (c) 2026 PDFjet Software
- * Licensed under the MIT License. See LICENSE file in the project root.
- *
- * Original author: Mark Paxton
- * Modified and adapted for use in PDFjet by Evgeni Dragoev
- */
+// optionalcontentgroup.go
+//
+// Copyright (c) 2026 PDFjet Software
+// Licensed under the MIT License. See LICENSE file in the project root.
+//
+// Original author: Mark Paxton
+// Modified and adapted for use in PDFjet by Evgeni Dragoev
 
 package pdfjet
 
