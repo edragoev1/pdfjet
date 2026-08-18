83adfa2a9c1868563bd0d36215c1c315ad3abd28
Evgeni Dragoev <edragoev@protonmail.com>
2026-08-18 13:06:33 -0400

    Fixed the comments

--- src/textframe.go
+++ src/textframe.go
@@ -1,9 +1,7 @@
-/**
- * textframe.go
- *
- * Copyright (c) 2026 PDFjet Software
- * Licensed under the MIT License. See LICENSE file in the project root.
- */
+// textframe.go
+//
+// Copyright (c) 2026 PDFjet Software
+// Licensed under the MIT License. See LICENSE file in the project root.
 
 package pdfjet
 
