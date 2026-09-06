8d809624aa04696787247f801302f93176f87edc
Evgeni Dragoev <edragoev@protonmail.com>
2026-09-06 18:39:38 -0400

    Switched the examples

--- examples/Example_46.java
+++ examples/Example_46.java
@@ -1,60 +1,68 @@
 package examples;
 
 import java.io.*;
-import java.util.*;
 import com.pdfjet.*;
-import com.pdfjet.encryption.*;
 
 /**
  * Example_46.java
  */
 public class Example_46 {
     public Example_46() throws Exception {
-        PDF pdf = new PDF(
-            new BufferedOutputStream(new FileOutputStream("Example_46.pdf")));
-        // pdf.setCompliance(Compliance.PDF_UA_1);
+        PDF pdf = new PDF(new BufferedOutputStream(new FileOutputStream("Example_46.pdf")));
 
-        Passwords passwords = new Passwords();
-        passwords.setUserPassword("hello");
-        passwords.setOwnerPassword("world");
+        Font f1 = new Font(pdf, CoreFont.HELVETICA_BOLD);
+        Font f2 = new Font(pdf, CoreFont.HELVETICA);
 
-        Permissions permissions = new Permissions();
-        permissions.setPermissions(
-            UserAccess.PRINT.getValue() |               // Set both to allow the user to print
-            UserAccess.PRINT_HIGH_QUALITY.getValue() |  // this document with high quality
-            // UserAccess.MODIFY_CONTENTS.getValue() |
-            // UserAccess.COPY_CONTENTS.getValue() |
-            UserAccess.ASSEMBLE_DOCUMENT.getValue(), true);
+        Image image1 = new Image(pdf, "images/map407.png");
+        image1.setLocation(10f, 100f);
 
-        pdf.setEncryption(new Encryption(pdf, passwords, permissions));
-
-        // Font f1 = new Font(pdf, CoreFont.HELVETICA);
-        Font f1 = new Font(pdf, IBMPlexSans.Regular);
-        f1.setSize(36f);
-
-        Image image = new Image(pdf, "images/ee-map.png");
-
-        EmbeddedFile file1 = new EmbeddedFile(pdf, "images/linux-logo.png", Compress.NO);
+        Image image2 = new Image(pdf, "images/qrcode.png");
+        image2.setLocation(10f, 100f);
 
         Page page = new Page(pdf, Letter.PORTRAIT);
 
-        TextLine textLine = new TextLine(f1, "Hello, World!");
-        textLine.setLocation(100f, 100f);
+        TextLine textLine = new TextLine(f2, "© OpenStreetMap contributors");
+        textLine.setLocation(10f, 655f);
+        float[] xy = textLine.drawOn(page);
+
+        textLine = new TextLine(f2, "http://www.openstreetmap.org/copyright");
+        textLine.setURIAction("http://www.openstreetmap.org/copyright");
+        textLine.setLocation(10f, xy[1] + f2.getHeight());
         textLine.drawOn(page);
 
-        image.setLocation(100, 150);
-        image.scaleBy(.5f);
-        image.drawOn(page);
+        OptionalContentGroup group = new OptionalContentGroup(pdf, "Map");
+        group.add(image1);
+        group.setVisible(true);
+        group.setPrintable(true);
+        group.drawOn(page);
 
-        // File attachment functionality
-        FileAttachment attachment = new FileAttachment(pdf, file1);
-        attachment.setLocation(100f, 550f);
-        attachment.setIconPushPin();
-        attachment.setIconSize(24f);
-        attachment.setTitle("Attached File: " + file1.getFileName());
-        attachment.setDescription(
-                "Right mouse click on the icon to save the attached file.");
-        attachment.drawOn(page);
+        TextBox textBox = new TextBox(f1);
+        // textBox.setFontSize(16f);
+        textBox.setText("Blue Layer Text");
+        textBox.setLocation(10f, 130f);
+
+        Line line = new Line();
+        line.setPointA(300f, 150f);
+        line.setPointB(500f, 150f);
+        line.setWidth(2f);
+        line.setColor(Color.blue);
+
+        group = new OptionalContentGroup(pdf, "Blue Line");
+        group.add(textBox);
+        group.add(line);
+        group.setVisible(true);
+        group.drawOn(page);
+
+        line = new Line();
+        line.setPointA(300f, 160f);
+        line.setPointB(500f, 160f);
+        line.setWidth(2f);
+        line.setColor(Color.red);
+
+        group = new OptionalContentGroup(pdf, "Barcode");
+        group.add(image2);
+        group.add(line);
+        group.drawOn(page);
 
         pdf.complete();
     }
